6f24fddcb5f68513ce3620b02c1b8b78c0cacb8d
Lexikos <steve@ragware.net>
2010-02-07 01:31:51 +1000

    L44 - bump revision number

--- source/defines.h
+++ source/defines.h
@@ -32,7 +32,7 @@ GNU General Public License for more details.
 #endif
 
 #ifndef NAME_L_REVISION
-#define NAME_L_REVISION ".L43" // L14: Added .Ln for AutoHotkey_L revision n.
+#define NAME_L_REVISION ".L44"
 #endif
 
 #define LANG_D "AutoHotkey"
