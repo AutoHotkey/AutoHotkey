5aa003bd2e925b9091795aaf3a1617b465f51a22
Lexikos <steve@ragware.net>
2010-03-27 13:25:46 +1000

    L50 - bump revision number

--- source/defines.h
+++ source/defines.h
@@ -32,7 +32,7 @@ GNU General Public License for more details.
 #endif
 
 #ifndef NAME_L_REVISION
-#define NAME_L_REVISION ".L49"
+#define NAME_L_REVISION ".L50"
 #endif
 
 #define LANG_D "AutoHotkey"
