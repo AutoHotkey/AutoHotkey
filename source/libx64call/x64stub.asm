4f0efb7304bb30b2bc49bca5a2697a4b5fab4dc7
Lexikos <steve@ragware.net>
2010-08-10 00:59:22 +1000

    x64: Really fix RegisterCallback

--- source/libx64call/x64stub.asm
+++ source/libx64call/x64stub.asm
@@ -14,11 +14,11 @@ RegisterCallbackAsmStub proc frame
 .allocstack 8*5
 .endprolog
 
-	; For the 'mov' further below.
-	add rsp, 4
+	; For the 'mov' further below
+	add rsp, 8
 
 	; Save the parameters in the spill area for consistency
-	mov qword ptr[rsp+8*0], rcx  ; 1 vs 0 to skip over the return address
+	mov qword ptr[rsp+8*0], rcx
 	mov qword ptr[rsp+8*1], rdx
 	mov qword ptr[rsp+8*2], r8
 	mov qword ptr[rsp+8*3], r9
@@ -28,7 +28,7 @@ RegisterCallbackAsmStub proc frame
 	mov rdx, rax ; RCCallbackFunc* cbAddress
 
 	; Call callback stub function
-	sub rsp, 8*6 ; padding+spill area
+	sub rsp, 8*6 ; retaddr+padding+spill area
 	call qword ptr[rax+CallbackFunctionOffset]
 	add rsp, 8*5
 
