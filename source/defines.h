93bf576c13b6f1a712d4b0cac7f076ba2c9c6252
Lexikos <steve@ragware.net>
2010-08-29 17:27:36 +1000

    L56 - bump revision number

--- source/defines.h
+++ source/defines.h
@@ -32,7 +32,7 @@ GNU General Public License for more details.
 #endif
 
 #ifndef NAME_L_REVISION
-#define NAME_L_REVISION ".L55"
+#define NAME_L_REVISION ".L56"
 #endif
 
 #define LANG_D "AutoHotkey"
