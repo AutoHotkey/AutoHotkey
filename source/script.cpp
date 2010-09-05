9084703a8a862d169e0fb27a9fd7263ed10b1573
Lexikos <steve@ragware.net>
2010-09-05 17:41:10 +1000

    Avoid cascading error message for failed #include <Lib>

--- source/script.cpp
+++ source/script.cpp
@@ -2679,7 +2679,7 @@ inline ResultType Script::IsDirective(LPTSTR aBuf)
 				if (!error_was_shown && file_was_found || ignore_load_failure)
 					return CONDITION_TRUE;
 				*parameter_end = '>'; // Restore '>' for display to the user.
-				return ScriptError(_T("Function library not found."), aBuf);
+				return error_was_shown ? FAIL : ScriptError(_T("Function library not found."), aBuf);
 			}
 			//else invalid syntax; treat it as a regular #include which will almost certainly fail.
 		}
