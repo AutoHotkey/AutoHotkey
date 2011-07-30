AutoHotkey/AutoHotkey

commit a8eeef4690817a9d64b84a907bb971937647792d

Optimized WinGet ProcessName.

source/script2.cpp
@@ -24,6 +24,7 @@ GNU General Public License for more details.
 #include "application.h" // for MsgSleep()
 #include "resources/resource.h"  // For InputBox.
 #include "TextIO.h"
+#include <Psapi.h> // for GetModuleBaseName.
 
 #define PCRE_STATIC             // For RegEx. PCRE_STATIC tells PCRE to declare its functions for normal, static
 #include "lib_pcre/pcre/pcre.h" // linkage rather than as functions inside an external DLL.
@@ -3634,9 +3635,9 @@ ResultType Line::WinGet(LPTSTR aCmd, LPTSTR aTitle, LPTSTR aText, LPTSTR aExclud
 			if (cmd == WINGET_CMD_PID)
 				return output_var.Assign(pid);
 			// Otherwise, get the full path and name of the executable that owns this window.
-			_ultot(pid, buf, 10);
 			TCHAR process_name[MAX_PATH];
-			if (ProcessExist(buf, process_name))
+			HANDLE hproc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
+			if (hproc && GetModuleBaseName(hproc, NULL, process_name, _countof(process_name)))
 				return output_var.Assign(process_name);
 		}
 		// If above didn't return:

AutoHotkeyx.vcxproj
@@ -95,7 +95,7 @@
     </ClCompile>
     <Link>
       <SubSystem>Windows</SubSystem>
-      <AdditionalDependencies>wsock32.lib;winmm.lib;version.lib;comctl32.lib;%(AdditionalDependencies)</AdditionalDependencies>
+      <AdditionalDependencies>wsock32.lib;winmm.lib;version.lib;comctl32.lib;psapi.lib;%(AdditionalDependencies)</AdditionalDependencies>
       <StackReserveSize>4194304</StackReserveSize>
       <TerminalServerAware>false</TerminalServerAware>
       <DataExecutionPrevention>false</DataExecutionPrevention>

source/script.h
@@ -211,18 +211,18 @@ enum CommandIDs {CONTROL_ID_FIRST = IDCANCEL + 1
 //----------------------------------------------------------------------------------
 
 void DoIncrementalMouseMove(int aX1, int aY1, int aX2, int aY2, int aSpeed);
-DWORD ProcessExist9x2000(LPTSTR aProcess, LPTSTR aProcessName);
+DWORD ProcessExist9x2000(LPTSTR aProcess);
 #ifdef CONFIG_WINNT4
-DWORD ProcessExistNT4(LPTSTR aProcess, LPTSTR aProcessName);
+DWORD ProcessExistNT4(LPTSTR aProcess);
 #endif
 
-inline DWORD ProcessExist(LPTSTR aProcess, LPTSTR aProcessName = NULL)
+inline DWORD ProcessExist(LPTSTR aProcess)
 {
 	return 
 #ifdef CONFIG_WINNT4
-		g_os.IsWinNT4() ? ProcessExistNT4(aProcess, aProcessName) :
+		g_os.IsWinNT4() ? ProcessExistNT4(aProcess) :
 #endif
-		ProcessExist9x2000(aProcess, aProcessName);
+		ProcessExist9x2000(aProcess);
 }
 
 bool Util_Shutdown(int nFlag);

source/script_autoit.cpp
@@ -2164,11 +2164,8 @@ void DoIncrementalMouseMove(int aX1, int aY1, int aX2, int aY2, int aSpeed)
 // PROCESS ROUTINES
 ////////////////////
 
-DWORD ProcessExist9x2000(LPTSTR aProcess, LPTSTR aProcessName)
+DWORD ProcessExist9x2000(LPTSTR aProcess)
 {
-	if (aProcessName) // Init this output variable in case of early return.
-		*aProcessName = '\0';
-
 	// We must dynamically load the function or program will probably not launch at all on NT4.
 	typedef BOOL (WINAPI *PROCESSWALK)(HANDLE hSnapshot, LPPROCESSENTRY32 lppe);
 	typedef HANDLE (WINAPI *CREATESNAPSHOT)(DWORD dwFlags, DWORD th32ProcessID);
@@ -2194,13 +2191,6 @@ DWORD ProcessExist9x2000(LPTSTR aProcess, LPTSTR aProcessName)
 	{
 		if (specified_pid && specified_pid == proc.th32ProcessID)
 		{
-			if (aProcessName) // Caller wanted process name also.
-			{
-				// For consistency in results, use _splitpath() both here and below rather than
-				// something that just checks for a rightmost backslash.
-				_tsplitpath(proc.szExeFile, szDrive, szDir, aProcessName, szExt);
-				_tcscat(aProcessName, szExt);
-			}
 			CloseHandle(snapshot);
 			return specified_pid;
 		}
@@ -2212,8 +2202,6 @@ DWORD ProcessExist9x2000(LPTSTR aProcess, LPTSTR aProcessName)
 		_tcscat(szFile, szExt);
 		if (!_tcsicmp(szFile, aProcess)) // lstrcmpi() is not used: 1) avoids breaking exisitng scripts; 2) provides consistent behavior across multiple locales; 3) performance.
 		{
-			if (aProcessName) // Caller wanted process name also.
-				_tcscpy(aProcessName, szFile);
 			CloseHandle(snapshot);
 			return proc.th32ProcessID;
 		}

commit c0289696ab1954d83fc68e806480b8aaa8b7f20b

Fixed handle leak in WinGet ProcessName (caused by commit a8eeef4).

source/script2.cpp
@@ -3634,11 +3634,15 @@ ResultType Line::WinGet(LPTSTR aCmd, LPTSTR aTitle, LPTSTR aText, LPTSTR aExclud
 			GetWindowThreadProcessId(target_window, &pid);
 			if (cmd == WINGET_CMD_PID)
 				return output_var.Assign(pid);
-			// Otherwise, get the full path and name of the executable that owns this window.
+			// Otherwise, get the name of the executable that owns this window.
 			TCHAR process_name[MAX_PATH];
 			HANDLE hproc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
-			if (hproc && GetModuleBaseName(hproc, NULL, process_name, _countof(process_name)))
-				return output_var.Assign(process_name);
+			if (hproc)
+			{
+				if (GetModuleBaseName(hproc, NULL, process_name, _countof(process_name)))
+					return output_var.Assign(process_name);
+				CloseHandle(hproc);
+			}
 		}
 		// If above didn't return:
 		return output_var.Assign();