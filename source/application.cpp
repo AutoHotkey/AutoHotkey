AutoHotkey/AutoHotkey

commit 4083affb34200662ed57a17122841944df92c5e9

Fixed CPU being maxed out while waiting in certain cases.

source/application.cpp
@@ -203,6 +203,7 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 	//bool was_interrupted = false;
 	bool sleep0_was_done = false;
 	bool empty_the_queue_via_peek = false;
+	int messages_received = 0; // This is used to ensure we Sleep() at least a minimal amount if no messages are received.
 
 	int i;
 	bool msg_was_handled;
@@ -379,6 +380,12 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 					continue;
 				}
 				// Otherwise: aSleepDuration is non-zero or we already did the Sleep(0)
+				if (messages_received == 0 && allow_early_return)
+				{
+					Sleep(5); // Since Peek() didn't find a message, avoid maxing the CPU.  This is a somewhat arbitrary value: the intent of a value below 10 is to avoid yielding more than one timeslice on all systems even if they have unusual timeslice sizes / system timers.
+					++messages_received; // Don't repeat this section.
+					continue;
+				}
 				// Notes for the macro further below:
 				// Must decrement prior to every RETURN to balance it.
 				// Do this prior to checking whether timer should be killed, below.
@@ -443,6 +450,7 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 
 		// Since above didn't return or "continue", a message has been received that is eligible
 		// for further processing.
+		++messages_received;
 
 		// For max. flexibility, it seems best to allow the message filter to have the first
 		// crack at looking at the message, before even TRANSLATE_AHK_MSG:

commit c05c18de47b1c3ecf303cc9c4afcdb9e6e2bf046

Fixed SendEvent with 0 key delay; broken by commit 4083aff.

source/application.cpp
@@ -382,7 +382,16 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 				// Otherwise: aSleepDuration is non-zero or we already did the Sleep(0)
 				if (messages_received == 0 && allow_early_return)
 				{
-					Sleep(5); // Since Peek() didn't find a message, avoid maxing the CPU.  This is a somewhat arbitrary value: the intent of a value below 10 is to avoid yielding more than one timeslice on all systems even if they have unusual timeslice sizes / system timers.
+					// Fix for v1.1.05.04: Since Peek() didn't find a message, avoid maxing the CPU.
+					// This specific section is needed for PerformWait() when an underlying thread
+					// is displaying a dialog, and perhaps in other cases.
+					// Fix for v1.1.07.00: Avoid Sleep() if caller specified a duration of zero;
+					// otherwise SendEvent with a key delay of 0 will be slower than expected.
+					// This affects auto-replace hotstrings in SendEvent mode (which is the default
+					// when SendInput is unavailable).  Note that if aSleepDuration == 0, Sleep(0)
+					// was already called above or by a prior iteration.
+					if (aSleepDuration > 0)
+						Sleep(5); // This is a somewhat arbitrary value: the intent of a value below 10 is to avoid yielding more than one timeslice on all systems even if they have unusual timeslice sizes / system timers.
 					++messages_received; // Don't repeat this section.
 					continue;
 				}