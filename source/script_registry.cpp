f4f7118268d466b7d41526e3eb1cd7ab15728fa1
Lexikos <steve@ragware.net>
2012-10-27 19:39:52 +1000

    Fixed RegRead indicating failure after successfully reading binary data.

--- source/script_registry.cpp
+++ source/script_registry.cpp
@@ -330,6 +330,7 @@ ResultType Line::RegRead(HKEY aRootKey, LPTSTR aRegSubkey, LPTSTR aValueName)
 			contents[j] = '\0'; // Terminate
 			if (!output_var.Close()) // Length() was already set by the earlier call to Assign().
 				return FAIL;
+			break;
 		}
 		default:
 			RegCloseKey(hRegKey);
