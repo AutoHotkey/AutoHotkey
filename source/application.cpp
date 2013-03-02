AutoHotkey/AutoHotkey

commit ee88fd78edc1808a9a38497511f0392c723cdf1a

Fix "Custom" control WM_NOTIFY handling

source/application.cpp
@@ -221,7 +221,8 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 	GuiControlType *pcontrol, *ptab_control;
 	GuiIndexType gui_control_index;
 	GuiEventType gui_action;
-	DWORD gui_event_info, gui_size;
+	DWORD_PTR gui_event_info;
+	DWORD gui_size;
 	bool *pgui_label_is_running, event_is_control_generated;
 	Label *gui_label;
 	HDROP hdrop_to_free;
@@ -667,7 +668,7 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 					// GUI windows might cause GuiWindowProc to fwd it back to us, creating an infinite loop.
 					goto break_out_of_main_switch; // Goto seems preferably in this case for code size & performance.
 				
-				gui_event_info =    (DWORD)msg.lParam;
+				gui_event_info =    (DWORD_PTR)msg.lParam;
 				gui_action =        LOWORD(msg.wParam);
 				gui_control_index = HIWORD(msg.wParam); // Caller has set it to NO_CONTROL_INDEX if it isn't applicable.
 

source/script.h
@@ -2513,6 +2513,7 @@ public:
 	static int FindFont(FontType &aFont);
 
 	void Event(GuiIndexType aControlIndex, UINT aNotifyCode, USHORT aGuiEvent = GUI_EVENT_NONE, UINT_PTR aEventInfo = 0);
+	void CustomCtrlWmNotify(GuiIndexType aControlIndex, LPNMHDR aNmHdr);
 
 	static WORD TextToHotkey(LPTSTR aText);
 	static LPTSTR HotkeyToText(WORD aHotkey, LPTSTR aBuf);

source/script_gui.cpp
@@ -8878,7 +8878,7 @@ void GuiType::Event(GuiIndexType aControlIndex, UINT aNotifyCode, USHORT aGuiEve
 			{
 				// This is a WM_NOTIFY handler and thus we need to set A_GuiEvent to 'N'. Due to the nature
 				// of WM_NOTIFY messages we cannot use PostMessage because otherwise the NMHDR pointer becomes toast.
-				SendMessage(mHwnd, AHK_GUI_ACTION, (WPARAM)((aControlIndex << 16) | 'N'), (LPARAM) aEventInfo);
+				CustomCtrlWmNotify(aControlIndex, (LPNMHDR) aEventInfo);
 				return;
 			}
 			// Copy the notification code to A_EventInfo.
@@ -9011,6 +9011,44 @@ void GuiType::Event(GuiIndexType aControlIndex, UINT aNotifyCode, USHORT aGuiEve
 	// would prevent them from being re-posted back to the queue (see "case AHK_GUI_ACTION" in GuiWindowProc()).
 }
 
+
+void GuiType::CustomCtrlWmNotify(GuiIndexType aControlIndex, LPNMHDR aNmHdr)
+{
+	// See MsgMonitor() in application.cpp for comments, as this method is based on the beforementioned function.
+
+	if (!INTERRUPTIBLE_IN_EMERGENCY)
+		return;
+
+	GuiControlType& aControl = mControl[aControlIndex];
+	Label& glabel = *aControl.jump_to_label;
+	Line* jumpToLine = glabel.mJumpToLine;
+
+	if (g_nThreads >= g_MaxThreadsTotal)
+		if (g_nThreads >= MAX_THREADS_EMERGENCY
+			|| jumpToLine->mActionType != ACT_EXITAPP && jumpToLine->mActionType != ACT_RELOAD)
+			return;
+
+	if (g->Priority > 0)
+		return;
+
+	TCHAR ErrorLevel_saved[ERRORLEVEL_SAVED_SIZE];
+	tcslcpy(ErrorLevel_saved, g_ErrorLevel->Contents(), _countof(ErrorLevel_saved));
+	InitNewThread(0, false, true, jumpToLine->mActionType);
+	DEBUGGER_STACK_PUSH(glabel.mJumpToLine, glabel.mName)
+
+	AddRef();
+	AddRef();
+	g->GuiWindow = this;
+	g->GuiDefaultWindow = this;
+	g->GuiControlIndex = aControlIndex;
+	g->GuiEvent = 'N';
+	g->EventInfo = (DWORD_PTR) aNmHdr;
+	glabel.Execute();
+
+	DEBUGGER_STACK_POP()
+	Release();
+	ResumeUnderlyingThread(ErrorLevel_saved);
+}
 
 
 WORD GuiType::TextToHotkey(LPTSTR aText)

commit 4381d8436616a23bff3b373087a74188bffbec41

Fixed x64 warning caused by commit ee88fd78.

source/application.cpp
@@ -674,7 +674,7 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 
 				if (gui_action == GUI_EVENT_RESIZE) // This section be done after above but before pcontrol below.
 				{
-					gui_size = gui_event_info; // Temp storage until the "g" struct becomes available for the new thread.
+					gui_size = (DWORD)gui_event_info; // Temp storage until the "g" struct becomes available for the new thread.
 					gui_event_info = gui_control_index; // SizeType is stored in index in this case.
 					gui_control_index = NO_CONTROL_INDEX;
 				}
@@ -1204,7 +1204,7 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 				{
 					LITEM item = {};
 					item.mask=LIF_URL|LIF_ITEMID|LIF_ITEMINDEX;
-					item.iLink = gui_event_info - 1;
+					item.iLink = (int)gui_event_info - 1;
 					if(SendMessage(pcontrol->hwnd,LM_GETITEM,NULL,(LPARAM)&item))
 						g_ErrorLevel->AssignString(*item.szUrl ? CStringTCharFromWCharIfNeeded(item.szUrl) : CStringTCharFromWCharIfNeeded(item.szID));
 				}