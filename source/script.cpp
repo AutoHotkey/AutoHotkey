AutoHotkey/AutoHotkey

commit 69e9675065547c151a0df315ddfddfd08debe4cb

Revised ExpandArgs(aResultToken) for maintainability; fixed `throw str`.

source/script.cpp
@@ -10037,6 +10037,7 @@ end_of_infix_to_postfix:
 	SymbolType only_symbol = only_token.symbol;
 	if (   postfix_count == 1 && IS_OPERAND(only_symbol) // This expression is a lone operand, like (1) or "string".
 		&& (mActionType < ACT_FOR || mActionType > ACT_UNTIL) // It's not WHILE or UNTIL, which currently perform better as expressions, or FOR, which performs the same but currently expects aResultToken to always be set.
+		&& (mActionType != ACT_THROW) // Exclude THROW to simplify variable handling (ensures vars are always dereferenced).
 		&& (only_symbol != SYM_STRING || mActionType != ACT_SENDMESSAGE && mActionType != ACT_POSTMESSAGE)   ) // It's not something like SendMessage WM_SETTEXT,,"New text" (which requires the leading quote mark to be present).
 	{
 		if (only_symbol == SYM_DYNAMIC) // This needs some extra checks to ensure correct behaviour.
@@ -10092,7 +10093,7 @@ end_of_infix_to_postfix:
 				break;
 			}
 			aArg.is_expression = false;
-			if (mActionType > ACT_FUNC && mActionType != ACT_RETURN) // Excludes ACT_ASSIGNEXPR, ACT_METHOD, ACT_FUNC and ACT_RETURN.
+			if (!ACT_USES_SIMPLE_POSTFIX(mActionType))
 				return OK;
 			// Otherwise, continue on below to copy the postfix token into persistent memory.
 		}
@@ -10883,12 +10884,13 @@ ResultType Line::ExecUntil(ExecUntilMode aMode, ResultToken *aResultToken, Line 
 				return result;
 			}
 
-			if (ARGVAR1) // Currently this is non-NULL only if the arg was a simple var reference, in which case token wasn't written to because ExpandExpression() wasn't called.
-				ARGVAR1->ToToken(*token);
-			if (token->symbol == SYM_STRING)
+			if (token->symbol == SYM_STRING && !token->mem_to_free)
 			{
-				// Throwing a string is pretty rare, so keep it simple:
-				if (!token->Malloc(token->marker, ArgLength(1)))
+				// There are a couple of shortcuts we could take here, but they aren't taken
+				// because throwing a string is pretty rare and not a performance priority.
+				// Shortcut #1: Assign _T("") if ARG1 is empty.
+				// Shortcut #2: Take over sDerefBuf if ARG1 == sDerefBuf.
+				if (!token->Malloc(token->marker, _tcslen(token->marker)))
 				{
 					delete token;
 					// See ThrowRuntimeException() for comments.
@@ -13035,16 +13037,19 @@ ResultType Line::Perform()
 		
 		for (int i = 0; i < param_count; ++i, ++arg)
 		{
-			if (sArgVar[arg] && sArgVar[arg]->Type() == VAR_NORMAL) // Only normal variables can be SYM_VAR.
+			if (sArgVar[arg])
 			{
-				param_tok[arg].symbol = SYM_VAR;
-				param_tok[arg].var = sArgVar[arg];
-			}
-			else
-			{
-				if (param_tok[arg].symbol == SYM_STRING) // i.e. still at the default we set.
+				if (sArgVar[arg]->Type() == VAR_NORMAL) // Only normal variables can be SYM_VAR.
+				{
+					param_tok[arg].symbol = SYM_VAR;
+					param_tok[arg].var = sArgVar[arg];
+				}
+				else
+				{
+					// This arg must be a simple built-in var reference, not an expression.
+					// ExpandArgs() didn't set marker in this case, so set it now:
 					param_tok[arg].marker = sArgDeref[arg];
-				//else use the type and value set by ExpandArgs().
+				}
 			}
 			param_ptr[i] = &param_tok[arg];
 		}

source/defines.h
@@ -472,6 +472,7 @@ enum enum_act {
 #define ACT_IS_IF_OR_ELSE_OR_LOOP(ActionType) (ActionType <= ACT_WHILE && ActionType >= ACT_ELSE)
 #define ACT_LOOP_ALLOWS_UNTIL(ActionType) (ActionType <= ACT_FOR && ActionType >= ACT_LOOP) // UNTIL is currently unsupported with WHILE, for performance/code size (doesn't seem useful anyway).
 #define ACT_EXPANDS_ITS_OWN_ARGS(ActionType) (ActionType <= ACT_FUNC || ActionType == ACT_WHILE || ActionType == ACT_FOR || ActionType == ACT_THROW)
+#define ACT_USES_SIMPLE_POSTFIX(ActionType) (ActionType <= ACT_FUNC || ActionType == ACT_RETURN) // Actions which are optimized to use arg.postfix when is_expression == false, via the "only_token" optimization.
 
 // For convenience in many places.  Must cast to int to avoid loss of negative values.
 #define BUF_SPACE_REMAINING ((int)(aBufSize - (aBuf - aBuf_orig)))

source/script_expression.cpp
@@ -1471,6 +1471,8 @@ push_this_token:
 		else // Deref buf is already large enough to fit the string.
 			if (aTarget != result) // Currently, might be always true.
 				tmemmove(aTarget, result, result_size); // memmove() vs. memcpy() in this case, since source and dest might overlap (i.e. "target" may have been used to put temporary things into aTarget, but those things are no longer needed and now safe to overwrite).
+		if (aResultToken)
+			aResultToken->marker = aTarget;
 		aTarget += result_size;
 		goto normal_end_skip_output_var; // output_var was already checked higher above, so no need to consider it again.
 
@@ -1975,6 +1977,9 @@ ResultType Line::ExpandArgs(ResultToken *aResultTokens)
 				arg_deref[i] = this_arg.text;  // Point the dereferenced arg to the arg text itself.
 				continue;  // Don't need to use the deref buffer in this case.
 			}
+			// Since above didn't continue, this arg is a plain variable reference.
+			// Even if aResultTokens != NULL, it isn't set because our callers handle vars
+			// in different ways (and checking sArgVar is easier than checking for SYM_VAR).
 
 			switch(ArgMustBeDereferenced(the_only_var_of_this_arg, i, arg_var)) // Yes, it was called by GetExpandedArgSize() too, but a review shows it's difficult to avoid this without being worse than the disease (10/22/2006).
 			{

commit 8bb0e699a28d687ff73af499f391dbc62787b0ab

Fixed plain text args of ACT_FUNC/ACT_METHOD (broken by 69e9675).

source/script.cpp
@@ -7993,9 +7993,19 @@ Line *Script::PreparseBlocks(Line *aStartingLine, bool aFindBlockEnd, Line *aPar
 		for (i = 0; i < line->mArgc; ++i) // For each arg.
 		{
 			ArgStruct &this_arg = line->mArg[i]; // For performance and convenience.
-			if (!*this_arg.text // Blank, or a pre-resolved input/output var.
-				 || !this_arg.is_expression && !this_arg.deref) // Plain text.
+			if (!*this_arg.text) // Blank, or a pre-resolved input/output var.
 				continue;
+			if (!this_arg.is_expression && !this_arg.deref) // Plain text.
+			{
+				if (ACT_USES_SIMPLE_POSTFIX(line->mActionType))
+				{
+					// This ensures ExpandArgs() always sets aResultTokens[i].marker.
+					this_arg.postfix = (ExprTokenType *)SimpleHeap::Malloc(sizeof(ExprTokenType));
+					this_arg.postfix->symbol = SYM_STRING;
+					this_arg.postfix->marker = this_arg.text;
+				}
+				continue;
+			}
 			// Otherwise, the arg will be processed by ExpressionToPostfix(), which will set is_expression
 			// based on whether the arg should be evaluated by ExpandExpression().  
 			if (this_arg.deref) // If false, no function-calls are present because the expression contains neither variables nor function calls.