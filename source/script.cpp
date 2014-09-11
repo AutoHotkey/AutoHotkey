AutoHotkey/AutoHotkey

commit a6ea27fc88fc83f105fe2528e0ff1ade3bd4dd28

Added capability to define property accessors in classes.

source/script.cpp
@@ -62,7 +62,7 @@ Script::Script()
 	, mFirstMenu(NULL), mLastMenu(NULL), mMenuCount(0)
 	, mVar(NULL), mVarCount(0), mVarCountMax(0), mLazyVar(NULL), mLazyVarCount(0)
 	, mCurrentFuncOpenBlockCount(0), mNextLineIsFunctionBody(false)
-	, mClassObjectCount(0), mUnresolvedClasses(NULL)
+	, mClassObjectCount(0), mUnresolvedClasses(NULL), mClassProperty(NULL), mClassPropertyDef(NULL)
 	, mCurrFileIndex(0), mCombinedLineNumber(0), mNoHotkeyLabels(true), mMenuUseErrorLevel(false)
 	, mFileSpec(_T("")), mFileDir(_T("")), mFileName(_T("")), mOurEXE(_T("")), mOurEXEDir(_T("")), mMainWindowTitle(_T(""))
 	, mIsReadyToExecute(false), mAutoExecSectionIsRunning(false)
@@ -1318,7 +1318,11 @@ ResultType Script::LoadIncludedFile(LPTSTR aFileSpec, bool aAllowDuplicateInclud
 	LPTSTR buf = buf1, next_buf = buf2; // Oscillate between bufs to improve performance (avoids memcpy from buf2 to buf1).
 	size_t buf_length, next_buf_length, suffix_length;
 	bool pending_buf_has_brace;
-	bool pending_buf_is_class; // True for class definition, false for function definition/call.
+	enum {
+		Pending_Func,
+		Pending_Class,
+		Pending_Property
+	} pending_buf_type;
 
 #ifndef AUTOHOTKEYSC
 	TextFile tfile, *fp = &tfile;
@@ -1874,13 +1878,17 @@ process_completed_line:
 			// Could use *g_act[ACT_BLOCK_BEGIN].Name instead of '{', but it seems too elaborate to be worth it.
 			if (*buf == '{' || pending_buf_has_brace) // v1.0.41: Support one-true-brace, e.g. fn(...) {
 			{
-				if (pending_buf_is_class)
+				switch (pending_buf_type)
 				{
+				case Pending_Class:
 					if (!DefineClass(pending_buf))
 						return FAIL;
-				}
-				else
-				{
+					break;
+				case Pending_Property:
+					if (!DefineClassProperty(pending_buf))
+						return FAIL;
+					break;
+				case Pending_Func:
 					// Note that two consecutive function definitions aren't possible:
 					// fn1()
 					// fn2()
@@ -1905,26 +1913,26 @@ process_completed_line:
 							return FAIL;
 						mCurrLine = NULL; // L30: Prevents showing misleading vicinity lines if the line after a OTB function def is a syntax error.
 					}
+					break;
+#ifdef _DEBUG
+				default:
+					return ScriptError(_T("DEBUG: pending_buf_type has an unexpected value."));
+#endif
 				}
 			}
 			else // It's a function call on a line by itself, such as fn(x). It can't be if(..) because another section checked that.
 			{
-				if (pending_buf_is_class) // Missing open-brace for class definition.
+				if (pending_buf_type != Pending_Func) // Missing open-brace for class definition.
 					return ScriptError(ERR_MISSING_OPEN_BRACE, pending_buf);
 				if (mClassObjectCount && !g->CurrentFunc) // Unexpected function call in class definition.
-					return ScriptError(ERR_INVALID_LINE_IN_CLASS_DEF, pending_buf);
+					return ScriptError(mClassProperty ? ERR_MISSING_OPEN_BRACE : ERR_INVALID_LINE_IN_CLASS_DEF, pending_buf);
 				if (!ParseAndAddLine(pending_buf, ACT_EXPRESSION))
 					return FAIL;
 				mCurrLine = NULL; // Prevents showing misleading vicinity lines if the line after a function call is a syntax error.
 			}
 			*pending_buf = '\0'; // Reset now that it's been fully handled, as an indicator for subsequent iterations.
-			if (pending_buf_is_class)
+			if (pending_buf_type != Pending_Func) // Class or property.
 			{
-				// We have the "{" for this class, either in pending_buf (OTB) or in buf (the line after).
-				// Add a block-begin so that PreparseBlocks() will point at this line if the block-end is
-				// missing.  Without this, such errors mightn't be detected at all.
-				if (!AddLine(ACT_BLOCK_BEGIN))
-					return FAIL;
 				if (!pending_buf_has_brace)
 				{
 					// This is the open-brace of a class definition, so requires no further processing.
@@ -1945,10 +1953,22 @@ process_completed_line:
 
 		if (*buf == '}' && mClassObjectCount && !g->CurrentFunc)
 		{
+			cp = buf;
+			if (mClassProperty)
+			{
+				// Close this property definition.
+				mClassProperty = NULL;
+				if (mClassPropertyDef)
+				{
+					free(mClassPropertyDef);
+					mClassPropertyDef = NULL;
+				}
+				cp = omit_leading_whitespace(cp + 1);
+			}
 			// Handling this before the two sections below allows a function or class definition
 			// to begin immediately after the close-brace of a previous class definition.
 			// This loop allows something like }}} to terminate multiple nested classes:
-			for (cp = buf; *cp == '}' && mClassObjectCount; cp = omit_leading_whitespace(cp + 1))
+			for ( ; *cp == '}' && mClassObjectCount; cp = omit_leading_whitespace(cp + 1))
 			{
 				// End of class definition.
 				--mClassObjectCount;
@@ -1959,15 +1979,35 @@ process_completed_line:
 					*cp1 = '\0';
 				else
 					*mClassName = '\0';
-				// Add a block-end to counter the block-begin, for validation purposes.
-				if (!AddLine(ACT_BLOCK_END))
-					return FAIL;
 			}
 			// cp now points at the next non-whitespace char after the brace.
 			if (!*cp)
 				goto continue_main_loop;
 			// Otherwise, there is something following this close-brace, so continue on below to process it.
 			tmemmove(buf, cp, buf_length = _tcslen(cp));
+		}
+
+		if (mClassProperty && !g->CurrentFunc) // This is checked before IsFunction() to prevent method definitions inside a property.
+		{
+			if (!_tcsnicmp(buf, _T("Get"), 3) || !_tcsnicmp(buf, _T("Set"), 3))
+			{
+				LPTSTR cp = omit_leading_whitespace(buf + 3);
+				if ( !*cp || (*cp == '{' && !cp[1]) )
+				{
+					// Defer this line until the next line comes in to simplify handling of '{' and OTB.
+					// For simplicity, pass the property definition to DefineFunc instead of the actual
+					// line text, even though it makes some error messages a bit inaccurate. (That would
+					// happen anyway when DefineFunc() finds a syntax error in the parameter list.)
+					_tcscpy(pending_buf, mClassPropertyDef);
+					LPTSTR dot = _tcschr(pending_buf, '.');
+					dot[1] = *buf; // Replace the x in property.xet(params).
+					pending_buf_line_number = mCombinedLineNumber;
+					pending_buf_has_brace = *cp == '{';
+					pending_buf_type = Pending_Func;
+					goto continue_main_loop;
+				}
+			}
+			return ScriptError(ERR_INVALID_LINE_IN_PROPERTY_DEF, buf);
 		}
 
 		// By doing the following section prior to checking for hotkey and hotstring labels, double colons do
@@ -1980,7 +2020,7 @@ process_completed_line:
 			// a function call vs. definition:
 			_tcscpy(pending_buf, buf);
 			pending_buf_line_number = mCombinedLineNumber;
-			pending_buf_is_class = false;
+			pending_buf_type = Pending_Func;
 			goto continue_main_loop; // In lieu of "continue", for performance.
 		}
 		
@@ -1991,7 +2031,7 @@ process_completed_line:
 				// Defer this line until the next line comes in to simplify handling of '{' and OTB:
 				_tcscpy(pending_buf, class_name);
 				pending_buf_line_number = mCombinedLineNumber;
-				pending_buf_is_class = true;
+				pending_buf_type = Pending_Class;
 				goto continue_main_loop; // In lieu of "continue", for performance.
 			}
 
@@ -2007,6 +2047,21 @@ process_completed_line:
 						if (!DefineClassVars(buf, false)) // See above for comments.
 							return FAIL;
 						goto continue_main_loop;
+					}
+					if ( !*cp || *cp == '[' || (*cp == '{' && !cp[1]) ) // Property
+					{
+						size_t length = _tcslen(buf);
+						if (pending_buf_has_brace = (buf[length - 1] == '{'))
+						{
+							// Omit '{' and trailing whitespace from further consideration.
+							rtrim(buf, length - 1);
+						}
+						
+						// Defer this line until the next line comes in to simplify handling of '{' and OTB:
+						_tcscpy(pending_buf, buf);
+						pending_buf_line_number = mCombinedLineNumber;
+						pending_buf_type = Pending_Property;
+						goto continue_main_loop; // In lieu of "continue", for performance.
 					}
 				}
 				if (!_tcsnicmp(buf, _T("Static"), 6) && IS_SPACE_OR_TAB(buf[6]))
@@ -2590,11 +2645,20 @@ continue_main_loop: // This method is used in lieu of "continue" for performance
 		// alternatives due to the use of "continue" in some places above.
 		saved_line_number = mCombinedLineNumber;
 		mCombinedLineNumber = pending_buf_line_number; // Done so that any syntax errors that occur during the calls below will report the correct line number.
-		if (pending_buf_is_class)
+		if (pending_buf_type != Pending_Func)
 			return ScriptError(pending_buf_has_brace ? ERR_MISSING_CLOSE_BRACE : ERR_MISSING_OPEN_BRACE, pending_buf);
 		if (!ParseAndAddLine(pending_buf, ACT_EXPRESSION)) // Must be function call vs. definition since otherwise the above would have detected the opening brace beneath it and already cleared pending_function.
 			return FAIL;
 		mCombinedLineNumber = saved_line_number;
+	}
+
+	if (mClassObjectCount) // or mClassProperty, which implies mClassObjectCount != 0.
+	{
+		// A class definition has not been closed with "}".  Previously this was detected by adding
+		// the open and close braces as lines, but this way is simpler and has less overhead.
+		// The downside is that the line number won't be shown; however, the class name will.
+		// Seems okay not to show mClassProperty->mName since the class is missing "}" as well.
+		return ScriptError(ERR_MISSING_CLOSE_BRACE, mClassName);
 	}
 
 	++mCombinedLineNumber; // L40: Put the implicit ACT_EXIT on the line after the last physical line (for the debugger).
@@ -6911,25 +6975,28 @@ ResultType Script::DefineFunc(LPTSTR aBuf, Var *aFuncGlobalVar[])
 	LPTSTR param_end, param_start = _tcschr(aBuf, '('); // Caller has ensured that this will return non-NULL.
 	int insert_pos;
 	
-	if (mClassObjectCount)
+	if (mClassObjectCount) // Class method or property getter/setter.
 	{
 		Object *class_object = mClassObject[mClassObjectCount - 1];
 		LPTSTR full_name;
+		
 		// Build the fully-qualified method name for A_ThisFunc and ListVars:
 		*param_start = '\0'; // Temporarily terminate.
 		if (  !(full_name = (LPTSTR)SimpleHeap::Malloc((_tcslen(mClassName) + _tcslen(aBuf) + 2) * sizeof(TCHAR)))  ) // +2 for dot and null-terminator.
 			return FAIL;
 		_stprintf(full_name, _T("%s.%s"), mClassName, aBuf);
+
 		// Check for duplicates and determine insert_pos:
 		Func *found_func;
 		ExprTokenType found_item;
-		if (class_object->GetItem(found_item, aBuf) // Must be done in addition to the below to detect conflicting var/method declarations.
+		if (!mClassProperty && class_object->GetItem(found_item, aBuf) // Must be done in addition to the below to detect conflicting var/method declarations.
 			|| (found_func = FindFunc(full_name, 0, &insert_pos))) // Must be done to determine insert_pos.
 		{
 			*param_start = '(';
 			return ScriptError(ERR_DUPLICATE_DECLARATION, aBuf);
 		}
 		*param_start = '('; // Undo termination.
+
 		// Below passes class_object for AddFunc() to store the func "by reference" in it:
 		if (  !(g->CurrentFunc = AddFunc(full_name, 0, false, insert_pos, class_object))  )
 			return FAIL;
@@ -6974,6 +7041,18 @@ ResultType Script::DefineFunc(LPTSTR aBuf, Var *aFuncGlobalVar[])
 		param[0].is_byref = false;
 		++param_count;
 		++func.mMinParams;
+
+		if (mClassProperty && toupper(param_start[-3]) == 'S') // Set
+		{
+			// Unlike __Set, the property setters accept the value as the second parameter,
+			// so that we can make it automatic here and make variadic properties easier.
+			if (  !(param[1].var = FindOrAddVar(_T("value"), 5, VAR_DECLARE_LOCAL | VAR_LOCAL_FUNCPARAM))  )
+				return FAIL;
+			param[1].default_type = PARAM_DEFAULT_NONE;
+			param[1].is_byref = false;
+			++param_count;
+			++func.mMinParams;
+		}
 	}
 
 	for (param_start = omit_leading_whitespace(param_start + 1);;)
@@ -7266,6 +7345,41 @@ ResultType Script::DefineClass(LPTSTR aBuf)
 	class_object->SetBase(base_class); // May be NULL.
 
 	++mClassObjectCount;
+	return OK;
+}
+
+
+ResultType Script::DefineClassProperty(LPTSTR aBuf)
+{
+	LPTSTR name_end = find_identifier_end(aBuf);
+	if (*name_end == '.')
+		return ScriptError(ERR_INVALID_LINE_IN_CLASS_DEF, aBuf);
+
+	LPTSTR param_start = omit_leading_whitespace(name_end);
+	if (*param_start == '[')
+	{
+		LPTSTR param_end = aBuf + _tcslen(aBuf);
+		if (param_end[-1] != ']')
+			return ScriptError(ERR_MISSING_CLOSE_BRACKET, aBuf);
+		*param_start = '(';
+		param_end[-1] = ')';
+	}
+	else
+		param_start = _T("()");
+	
+	// Save the property name and parameter list for later use with DefineFunc().
+	mClassPropertyDef = tmalloc(_tcslen(aBuf) + 7); // +7 for ".Get()\0"
+	if (!mClassPropertyDef)
+		return ScriptError(ERR_OUTOFMEM);
+	_stprintf(mClassPropertyDef, _T("%.*s.Get%s"), int(name_end - aBuf), aBuf, param_start);
+
+	Object *class_object = mClassObject[mClassObjectCount - 1];
+	*name_end = 0; // Terminate for aBuf use below.
+	if (class_object->GetItem(ExprTokenType(), aBuf))
+		return ScriptError(ERR_DUPLICATE_DECLARATION, aBuf);
+	mClassProperty = new Property();
+	if (!mClassProperty || !class_object->SetItem(aBuf, mClassProperty))
+		return ScriptError(ERR_OUTOFMEM);
 	return OK;
 }
 
@@ -8244,11 +8358,19 @@ Func *Script::AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, bool aIsBuiltIn
 			ScriptError(_T("Invalid method name."), new_name); // Shouldn't ever happen.
 			return NULL;
 		}
-		if (!aClassObject->SetItem(key + 1, the_new_func))
+		if (mClassProperty)
 		{
-			ScriptError(ERR_OUTOFMEM);
-			return NULL;
+			if (toupper(key[1]) == 'G')
+				mClassProperty->mGet = the_new_func;
+			else
+				mClassProperty->mSet = the_new_func;
 		}
+		else
+			if (!aClassObject->SetItem(key + 1, the_new_func))
+			{
+				ScriptError(ERR_OUTOFMEM);
+				return NULL;
+			}
 		the_new_func->mClass = aClassObject;
 		// Also add it to the script's list of functions, to support #Warn LocalSameAsGlobal
 		// and automatic cleanup of objects in static vars on program exit.

source/script.h
@@ -197,7 +197,8 @@ enum CommandIDs {CONTROL_ID_FIRST = IDCANCEL + 1
 #define ERR_UNQUOTED_NON_ALNUM _T("Unquoted literals may only consist of alphanumeric characters/underscore.")
 #define ERR_DUPLICATE_DECLARATION _T("Duplicate declaration.")
 #define ERR_INVALID_CLASS_VAR _T("Invalid class variable declaration.")
-#define ERR_INVALID_LINE_IN_CLASS_DEF _T("Expected assignment or class/method definition.")
+#define ERR_INVALID_LINE_IN_CLASS_DEF _T("Not a valid method, class or property definition.")
+#define ERR_INVALID_LINE_IN_PROPERTY_DEF _T("Not a valid property getter/setter.")
 #define ERR_INVALID_GUI_NAME _T("Invalid Gui name.")
 #define ERR_INVALID_OPTION _T("Invalid option.") // Generic message used by Gui and GuiControl/Get.
 
@@ -2598,6 +2599,8 @@ private:
 	Object *mClassObject[MAX_NESTED_CLASSES]; // Class definition currently being parsed.
 	TCHAR mClassName[MAX_CLASS_NAME_LENGTH + 1]; // Only used during load-time.
 	Object *mUnresolvedClasses;
+	Property *mClassProperty;
+	LPTSTR mClassPropertyDef;
 
 	// These two track the file number and line number in that file of the line currently being loaded,
 	// which simplifies calls to ScriptError() and LineError() (reduces the number of params that must be passed).
@@ -2711,6 +2714,7 @@ public:
 
 	ResultType DefineClass(LPTSTR aBuf);
 	ResultType DefineClassVars(LPTSTR aBuf, bool aStatic);
+	ResultType DefineClassProperty(LPTSTR aBuf);
 	Object *FindClass(LPCTSTR aClassName, size_t aClassNameLength = 0);
 	ResultType ResolveClasses();
 

source/script_object.cpp
@@ -378,30 +378,66 @@ ResultType STDMETHODCALLTYPE Object::Invoke(
 		}
 	}
 	
+	bool findfield_after_calling_base;
 	int param_count_excluding_rvalue = aParamCount;
 
 	if (IS_INVOKE_SET)
 	{
-		// Prior validation of ObjSet() param count ensures the result won't be negative:
+		// Due to the way expression parsing works, the result should never be negative
+		// (and any direct callers of Invoke must always pass aParamCount >= 1):
 		--param_count_excluding_rvalue;
-		
-		if (IS_INVOKE_META)
-		{
-			if (param_count_excluding_rvalue == 1)
-				// Prevent below from searching for or setting a field, since this is a base object of aThisToken.
-				// Relies on mBase->Invoke recursion using aParamCount and not param_count_excluding_rvalue.
-				param_count_excluding_rvalue = 0;
-			//else: Allow SET to operate on a field of an object stored in the target's base.
-			//		For instance, x[y,z]:=w may operate on x[y][z], x.base[y][z], x[y].base[z], etc.
-		}
 	}
 	
 	if (param_count_excluding_rvalue)
 	{
 		field = FindField(*aParam[0], aResultToken.buf, /*out*/ key_type, /*out*/ key, /*out*/ insert_pos);
+		findfield_after_calling_base = true; // Set default.
+
+		// There used to be a check prior to the FindField() call above which avoided searching
+		// for base[field] when performing an assignment.  As an unintended side-effect, it was
+		// possible for base.base.__Set to be called even if base[field] exists, which is
+		// inconsistent with __Get and __Call.  That check was removed for v1.1.16 in order
+		// to implement property accessors, and a check was added below to retain the old
+		// behaviour for compatibility -- this should be changed in v2.
+		if (IS_INVOKE_META) // v1.1.16: See above.
+		{
+			// Handle class properties with getter/setter:
+			if (field && field->symbol == SYM_OBJECT && dynamic_cast<Property *>(field->object))
+			{
+				if (aParamCount > 2 && IS_INVOKE_SET)
+				{
+					// Do some shuffling to put value before the other parameters:
+					ExprTokenType *value = aParam[aParamCount - 1];
+					for (int i = aParamCount - 1; i > 1; --i)
+						aParam[i] = aParam[i - 1];
+					aParam[1] = value; // Corresponds to the setter's hidden "value" parameter.
+				}
+				ExprTokenType *name_token = aParam[0];
+				aParam[0] = &aThisToken; // For the hidden "this" parameter in the getter/setter.
+				// Pass IF_FUNCOBJ so that it'll pass all parameters to the getter/setter.
+				// For a functor Object, we would need to pass a token representing "this" Property,
+				// but since Property::Invoke doesn't use it, we pass our aThisToken for simplicity.
+				ResultType result = field->object->Invoke(aResultToken, aThisToken, aFlags | IF_FUNCOBJ, aParam, aParamCount);
+				aParam[0] = name_token;
+				if (result == EARLY_RETURN)
+					result = OK;
+				if (result != INVOKE_NOT_HANDLED)
+					return result;
+				// The property was missing get/set (whichever this invocation is), so continue as
+				// if the property itself wasn't defined.
+				field = NULL;
+				findfield_after_calling_base = false;
+			}
+			else if (IS_INVOKE_SET && param_count_excluding_rvalue == 1) // For compatibility - see above.
+			{
+				field = NULL;
+				param_count_excluding_rvalue = 0;
+			}
+		}
 	}
 	else
 	{
+		findfield_after_calling_base = false;
 		key_type = SYM_INVALID; // Allow key_type checks below without requiring that param_count_excluding_rvalue also be checked.
 		field = NULL;
 	}
@@ -424,7 +460,7 @@ ResultType STDMETHODCALLTYPE Object::Invoke(
 			// Since the above may have inserted or removed fields (including the specified one),
 			// insert_pos may no longer be correct or safe.  Updating field also allows a meta-function
 			// to initialize a field and allow processing to continue as if it already existed.
-			if (param_count_excluding_rvalue)
+			if (findfield_after_calling_base)
 				field = FindField(key_type, key, /*out*/ insert_pos);
 		}
 
@@ -1493,6 +1529,69 @@ Object::FieldType *Object::Insert(SymbolType key_type, KeyType key, IndexType at
 	field.symbol = SYM_OPERAND;
 
 	return &field;
+}
+
+
+//
+// Property: Invoked when a derived object gets/sets the corresponding key.
+//
+
+ResultType STDMETHODCALLTYPE Property::Invoke(ExprTokenType &aResultToken, ExprTokenType &aThisToken, int aFlags, ExprTokenType *aParam[], int aParamCount)
+{
+	Func **member;
+
+	if (aFlags & IF_FUNCOBJ)
+	{
+		// Use mGet even if IS_INVOKE_CALL, for symmetry:
+		//   obj.prop()         ; Get
+		//   obj.prop() := val  ; Set (translation is performed by the preparser).
+		member = IS_INVOKE_SET ? &mSet : &mGet;
+	}
+	else
+	{
+		if (!aParamCount)
+			return INVOKE_NOT_HANDLED;
+
+		LPTSTR name = TokenToString(*aParam[0]);
+		
+		if (!_tcsicmp(name, _T("Get")))
+		{
+			member = &mGet;
+		}
+		else if (!_tcsicmp(name, _T("Set")))
+		{
+			member = &mSet;
+		}
+		else
+			return INVOKE_NOT_HANDLED;
+		// ALL CODE PATHS ABOVE MUST RETURN OR SET member.
+
+		if (!IS_INVOKE_CALL)
+		{
+			if (IS_INVOKE_SET)
+			{
+				if (aParamCount != 2)
+					return OK;
+				// Allow changing the GET/SET function, since it's simple and seems harmless.
+				*member = TokenToFunc(*aParam[1]); // Can be NULL.
+				--aParamCount;
+			}
+			if (*member && aParamCount == 1)
+			{
+				aResultToken.symbol = SYM_OBJECT;
+				aResultToken.object = *member;
+			}
+			return OK;
+		}
+		// Since above did not return, we're explicitly calling Get or Set.
+		++aParam;      // Omit method name.
+		--aParamCount; // 
+	}
+	// Since above did not return, member must have been set to either &mGet or &mSet.
+	// However, their actual values might be NULL:
+	if (!*member)
+		return INVOKE_NOT_HANDLED;
+	return CallFunc(**member, aResultToken, aParam, aParamCount);
 }
 
 

source/script_object.h
@@ -296,6 +296,21 @@ public:
 
 
 //
+// Property: Invoked when a derived object gets/sets the corresponding key.
+//
+
+class Property : public ObjectBase
+{
+public:
+	Func *mGet, *mSet;
+
+	Property() : mGet(NULL), mSet(NULL) { }
+	
+	ResultType STDMETHODCALLTYPE Invoke(ExprTokenType &aResultToken, ExprTokenType &aThisToken, int aFlags, ExprTokenType *aParam[], int aParamCount);
+};
+
+
+//
 // MetaObject:	Used only by g_MetaObject (not every meta-object); see comments below.
 //
 class MetaObject : public Object

commit 4021eb3a1376d13ef6efac1d9d9f6f0c999fd436

Fixed parser to allow #include inside a class (broken by a6ea27f).

source/script.cpp
@@ -2650,7 +2650,7 @@ continue_main_loop: // This method is used in lieu of "continue" for performance
 		mCombinedLineNumber = saved_line_number;
 	}
 
-	if (mClassObjectCount) // or mClassProperty, which implies mClassObjectCount != 0.
+	if (mClassObjectCount && !source_file_index) // or mClassProperty, which implies mClassObjectCount != 0.
 	{
 		// A class definition has not been closed with "}".  Previously this was detected by adding
 		// the open and close braces as lines, but this way is simpler and has less overhead.