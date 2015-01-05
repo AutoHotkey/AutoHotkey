AutoHotkey/AutoHotkey

commit d4b0474555ed067aef17a2e05839af39af0b19da

Fixed the Hotkey command to update the ~ and $ status of the hotkey.
- v1.1.15 allowed ~ to be added or removed, but not if Label is omitted.
- $ was applied to all variants, but only when creating a new variant.

source/hotkey.cpp
@@ -1045,6 +1045,20 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aLabelName, LPTSTR aOption
 				// their interdependencies, will be re-initialized correctly.
 				update_all_hotkeys = true;
 			}
+			
+			// v1.1.15: Allow the ~tilde prefix to be added/removed from an existing hotkey variant.
+			// v1.1.18.01: Apply this change even if aJumpToLabel is omitted.
+			if (variant->mNoSuppress = suffix_has_tilde)
+				hk->mNoSuppress |= AT_LEAST_ONE_VARIANT_HAS_TILDE;
+			else
+				hk->mNoSuppress |= AT_LEAST_ONE_VARIANT_LACKS_TILDE;
+			// v1.1.18.01: Allow the $UseHook prefix to be added to an existing hotkey.
+			if (!hk->mKeybdHookMandatory && (variant->mNoSuppress || hook_is_mandatory))
+			{
+				update_all_hotkeys = true; // Since it may be switching from reg to k-hook.
+				hk->mKeybdHookMandatory = true; // See Hotkey::AddVariant() for comments.
+			}
+
 			// If the above changed the action from an Alt-tab type to non-alt-tab, there may be a label present
 			// to be applied to the existing variant (or created as a new variant).
 			if (aJumpToLabel) // COMMAND (update hotkey): Hotkey, Name, LabelName [, Options]
@@ -1069,18 +1083,6 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aLabelName, LPTSTR aOption
 						// never change; it will always contain the true name of this hotkey, namely its
 						// keystroke+modifiers (e.g. ^!c).
 					}
-					// v1.1.15: Allow the ~tilde prefix to be added/removed from an existing hotkey variant.
-					if (variant->mNoSuppress = suffix_has_tilde)
-					{
-						hk->mNoSuppress |= AT_LEAST_ONE_VARIANT_HAS_TILDE;
-						if (!hk->mKeybdHookMandatory)
-						{
-							update_all_hotkeys = true; // Since it may be switching from reg to k-hook.
-							hk->mKeybdHookMandatory = true; // See Hotkey::AddVariant() for comments.
-						}
-					}
-					else
-						hk->mNoSuppress |= AT_LEAST_ONE_VARIANT_LACKS_TILDE;
 				}
 				else // No existing variant matching current #IfWin criteria, so create a new variant.
 				{

commit a17fb4012a94c15309e755d1ba157a809ffadb14

Fixed Hotkey command unable to create new variants (broken by d4b04745).

source/hotkey.cpp
@@ -1053,19 +1053,6 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aLabelName, LPTSTR aOption
 				update_all_hotkeys = true;
 			}
 			
-			// v1.1.15: Allow the ~tilde prefix to be added/removed from an existing hotkey variant.
-			// v1.1.19: Apply this change even if aJumpToLabel is omitted.
-			if (variant->mNoSuppress = suffix_has_tilde)
-				hk->mNoSuppress |= AT_LEAST_ONE_VARIANT_HAS_TILDE;
-			else
-				hk->mNoSuppress |= AT_LEAST_ONE_VARIANT_LACKS_TILDE;
-			// v1.1.19: Allow the $UseHook prefix to be added to an existing hotkey.
-			if (!hk->mKeybdHookMandatory && (variant->mNoSuppress || hook_is_mandatory))
-			{
-				update_all_hotkeys = true; // Since it may be switching from reg to k-hook.
-				hk->mKeybdHookMandatory = true; // See Hotkey::AddVariant() for comments.
-			}
-
 			// If the above changed the action from an Alt-tab type to non-alt-tab, there may be a label present
 			// to be applied to the existing variant (or created as a new variant).
 			if (aJumpToLabel) // COMMAND (update hotkey): Hotkey, Name, LabelName [, Options]
@@ -1097,22 +1084,43 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aLabelName, LPTSTR aOption
 						RETURN_HOTKEY_ERROR(HOTKEY_EL_MEM, ERR_OUTOFMEM, aHotkeyName);
 					variant_was_just_created = true;
 					update_all_hotkeys = true;
-					if (hook_is_mandatory || (!g_os.IsWin9x() && g_ForceKeybdHook))
-					{
-						// Require the hook for all variants of this hotkey if any variant requires it.
-						// This seems more intuitive than the old behaviour, which required $ or #UseHook
-						// to be used on the *first* variant, even though it affected all variants.
-#ifdef CONFIG_WIN9X
-						if (g_os.IsWin9x())
-							hk->mUnregisterDuringThread = true;
-						else
-#endif
-							hk->mKeybdHookMandatory = true;
-					}
+					// It seems undesirable for #UseHook to be applied to a hotkey just because it's options
+					// were updated with the Hotkey command; therefore, #UseHook is only applied for newly
+					// created variants such as this one.  For others, the $ prefix can be applied.
+					if (g_ForceKeybdHook && !g_os.IsWin9x())
+						hook_is_mandatory = true;
 				}
 			}
-			//else NULL label, so either it just became an alt-tab hotkey above, or it's "Hotkey, Name,, Options".
-			// Either way, continue on and let the error-catch below report it if it qualifies as an error.
+			else
+				// NULL label, so either it just became an alt-tab hotkey above, or it's "Hotkey, Name,, Options".
+				if (!variant) // Below relies on this check.
+					break; // Let the error-catch below report it as an error.
+#ifdef CONFIG_WIN9X
+			if (g_os.IsWin9x())
+			{
+				if (hook_is_mandatory)
+					hk->mUnregisterDuringThread = true;
+				// Skip the checks below, since the tilde prefix and #UseHook are ignored on Win9x.
+				break;
+			}
+#endif
+			// v1.1.15: Allow the ~tilde prefix to be added/removed from an existing hotkey variant.
+			// v1.1.19: Apply this change even if aJumpToLabel is omitted.  This is redundant if
+			// variant_was_just_created, but checking that condition seems counter-productive.
+			if (variant->mNoSuppress = suffix_has_tilde)
+				hk->mNoSuppress |= AT_LEAST_ONE_VARIANT_HAS_TILDE;
+			else
+				hk->mNoSuppress |= AT_LEAST_ONE_VARIANT_LACKS_TILDE;
+				
+			// v1.1.19: Allow the $UseHook prefix to be added to an existing hotkey.
+			if (!hk->mKeybdHookMandatory && (hook_is_mandatory || suffix_has_tilde))
+			{
+				// Require the hook for all variants of this hotkey if any variant requires it.
+				// This seems more intuitive than the old behaviour, which required $ or #UseHook
+				// to be used on the *first* variant, even though it affected all variants.
+				update_all_hotkeys = true; // Since it may be switching from reg to k-hook.
+				hk->mKeybdHookMandatory = true;
+			}
 		} // Hotkey already existed.
 		break;
 	} // switch(hook_action)