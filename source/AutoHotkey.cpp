AutoHotkey/AutoHotkey

commit 9c38f81a674318b351258716acc6dc3ac85c74a8

Fixed /iLib switch to write the file even if there's a syntax error.

source/AutoHotkey.cpp
@@ -180,7 +180,11 @@ int WINAPI _tWinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmd
 	UINT load_result = g_script.LoadFromFile(script_filespec == NULL);
 #endif
 	if (load_result == LOADING_FAILED) // Error during load (was already displayed by the function call).
+	{
+		if (g_script.mIncludeLibraryFunctionsThenExit)
+			g_script.mIncludeLibraryFunctionsThenExit->Close(); // Flush its buffer to disk.
 		return CRITICAL_ERROR;  // Should return this value because PostQuitMessage() also uses it.
+	}
 	if (!load_result) // LoadFromFile() relies upon us to do this check.  No script was loaded or we're in /iLib mode, so nothing more to do.
 		return 0;
 

commit 69ae7fd01d02cc567e4860c9fe6409082831eddb

Fixed AutoHotkeySC build errors caused by commit 9c38f81a67.

source/AutoHotkey.cpp
@@ -181,8 +181,10 @@ int WINAPI _tWinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmd
 #endif
 	if (load_result == LOADING_FAILED) // Error during load (was already displayed by the function call).
 	{
+#ifndef AUTOHOTKEYSC
 		if (g_script.mIncludeLibraryFunctionsThenExit)
 			g_script.mIncludeLibraryFunctionsThenExit->Close(); // Flush its buffer to disk.
+#endif
 		return CRITICAL_ERROR;  // Should return this value because PostQuitMessage() also uses it.
 	}
 	if (!load_result) // LoadFromFile() relies upon us to do this check.  No script was loaded or we're in /iLib mode, so nothing more to do.

source/script.cpp
@@ -2430,7 +2430,9 @@ examine_line:
 						// This hotkey uses a single-character key name, which could be valid on some other
 						// keyboard layout.  Allow the script to start, but warn the user about the problem.
 						// Note that this hotkey's label is still valid even though the hotkey wasn't created.
+#ifndef AUTOHOTKEYSC
 						if (!mIncludeLibraryFunctionsThenExit) // Current keyboard layout is not relevant in /iLib mode.
+#endif
 						{
 							sntprintf(msg_text, _countof(msg_text), _T("Note: The hotkey %s will not be active because it does not exist in the current keyboard layout."), buf);
 							MsgBox(msg_text);