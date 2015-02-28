e1cd5e9b00ac069e11f30677cc84c14305b11a3c
Lexikos <unravelin@gmail.com>
2015-02-28 13:31:30 +1000

    Added ExprTokenType::SetValue()

--- source/defines.h
+++ source/defines.h
@@ -294,6 +294,33 @@ struct ExprTokenType  // Something in the compiler hates the name TokenType, so 
 	// The above two probably need to be adjacent to each other to conserve memory due to 8-byte alignment,
 	// which is the default alignment (for performance reasons) in any struct that contains 8-byte members
 	// such as double and __int64.
+	
+	void SetValue(__int64 aValue)
+	{
+		symbol = SYM_INTEGER;
+		value_int64 = aValue;
+	}
+	void SetValue(int aValue) { SetValue((__int64)aValue); }
+	void SetValue(UINT aValue) { SetValue((__int64)aValue); }
+	void SetValue(UINT64 aValue) { SetValue((__int64)aValue); }
+	void SetValue(double aValue)
+	{
+		symbol = SYM_FLOAT;
+		value_double = aValue;
+	}
+	void SetValue(LPTSTR aValue)
+	{
+		ASSERT(aValue);
+		symbol = SYM_STRING;
+		marker = aValue;
+	}
+	void SetValue(IObject *aValue)
+	// Caller must AddRef() if appropriate.
+	{
+		ASSERT(aValue);
+		symbol = SYM_OBJECT;
+		object = aValue;
+	}
 };
 #define MAX_TOKENS 512 // Max number of operators/operands.  Seems enough to handle anything realistic, while conserving call-stack space.
 #define STACK_PUSH(token_ptr) stack[stack_count++] = token_ptr
