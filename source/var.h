f14e4c8a3239630c592f89e0d1ff3f3a4c45cb00
Lexikos <unravelin@gmail.com>
2015-05-23 11:34:19 +1000

    Fixed var return optimization (unspecified order of evaluation).

--- source/var.h
+++ source/var.h
@@ -962,7 +962,8 @@ public:
 inline void ResultToken::StealMem(Var *aVar)
 // Caller must ensure that aVar->mType == VAR_NORMAL and aVar->mHowAllocated == ALLOC_MALLOC.
 {
-	AcceptMem(aVar->StealMem(), aVar->Length());
+	VarSizeType length = aVar->Length(); // Must not be combined with the line below, as the compiler is free to evaluate parameters in whatever order.
+	AcceptMem(aVar->StealMem(), length);
 }
 
 #endif
