3c60e852a5fdfa0ddcf2f6573451830e4c8da443
HotKeyIt <bios2000@web.de>
2015-10-04 19:54:38 +0200

    Remove comments

--- source/script_gui.cpp
+++ source/script_gui.cpp
@@ -8048,19 +8048,17 @@ LRESULT CALLBACK GuiWindowProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lPara
 				if (aControl.mAX)
 				{
 					x = aControl.mX + addedWidth + (int)(aControl.mAX * addWidth) - (pgui->mStyle & WS_HSCROLL ? pgui->mHScroll->nPos : 0);
-					// Backup previous added Width
 					addedWidth += (int)(aControl.mAX * addWidth);
 				}
 				if (aControl.mAY) {
 					y = aControl.mY + addedHeight + (int)(aControl.mAY * addHeight) - (pgui->mStyle & WS_VSCROLL ? pgui->mVScroll->nPos : 0);
-					// Backup previous added Height
 					addedHeight += (int)(aControl.mAY * addHeight);
 				}
 				if (aControl.mAWidth)
 					width = aControl.mWidth + (int)(aControl.mAWidth * addWidth);
 				if (aControl.mAHeight)
 					height = aControl.mHeight + (int)(aControl.mAHeight * addHeight);
-				
+
 				// Continue if size of control was not changed
 				if (x == pt.x && y == pt.y && width == rect.right - rect.left && height == rect.bottom - rect.top)
 					continue;
