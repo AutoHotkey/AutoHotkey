cb72b975cdc582d511ce0d135ef0a9efa7f844d0
Lexikos <unravelin@gmail.com>
2017-06-24 09:21:24 +1000

    Fixed Exception(,-n) to only count user-defined functions/subs as in v1.

--- source/script2.cpp
+++ source/script2.cpp
@@ -16409,21 +16409,23 @@ BIF_DECL(BIF_Exception)
 	{
 #ifdef CONFIG_DEBUGGER
 		int offset = TokenIsNumeric(*aParam[1]) ? ParamIndexToInt(1) : 0;
-		if (offset < 0 && offset >= (g_Debugger.mStack.mBottom - g_Debugger.mStack.mTop)) // (mBottom - mTop) is safe against overflow, unlike (se >= mBottom). 
+		DbgStack::Entry *se = g_Debugger.mStack.mTop;
+		while (--se >= g_Debugger.mStack.mBottom)
 		{
-			DbgStack::Entry *se = g_Debugger.mStack.mTop + offset;
-			// Self-contained loop to ensure the entry belongs to the current thread
-			// (below also relies on this loop to verify se[1].type != SE_Thread):
-			while (++offset <= 0 && g_Debugger.mStack.mTop[offset].type != DbgStack::SE_Thread); // Relies on short-circuit evaluation.
-			if (offset == 1)
+			if (se->type == DbgStack::SE_Thread)
+				break; // Never return stack locations in other threads.
+			if (se->type == DbgStack::SE_Func && se->func->mIsBuiltIn)
+				continue; // Skip built-in functions such as Op_ObjInvoke (common).
+			if (++offset == 0)
 			{
-				line = se->line;
+				line = se > g_Debugger.mStack.mBottom ? se[-1].line : se->line;
 				// se->line contains the line at the given offset from the top of the stack.
 				// Rather than returning the name of the function or sub which contains that
 				// line, return the name of the function or sub which that line called.
 				// In other words, an offset of -1 gives the name of the current function and
 				// the file and number of the line which it was called from.
-				what = se[1].type == DbgStack::SE_Func ? se[1].func->mName : se[1].sub->mName;
+				what = se->type == DbgStack::SE_Func ? se->func->mName : se->sub->mName;
+				break;
 			}
 		}
 #endif
