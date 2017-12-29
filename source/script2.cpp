AutoHotkey/AutoHotkey

commit a25d6a32977004b81fe4f9d6303b148c2dc4ef49

Changed Input to produce upper-case EndKey: values for A-Z.

source/script2.cpp
@@ -1767,8 +1767,15 @@ ResultType Line::Input()
 			*(key_name + 7 + count) = '\0';  // Terminate the string.
 		}
 		else
+		{
 			g_input.EndedBySC ? SCtoKeyName(g_input.EndingSC, key_name + 7, _countof(key_name) - 7)
 				: VKtoKeyName(g_input.EndingVK, key_name + 7, _countof(key_name) - 7);
+			// For partial backward-compatibility, keys A-Z are upper-cased when handled by VK,
+			// but only if they actually correspond to those characters.  If this wasn't done,
+			// the character would always be lowercase since the shift state is not considered.
+			if (key_name[7] >= 'a' && key_name[7] <= 'z')
+				key_name[7] -= 32;
+		}
 		g_ErrorLevel->Assign(key_name);
 		break;
 	}

commit b0eef81ae429210777d6bea017245e0e205d8eb1

Fixed Input to set EndKey:scXXX/vkXX, not EndKey:ScXXX/VkXX.
Caused by commit a25d6a3 (v1.1.20).

source/script2.cpp
@@ -1832,7 +1832,7 @@ ResultType Line::Input()
 			// For partial backward-compatibility, keys A-Z are upper-cased when handled by VK,
 			// but only if they actually correspond to those characters.  If this wasn't done,
 			// the character would always be lowercase since the shift state is not considered.
-			if (key_name[7] >= 'a' && key_name[7] <= 'z')
+			if (key_name[7] >= 'a' && key_name[7] <= 'z' && !key_name[8])
 				key_name[7] -= 32;
 		}
 		g_ErrorLevel->Assign(key_name);