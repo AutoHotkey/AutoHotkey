AutoHotkey/AutoHotkey

commit 4ac933ad8ffa61f4d868c50388d46fc93f4c7662

Fixed Gui with +Parent to center within the parent GUI vs. the screen.

source/script_gui.cpp
@@ -6840,7 +6840,10 @@ ResultType GuiType::Show(LPTSTR aOptions, LPTSTR aText)
 		height = rect.bottom - rect.top; // rect.top might be slightly less than zero. A status bar is properly handled since it's inside the window's client area.
 
 		RECT work_rect;
-		SystemParametersInfo(SPI_GETWORKAREA, 0, &work_rect, 0);  // Get desktop rect excluding task bar.
+		if (mOwner && (mStyle & WS_CHILD))
+			GetClientRect(mOwner, &work_rect); // Center within parent window (our position is set relative to mOwner's client area, not in screen coordinates).
+		else
+			SystemParametersInfo(SPI_GETWORKAREA, 0, &work_rect, 0);  // Get desktop rect excluding task bar.
 		int work_width = work_rect.right - work_rect.left;  // Note that "left" won't be zero if task bar is on left!
 		int work_height = work_rect.bottom - work_rect.top; // Note that "top" won't be zero if task bar is on top!
 
@@ -6857,12 +6860,8 @@ ResultType GuiType::Show(LPTSTR aOptions, LPTSTR aText)
 
 		if (x == COORD_CENTERED || y == COORD_CENTERED) // Center it, based on its dimensions determined above.
 		{
-			// This does not currently handle multi-monitor systems explicitly, since those calculations
-			// require API functions that don't exist in Win95/NT (and thus would have to be loaded
-			// dynamically to allow the program to launch).  Therefore, windows will likely wind up
-			// being centered across the total dimensions of all monitors, which usually results in
-			// half being on one monitor and half in the other.  This doesn't seem too terrible and
-			// might even be what the user wants in some cases (i.e. for really big windows).
+			// This does not handle multi-monitor systems explicitly, and has no need to do so since
+			// SPI_GETWORKAREA "Retrieves the size of the work area on the primary display monitor".
 			if (x == COORD_CENTERED)
 				x = work_rect.left + ((work_width - width) / 2);
 			if (y == COORD_CENTERED)

commit 5d3e745b6319ab86a0accf12ed1799fa30e63120

Fixed default size of Gui with +Parent to not be restricted by parent.
Broken by 4ac933a.

source/script_gui.cpp
@@ -6840,7 +6840,8 @@ ResultType GuiType::Show(LPTSTR aOptions, LPTSTR aText)
 		height = rect.bottom - rect.top; // rect.top might be slightly less than zero. A status bar is properly handled since it's inside the window's client area.
 
 		RECT work_rect;
-		if (mOwner && (mStyle & WS_CHILD))
+		bool is_child_window = mOwner && (style & WS_CHILD);
+		if (is_child_window)
 			GetClientRect(mOwner, &work_rect); // Center within parent window (our position is set relative to mOwner's client area, not in screen coordinates).
 		else
 			SystemParametersInfo(SPI_GETWORKAREA, 0, &work_rect, 0);  // Get desktop rect excluding task bar.
@@ -6850,7 +6851,7 @@ ResultType GuiType::Show(LPTSTR aOptions, LPTSTR aText)
 		// Seems best to restrict window size to the size of the desktop whenever explicit sizes
 		// weren't given, since most users would probably want that.  But only on first use of
 		// "Gui Show" (even "Gui, Show, Hide"):
-		if (mGuiShowHasNeverBeenDone)
+		if (mGuiShowHasNeverBeenDone && !is_child_window)
 		{
 			if (width_orig == COORD_UNSPECIFIED && width > work_width)
 				width = work_width;