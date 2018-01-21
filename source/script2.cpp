AutoHotkey/AutoHotkey

commit c05ab55cd945a2d7e10b07b3beee2e6d388c3376

Replace TrayMenu() with A_TrayMenu

source/script2.cpp
@@ -9466,6 +9466,17 @@ VarSizeType BIV_ScreenDPI(LPTSTR aBuf, LPTSTR aVarName)
 
 
 
+BIV_DECL_R(BIV_TrayMenu)
+{
+	// The actual value (an object) is resolved in ExpandExpression,
+	// to work around the limitations of the current BIV interface.
+	if (aBuf)
+		*aBuf = '\0';
+	return 0;
+}
+
+
+
 BIV_DECL_R(BIV_AllowMainWindow)
 {
 	if (aBuf)
@@ -14971,10 +14982,6 @@ BIF_DECL(BIF_Menu)
 		break;
 	case FID_MenuFromHandle:
 		menu = g_script.FindMenu((HMENU)ParamIndexToInt64(0));
-		break;
-	case FID_TrayMenu:
-		menu = g_script.mTrayMenu;
-		menu->AddRef();
 		break;
 	}
 	if (menu)

source/script.cpp
@@ -279,7 +279,6 @@ FuncEntry g_BIF[] =
 
 	BIFn(MenuCreate, 0, 0, true, BIF_Menu),
 	BIFn(MenuFromHandle, 1, 1, true, BIF_Menu),
-	BIFn(TrayMenu, 0, 0, true, BIF_Menu),
 	BIF1(TraySetIcon, 0, 3, false),
 
 	BIF1(LoadPicture, 1, 3, true),
@@ -437,6 +436,7 @@ VarEntry g_BIV_A[] =
 	A_(TimeSinceThisHotkey),
 	A_w(TitleMatchMode),
 	A_wx(TitleMatchModeSpeed, BIV_TitleMatchModeSpeed, BIV_TitleMatchMode_Set),
+	A_(TrayMenu),
 	A_x(UserName, BIV_UserName_ComputerName),
 	A_x(WDay, BIV_DateTime),
 	A_wx(WinDelay, BIV_xDelay, BIV_xDelay_Set),
@@ -9445,7 +9445,8 @@ end_of_infix_to_postfix:
 						return LineError(ERR_VAR_IS_READONLY, FAIL, aArg.text);
 				}
 				else if ( !(only_token.var->mType == VAR_VIRTUAL
-					&& (only_token.var->mVV->Get == BIV_LoopIndex || only_token.var->mVV->Get == BIV_EventInfo)) )
+					&& (only_token.var->mVV->Get == BIV_LoopIndex || only_token.var->mVV->Get == BIV_EventInfo))
+					&& !(only_token.var->mType == VAR_BUILTIN && only_token.var->mBIV == BIV_TrayMenu) ) // This exception is required for A_TrayMenu to return an object.
 				{
 					aArg.type = ARG_TYPE_INPUT_VAR;
 				}

source/script.h
@@ -567,7 +567,7 @@ enum BuiltInFunctionID {
 	FID_ProcessExist = 0, FID_ProcessClose, FID_ProcessWait, FID_ProcessWaitClose, 
 	FID_MonitorGet = 0, FID_MonitorGetWorkArea, FID_MonitorGetCount, FID_MonitorGetPrimary, FID_MonitorGetName, 
 	FID_OnExit = 0, FID_OnClipboardChange,
-	FID_MenuCreate = 0, FID_MenuFromHandle, FID_TrayMenu,
+	FID_MenuCreate = 0, FID_MenuFromHandle,
 	FID_ControlGetChecked = 0, FID_ControlGetEnabled, FID_ControlGetVisible, FID_ControlGetTab, FID_ControlFindItem, FID_ControlGetChoice, FID_ControlGetList, FID_ControlGetLineCount, FID_ControlGetCurrentLine, FID_ControlGetCurrentCol, FID_ControlGetLine, FID_ControlGetSelected, FID_ControlGetStyle, FID_ControlGetExStyle, FID_ControlGetHwnd,
 	FID_ControlSetChecked = 0, FID_ControlSetEnabled, FID_ControlShow, FID_ControlHide, FID_ControlSetStyle, FID_ControlSetExStyle, FID_ControlShowDropDown, FID_ControlHideDropDown, FID_ControlSetTab, FID_ControlAddItem, FID_ControlDeleteItem, FID_ControlChoose, FID_ControlChooseString, FID_ControlEditPaste,
 	FID_DriveEject = 0, FID_DriveLock, FID_DriveUnlock, FID_DriveSetLabel,
@@ -2885,6 +2885,7 @@ BIV_DECL_RW(BIV_FileEncoding);
 BIV_DECL_RW(BIV_RegView);
 BIV_DECL_RW(BIV_LastError);
 BIV_DECL_RW(BIV_AllowMainWindow);
+BIV_DECL_R (BIV_TrayMenu);
 BIV_DECL_RW(BIV_IconHidden);
 BIV_DECL_RW(BIV_IconTip);
 BIV_DECL_R (BIV_IconFile);

source/script_expression.cpp
@@ -241,6 +241,11 @@ LPTSTR Line::ExpandExpression(int aArgIndex, ResultType &aResult, ResultToken *a
 						LineError(ERR_VAR_IS_READONLY, FAIL, this_token.var->mName);
 						goto abort;
 					}
+					if (this_token.var->mBIV == BIV_TrayMenu) // Handled here to work around limitations of the BIV interface.
+					{
+						this_token.SetValue(g_script.mTrayMenu);
+						goto push_this_token;
+					}
   				}
 				// Otherwise, it's a built-in variable.
 				result_size = this_token.var->Get() + 1;

commit de138b2c23a3fc90ca256eec2ebee9c766dd460a

Fixed A_IsCompiled in release builds [broken by c05ab55].

source/script2.cpp
@@ -9498,11 +9498,15 @@ VarSizeType BIV_ScreenDPI(LPTSTR aBuf, LPTSTR aVarName)
 
 BIV_DECL_R(BIV_TrayMenu)
 {
-	// The actual value (an object) is resolved in ExpandExpression,
-	// to work around the limitations of the current BIV interface.
+	// The actual value (an object) is resolved in ExpandExpression, to work around the
+	// limitations of the current BIV interface.  The current implementation requires that
+	// this function has a unique address, which would not be the case if COMDAT folding
+	// caused it to be "folded" with an identical function (such as A_IsCompiled, which
+	// also just returns an empty string ifndef AUTOHOTKEYSC).  So to ensure it gets a
+	// unique address, we check aVarName.
 	if (aBuf)
 		*aBuf = '\0';
-	return 0;
+	return !_tcsicmp(aVarName, _T("A_TrayMenu")) ? 0 : 1;
 }
 
 