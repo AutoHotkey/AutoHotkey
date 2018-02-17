AutoHotkey/AutoHotkey

commit e4a5493749362b11c83ff71a9695782e72f82c65

Removed #IfWin and optimized #If Win().

source/hotkey.cpp
@@ -66,16 +66,23 @@ HWND HotCriterionAllowsFiring(HotkeyCriterion *aCriterion, LPTSTR aHotkeyName)
 
 
 ResultType SetHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText)
-// Allocate memory for aWinTitle/Text (if necessary) and update g->HotWinTitle/Text to point to it.
 // Returns FAIL if memory couldn't be allocated, or OK otherwise.
 // This is a global function because it's used by both hotkeys and hotstrings.
 {
-	// Caller relies on this check:
-	if (!(*aWinTitle || *aWinText)) // In case of something weird but legit like: #IfWinActive, ,
-	{
-		g->HotCriterion = NULL;
-		return OK;
-	}
+	HotkeyCriterion *cp;
+	if (  !(cp = FindHotkeyCriterion(aType, aWinTitle, aWinText))
+		&& !(cp = AddHotkeyCriterion(aType, aWinTitle, aWinText))  )
+		return FAIL;
+	g->HotCriterion = cp;
+	return OK;
+}
+
+
+HotkeyCriterion *FindHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText)
+{
+	// Caller may rely on this check:
+	if (!(*aWinTitle || *aWinText))
+		return NULL;
 
 	// Storing combinations of WinTitle+WinText doesn't have as good a best-case memory savings as
 	// have a separate linked list for Title vs. Text.  But it does save code size, and in the vast
@@ -83,37 +90,40 @@ ResultType SetHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR a
 	HotkeyCriterion *cp;
 	for (cp = g_FirstHotCriterion; cp; cp = cp->NextCriterion)
 		if (cp->Type == aType && !_tcscmp(cp->WinTitle, aWinTitle) && !_tcscmp(cp->WinText, aWinText)) // Case insensitive.
-		{
-			// Match found, so point to the existing memory.
-			g->HotCriterion = cp;
-			return OK;
-		}
+			return cp;
 
-	// Since above didn't return, there is no existing entry in the linked list that matches this exact
-	// combination of Title and Text.  So create a new item and allocate memory for it.
+	return NULL;
+}
+
+
+HotkeyCriterion *AddHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText)
+{
+	HotkeyCriterion *cp;
 	if (   !(cp = (HotkeyCriterion *)SimpleHeap::Malloc(sizeof(HotkeyCriterion)))   )
-		return FAIL;
+		return NULL;
 	cp->Type = aType;
 	cp->ExprLine = NULL;
-	cp->NextCriterion = NULL;
 	if (*aWinTitle)
 	{
 		if (   !(cp->WinTitle = SimpleHeap::Malloc(aWinTitle))   )
-			return FAIL;
+			return NULL;
 	}
 	else
 		cp->WinTitle = _T("");
 	if (*aWinText)
 	{
 		if (   !(cp->WinText = SimpleHeap::Malloc(aWinText))   )
-			return FAIL;
+			return NULL;
 	}
 	else
 		cp->WinText = _T("");
+	return AddHotkeyCriterion(cp);
+}
 
-	g->HotCriterion = cp;
 
-	// Update the linked list:
+HotkeyCriterion *AddHotkeyCriterion(HotkeyCriterion *cp)
+{
+	cp->NextCriterion = NULL;
 	if (!g_FirstHotCriterion)
 		g_FirstHotCriterion = g_LastHotCriterion = cp;
 	else
@@ -122,8 +132,7 @@ ResultType SetHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR a
 		// This must be done after the above:
 		g_LastHotCriterion = cp;
 	}
-
-	return OK;
+	return cp;
 }
 
 
@@ -132,9 +141,9 @@ HotkeyCriterion *AddHotkeyIfExpr()
 	HotkeyCriterion *cp;
 	if (   !(cp = (HotkeyCriterion *)SimpleHeap::Malloc(sizeof(HotkeyCriterion)))   )
 		return NULL;
-	cp->NextCriterion = NULL;
+	cp->NextExpr = NULL;
 	if (g_LastHotExpr)
-		g_LastHotExpr->NextCriterion = cp;
+		g_LastHotExpr->NextExpr = cp;
 	else
 		g_FirstHotExpr = cp;
 	g_LastHotExpr = cp;
@@ -144,10 +153,50 @@ HotkeyCriterion *AddHotkeyIfExpr()
 
 HotkeyCriterion *FindHotkeyIfExpr(LPTSTR aExpr)
 {
-	for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextCriterion)
-		if (!_tcscmp(aExpr, cp->WinTitle)) // Case-sensitive since the expression might be.
+	for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextExpr)
+		if (!_tcscmp(aExpr, cp->ExprLine->mArg[0].text)) // Case-sensitive since the expression might be.
 			return cp;
 	return NULL;
+}
+
+
+void Script::PreparseHotkeyIfExpr(Line *aLine)
+// Optimize simple #If expressions into the more specific HOT_IF_ types so that they can be
+// evaluated by the hook directly, without synchronizing with the main thread.
+{
+	ExprTokenType *postfix = aLine->mArg[0].postfix;
+	int param_count = 0;
+	while (postfix[param_count].symbol == SYM_STRING)
+		++param_count;
+	if (postfix[param_count].symbol != SYM_FUNC)
+		return; // Not a function call, or it doesn't only accept strings.
+	if (param_count > 2)
+		return; // Too many parameters.
+	Func &fn = *postfix[param_count].deref->func;
+	if (!fn.mIsBuiltIn || fn.mBIF != &BIF_WinExistActive)
+		return; // Not WinExist() or WinActive().
+	bool invert = postfix[param_count+1].symbol == SYM_LOWNOT || postfix[param_count+1].symbol == SYM_HIGHNOT;
+	if (postfix[param_count+1+invert].symbol != SYM_INVALID)
+		return; // There's more to the expression.
+	// Otherwise, it was a single call to WinExist() or WinActive() where each parameter
+	// was exactly one literal string and the result was optionally inverted with "not" or "!".
+	HotkeyCriterion *hc = (HotkeyCriterion *)aLine->mAttribute;
+	// Change the parameters of this criterion.  FindHotkeyIfExpr() will still be able to
+	// find it based on the expression text since it only relies on ExprLine.
+	if (ctoupper(fn.mName[3]) == 'A')
+		hc->Type = invert ? HOT_IF_NOT_ACTIVE : HOT_IF_ACTIVE;
+	else
+		hc->Type = invert ? HOT_IF_NOT_EXIST : HOT_IF_EXIST;
+	hc->WinTitle = param_count > 0 ? postfix[0].marker : _T("");
+	hc->WinText = param_count > 1 ? postfix[1].marker : _T("");
+	// The following adds a duplicate in the event that there are two different expressions
+	// which resolve to the same criterion, such as WinExist("x","") and WinExist("x", "").
+	// In that case, only the first criterion can be referenced by its WinTitle & WinText
+	// (but each can be referenced by its unique expression text).  This seems unavoidable
+	// since variants are only unique to a given expression, and trying to work around that
+	// here would cause inconsistency since this only applies to very specific expressions.
+	// At this stage, the only criterion in the list are those added by the following line:
+	AddHotkeyCriterion(hc);
 }
 
 

source/defines.h
@@ -799,7 +799,7 @@ struct HotkeyCriterion
 		Line *ExprLine;
 		IObject *Callback;
 	};
-	HotkeyCriterion *NextCriterion;
+	HotkeyCriterion *NextCriterion, *NextExpr;
 
 	ResultType Eval(LPTSTR aHotkeyName); // For HOT_IF_EXPR and HOT_IF_CALLBACK.
 };

source/hotkey.h
@@ -60,7 +60,10 @@ enum HotkeyTypeEnum {HK_NORMAL, HK_KEYBD_HOOK, HK_MOUSE_HOOK, HK_BOTH_HOOKS, HK_
 HWND HotCriterionAllowsFiring(HotkeyCriterion *aCriterion, LPTSTR aHotkeyName); // Used by hotkeys and hotstrings.
 bool HotInputLevelAllowsFiring(SendLevelType inputLevel, ULONG_PTR aEventExtraInfo, LPTSTR aKeyHistoryChar);
 ResultType SetHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText);
+HotkeyCriterion *AddHotkeyCriterion(HotkeyCriterion *aCriterion);
+HotkeyCriterion *AddHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText);
 HotkeyCriterion *AddHotkeyIfExpr();
+HotkeyCriterion *FindHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText);
 HotkeyCriterion *FindHotkeyIfExpr(LPTSTR aExpr);
 
 

source/script.cpp
@@ -3552,6 +3552,7 @@ inline ResultType Script::IsDirective(LPTSTR aBuf)
 		g->HotCriterion->ExprLine = hot_expr_line;
 		g->HotCriterion->WinTitle = hot_expr_line->mArg[0].text;
 		g->HotCriterion->WinText = _T("");
+		hot_expr_line->mAttribute = g->HotCriterion;
 		return CONDITION_TRUE;
 	}
 
@@ -3562,73 +3563,6 @@ inline ResultType Script::IsDirective(LPTSTR aBuf)
 			g_HotExprTimeout = ATOU(parameter);
 		return CONDITION_TRUE;
 	}
-
-	if (!_tcsnicmp(aBuf, _T("#IfWin"), 6))
-	{
-		HotCriterionType hot_criterion;
-		bool invert = !_tcsnicmp(aBuf + 6, _T("Not"), 3);
-		if (!_tcsnicmp(aBuf + (invert ? 9 : 6), _T("Active"), 6)) // It matches #IfWin[Not]Active.
-			hot_criterion = invert ? HOT_IF_NOT_ACTIVE : HOT_IF_ACTIVE;
-		else if (!_tcsnicmp(aBuf + (invert ? 9 : 6), _T("Exist"), 5))
-			hot_criterion = invert ? HOT_IF_NOT_EXIST : HOT_IF_EXIST;
-		else // It starts with #IfWin but isn't Active or Exist: Don't alter g->HotCriterion.
-			return CONDITION_FALSE; // Indicate unknown directive since there are currently no other possibilities.
-		if (!parameter) // The omission of the parameter indicates that any existing criteria should be turned off.
-		{
-			g->HotCriterion = NULL; // Indicate that no criteria are in effect for subsequent hotkeys.
-			return CONDITION_TRUE;
-		}
-		LPTSTR hot_win_title = parameter, hot_win_text; // Set default for title; text is determined later.
-		// Scan for the first non-escaped comma.  If there is one, it marks the second parameter: WinText.
-		LPTSTR cp, first_non_escaped_comma;
-		for (first_non_escaped_comma = NULL, cp = hot_win_title; ; ++cp)  // Increment to skip over the symbol just found by the inner for().
-		{
-			for (; *cp && !(*cp == g_EscapeChar || *cp == g_delimiter || *cp == g_DerefChar); ++cp);  // Find the next escape char, comma, or %.
-			if (!*cp) // End of string was found.
-				break;
-#define ERR_ESCAPED_COMMA_PERCENT _T("Literal commas and percent signs must be escaped (e.g. `%)")
-			if (*cp == g_DerefChar)
-				return ScriptError(ERR_ESCAPED_COMMA_PERCENT, aBuf);
-			if (*cp == g_delimiter) // non-escaped delimiter was found.
-			{
-				// Preserve the ability to add future-use parameters such as section of window
-				// over which the mouse is hovering, e.g. #IfWinActive, Untitled - Notepad,, TitleBar
-				if (first_non_escaped_comma) // A second non-escaped comma was found.
-					return ScriptError(ERR_ESCAPED_COMMA_PERCENT, aBuf);
-				// Otherwise:
-				first_non_escaped_comma = cp;
-				continue; // Check if there are any more non-escaped commas.
-			}
-			// Otherwise, an escape character was found, so skip over the next character (if any).
-			if (!*(++cp)) // The string unexpectedly ends in an escape character, so avoid out-of-bounds.
-				break;
-			// Otherwise, the ++cp above has skipped over the escape-char itself, and the loop's ++cp will now
-			// skip over the char-to-be-escaped, which is not the one we want (even if it is a comma).
-		}
-		if (first_non_escaped_comma) // Above found a non-escaped comma, so there is a second parameter (WinText).
-		{
-			// Omit whitespace to (seems best to conform to convention/expectations rather than give
-			// strange whitespace flexibility that would likely cause unwanted bugs due to inadvertently
-			// have two spaces instead of one).  The user may use `s and `t to put literal leading/trailing
-			// spaces/tabs into these parameters.
-			hot_win_text = omit_leading_whitespace(first_non_escaped_comma + 1);
-			*first_non_escaped_comma = '\0'; // Terminate at the comma to split off hot_win_title on its own.
-			rtrim(hot_win_title, first_non_escaped_comma - hot_win_title);  // Omit whitespace (see similar comment above).
-			// The following must be done only after trimming and omitting whitespace above, so that
-			// `s and `t can be used to insert leading/trailing spaces/tabs.  ConvertEscapeSequences()
-			// also supports insertion of literal commas via escaped sequences.
-			ConvertEscapeSequences(hot_win_text, NULL);
-		}
-		else
-			hot_win_text = _T(""); // And leave hot_win_title set to the entire string because there's only one parameter.
-		// The following must be done only after trimming and omitting whitespace above (see similar comment above).
-		ConvertEscapeSequences(hot_win_title, NULL);
-		// The following also handles the case where both title and text are blank, which could happen
-		// due to something weird but legit like: #IfWinActive, ,
-		if (!SetHotkeyCriterion(hot_criterion, hot_win_title, hot_win_text))
-			return ScriptError(ERR_OUTOFMEM); // So rare that no second param is provided (since its contents may have been temp-terminated or altered above).
-		return CONDITION_TRUE;
-	} // Above completely handles all directives and non-directives that start with "#IfWin".
 
 	if (IS_DIRECTIVE_MATCH(_T("#Hotstring")))
 	{
@@ -7353,9 +7287,9 @@ ResultType Script::PreparseStaticLines(Line *aStartingLine)
 			mLastStaticLine = line;
 			break;
 		case ACT_HOTKEY_IF:
+			if (line->mArg[0].is_expression) // May be false for optimized cases like "#If somevar".
+				PreparseHotkeyIfExpr(line);
 			// It's already been added to the hot-expr list, so just remove it from the script (below).
-			// Override mActionType so ACT_HOTKEY_IF doesn't have to be handled by EvaluateCondition():
-			line->mActionType = ACT_IF;
 			break;
 		default:
 			continue;
@@ -9291,6 +9225,7 @@ end_of_infix_to_postfix:
 	if (   postfix_count == 1 && IS_OPERAND(only_symbol) // This expression is a lone operand, like (1) or "string".
 		&& (mActionType < ACT_FOR || mActionType > ACT_UNTIL) // It's not WHILE or UNTIL, which currently perform better as expressions, or FOR, which performs the same but currently expects aResultToken to always be set.
 		&& (mActionType != ACT_THROW) // Exclude THROW to simplify variable handling (ensures vars are always dereferenced).
+		&& (mActionType != ACT_HOTKEY_IF) // #If requires the expression text not be modified.
 		&& ((only_symbol != SYM_VAR && only_symbol != SYM_DYNAMIC) || mActionType != ACT_RETURN) // "return var" is kept as an expression for correct handling of built-ins, locals (see "ToReturnValue") and ByRef.
 		)
 	{
@@ -10326,7 +10261,7 @@ ResultType Line::EvaluateCondition()
 // Returns CONDITION_TRUE or CONDITION_FALSE (FAIL is returned only in DEBUG mode).
 {
 #ifdef _DEBUG
-	if (mActionType != ACT_IF)
+	if (mActionType != ACT_IF && mActionType != ACT_HOTKEY_IF)
 		return LineError(_T("DEBUG: EvaluateCondition() was called with a line that isn't a condition."));
 #endif
 

source/script.h
@@ -2684,6 +2684,7 @@ private:
 	// be done before dereferencing any line's mNextLine, for example:
 	ResultType PreparseExpressions(Line *aStartingLine);
 	ResultType PreparseStaticLines(Line *aStartingLine);
+	void PreparseHotkeyIfExpr(Line *aLine);
 	Line *PreparseBlocks(Line *aStartingLine, ExecUntilMode aMode = NORMAL_MODE, Line *aParentLine = NULL, const ActionTypeType aLoopType = ACT_INVALID);
 	Line *PreparseCommands(Line *aStartingLine);
 

commit 3e8c8e4144ed3bb9b68b94ad9084f081e690097c

Fixed some issues with #If [broken by e4a54937].

source/hotkey.cpp
@@ -155,7 +155,7 @@ HotkeyCriterion *AddHotkeyIfExpr()
 HotkeyCriterion *FindHotkeyIfExpr(LPTSTR aExpr)
 {
 	for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextExpr)
-		if (!_tcscmp(aExpr, cp->ExprLine->mArg[0].text)) // Case-sensitive since the expression might be.
+		if (cp->Type == HOT_IF_EXPR && !_tcscmp(aExpr, cp->ExprLine->mArg[0].text)) // Case-sensitive since the expression might be.
 			return cp;
 	return NULL;
 }
@@ -1023,7 +1023,7 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aLabelName, LPTSTR aOption
 		if (aJumpToLabel)
 		{
 			HotkeyCriterion *cp;
-			for (cp = g_FirstHotExpr; ; cp = cp->NextCriterion)
+			for (cp = g_FirstHotExpr; ; cp = cp->NextExpr)
 			{
 				if (!cp) // End of the list and it wasn't found.
 				{

source/script2.cpp
@@ -4184,7 +4184,7 @@ LRESULT CALLBACK MainWindowProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lPar
 
 	case AHK_HOT_IF_EVAL: // HotCriterionAllowsFiring uses this to ensure expressions are evaluated only on the main thread.
 		// Ensure wParam is a valid criterion (might prevent shatter attacks):
-		for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextCriterion)
+		for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextExpr)
 			if ((WPARAM)cp == wParam)
 				return cp->Eval((LPTSTR)lParam);
 		return 0;