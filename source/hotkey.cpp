AutoHotkey/AutoHotkey

commit 3e8c8e4144ed3bb9b68b94ad9084f081e690097c

Fixed some issues with #If [broken by e4a54937].

source/hotkey.cpp
@@ -155,7 +155,7 @@ HotkeyCriterion *AddHotkeyIfExpr()
 HotkeyCriterion *FindHotkeyIfExpr(LPTSTR aExpr)
 {
 	for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextExpr)
-		if (!_tcscmp(aExpr, cp->ExprLine->mArg[0].text)) // Case-sensitive since the expression might be.
+		if (cp->Type == HOT_IF_EXPR && !_tcscmp(aExpr, cp->ExprLine->mArg[0].text)) // Case-sensitive since the expression might be.
 			return cp;
 	return NULL;
 }
@@ -1023,7 +1023,7 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aLabelName, LPTSTR aOption
 		if (aJumpToLabel)
 		{
 			HotkeyCriterion *cp;
-			for (cp = g_FirstHotExpr; ; cp = cp->NextCriterion)
+			for (cp = g_FirstHotExpr; ; cp = cp->NextExpr)
 			{
 				if (!cp) // End of the list and it wasn't found.
 				{

source/script2.cpp
@@ -4184,7 +4184,7 @@ LRESULT CALLBACK MainWindowProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lPar
 
 	case AHK_HOT_IF_EVAL: // HotCriterionAllowsFiring uses this to ensure expressions are evaluated only on the main thread.
 		// Ensure wParam is a valid criterion (might prevent shatter attacks):
-		for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextCriterion)
+		for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextExpr)
 			if ((WPARAM)cp == wParam)
 				return cp->Eval((LPTSTR)lParam);
 		return 0;

commit b37285d497520685a0b38c7f2917c04485cc331b

Fixed `Hotkey("If", "Expression")` [broken by 3e8c8e41 and f9822637].

source/hotkey.cpp
@@ -155,7 +155,8 @@ HotkeyCriterion *AddHotkeyIfExpr()
 HotkeyCriterion *FindHotkeyIfExpr(LPTSTR aExpr)
 {
 	for (HotkeyCriterion *cp = g_FirstHotExpr; cp; cp = cp->NextExpr)
-		if (cp->Type == HOT_IF_EXPR && !_tcscmp(aExpr, cp->ExprLine->mArg[0].text)) // Case-sensitive since the expression might be.
+		if (cp->Type != HOT_IF_CALLBACK // i.e. HOT_IF_EXPR or an expression optimised to be any other type.
+			&& !_tcscmp(aExpr, cp->ExprLine->mArg[0].text)) // Case-sensitive since the expression might be.
 			return cp;
 	return NULL;
 }

source/script2.cpp
@@ -14458,7 +14458,7 @@ BIF_DECL(BIF_Hotkey)
 	{
 		if (!ParamIndexIsOmitted(1))
 			functor = TokenToFunctor(*aParam[1]);
-		result = Hotkey::IfExpr(aParam0, functor);
+		result = Hotkey::IfExpr(aParam1, functor);
 	}
 	else
 	{