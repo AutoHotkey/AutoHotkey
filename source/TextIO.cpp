26cfcfcb8b7bc8248a9b37b3090d921d698bbadf
nnnik <nnnik@web.de>
2018-05-01 09:03:57 +0200

    Changed Error priority and cleaned code -Using an unknown property incorrectly now throws an unkown property error instead of invalid use

--- source/TextIO.cpp
+++ source/TextIO.cpp
@@ -733,9 +733,9 @@ class FileObject : public ObjectBase // fincs: No longer allowing the script to 
 		--aParamCount; // Exclude name from param count.
 		LPTSTR name = TokenToString(*aParam[0]); // Name of method or property.
 		MemberID member = INVALID;
-		if (IS_INVOKE_CALL) //Check for methods
+		if (IS_INVOKE_CALL) //nnnik: Check for methods
 		{
-			//Gave up and hard coded all the parameter counts
+			//nnnik: Gave up and hard coded all the parameter counts
 			#define p_exact(e,p)	if (aParamCount == p) member = e; else if (aParamCount < p) _o_throw(ERR_TOO_FEW_PARAMS); else _o_throw(ERR_TOO_MANY_PARAMS);
 			#define p_minmax(e,pmin,pmax)	if (aParamCount <= pmax && aParamCount >= pmin) member = e; else if (aParamCount < pmin) _o_throw(ERR_TOO_FEW_PARAMS); else _o_throw(ERR_TOO_MANY_PARAMS);
 			#define if_member_p_exact(s,e,p)	else if (!_tcsicmp(name, _T(s))) p_exact(e,p)
@@ -768,25 +768,30 @@ class FileObject : public ObjectBase // fincs: No longer allowing the script to 
 			#undef if_member_p_minmx
 			#undef p_exact
 			#undef p_minmax
-			#define if_member_rw(s,e)	else if (!_tcsicmp(name, _T(s))) member = e;                                                       //read and write property
-			#define if_member_ro(s,e)	else if (!_tcsicmp(name, _T(s))) if (IS_INVOKE_GET) member = e; else _o_throw(ERR_INVALID_USAGE);  //read only property
 		}
-		else if (aParamCount != (IS_INVOKE_SET ? 1 : 0)) 
-			_o_throw(ERR_INVALID_USAGE);
-		if_member_rw("Pos", Position)
-		//for clarity
-		if_member_rw("Position", Position) 
-		if_member_rw("Length", Length)
-		if_member_ro("AtEOF", AtEOF)
-		if_member_rw("Encoding", Encoding)
-		if_member_ro("Handle", Handle)
-	#undef if_member_rw
-	#undef if_member_ro
+		else //nnnik: check for properties
+		{
+			#define if_member_rw(s,e)	else if (!_tcsicmp(name, _T(s))) member = e;                                                       //nnnik: read and write property
+			#define if_member_ro(s,e)	else if (!_tcsicmp(name, _T(s))) if (IS_INVOKE_GET) member = e; else _o_throw(ERR_INVALID_USAGE);  //nnnik: read only property
+			if (false); //nnnik: to start the else if ladder
+			if_member_rw("Pos", Position)
+			//for clarity
+			if_member_rw("Position", Position)
+			if_member_rw("Length", Length)
+			if_member_ro("AtEOF", AtEOF)
+			if_member_rw("Encoding", Encoding)
+			if_member_ro("Handle", Handle)
+			if (member) //nnnik: This changes the ordering of the unknown property and invalid use errors
+				if (aParamCount != (IS_INVOKE_SET ? 1 : 0))
+					_o_throw(ERR_INVALID_USAGE);
+			#undef if_member_rw
+			#undef if_member_ro
+		}
 		if (member == INVALID)
 			return INVOKE_NOT_HANDLED;
 
 		/*
-		// Syntax validation: Already handled above
+		// Syntax validation: //nnnik: Already handled above
 		if (!IS_INVOKE_CALL)
 		{
 			if (member < LastMethodPlusOne)
@@ -963,7 +968,7 @@ class FileObject : public ObjectBase // fincs: No longer allowing the script to 
 
 		case RawReadWrite:
 			{
-				//if (aParamCount < 1) //replaced by syntax checks in the member lookup
+				//if (aParamCount < 1) //nnnik: Replaced by syntax checks in the member lookup
 				//	_o_throw(ERR_TOO_FEW_PARAMS);
 
 				bool reading = (name[3] == 'R' || name[3] == 'r');
@@ -1051,16 +1056,17 @@ class FileObject : public ObjectBase // fincs: No longer allowing the script to 
 			}
 			break;
 
+	#define IS_INVOKE_GET_EX aParamCount == 0
 		case Position:
-			if (IS_INVOKE_GET)
+			if (IS_INVOKE_GET_EX) //nnnik: replaced aParamCount check with IS_INVOKE_GET_EX for code clarity
 			{
-		case PositionMethodGet: //tell
+		case PositionMethodGet: //nnnik: tell
 				aResultToken.value_int64 = mFile.Tell();
 				return OK;
 			}
 			else
 			{
-		case PositionMethodSet: //for seek
+		case PositionMethodSet: //nnnik: seek
 				__int64 distance = TokenToInt64(*aParam[1]);
 				int origin;
 				if (aParamCount >= 2)
@@ -1073,8 +1079,9 @@ class FileObject : public ObjectBase // fincs: No longer allowing the script to 
 			}
 			break;
 
+
 		case Length:
-			if (IS_INVOKE_GET)
+			if (IS_INVOKE_GET_EX) 
 			{
 				aResultToken.value_int64 = mFile.Length();
 				return OK;
@@ -1105,7 +1112,7 @@ class FileObject : public ObjectBase // fincs: No longer allowing the script to 
 			//  - It's questionable which behaviour is more more useful, but excluding "-RAW" is definitely simpler.
 			//  - Existing scripts may rely on File.Encoding not returning "-RAW".
 			UINT codepage;
-			if (IS_INVOKE_SET)
+			if (!IS_INVOKE_GET_EX)
 			{
 				if (TokenIsNumeric(*aParam[1]))
 					codepage = (UINT)TokenToInt64(*aParam[1]);
@@ -1137,6 +1144,7 @@ class FileObject : public ObjectBase // fincs: No longer allowing the script to 
 			aResultToken.marker = name;
 			return OK;
 		}
+	#undef IS_GET_INVOKE_EX
 
 		case Close:
 			mFile.Close();
