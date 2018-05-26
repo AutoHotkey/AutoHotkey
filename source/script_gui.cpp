AutoHotkey/AutoHotkey

commit e5f0dcbab4ca2fb3b5053054f4d02100c4024e0c

Changed Gui to handle malloc failure for sFont more gracefully.

source/script_gui.cpp
@@ -317,6 +317,11 @@ ResultType Script::PerformGui(LPTSTR aBuf, LPTSTR aParam2, LPTSTR aParam3, LPTST
 		// Otherwise: Create the object and (later) its window, since all the other sub-commands below need it:
 		for (;;) // For break, to reduce repetition of cleanup-on-failure code.
 		{
+			// v1.0.44.14: sFont is created upon first use to conserve ~14 KB memory in non-GUI scripts.
+			// v1.1.29.00: sFont is created here rather than in FindOrCreateFont(), which is called by
+			// the constructor below, to avoid the need to add extra logic in several places to detect
+			// a failed/NULL array.  Previously that was done by simply terminating the script.
+			if (  (GuiType::sFont || (GuiType::sFont = (FontType *)malloc(sizeof(FontType) * MAX_GUI_FONTS)))  )
 			if (pgui = new GuiType())
 			{
 				if (pgui->mControl = (GuiControlType *)malloc(GUI_CONTROL_BLOCK_SIZE * sizeof(GuiControlType)))
@@ -333,7 +338,7 @@ ResultType Script::PerformGui(LPTSTR aBuf, LPTSTR aParam2, LPTSTR aParam3, LPTST
 				}
 				delete pgui;
 			}
-			result = FAIL; // No error displayed since extremely rare.
+			result = ScriptError(ERR_OUTOFMEM);
 			goto return_the_result;
 		}
 	}
@@ -7691,9 +7696,6 @@ int GuiType::FindOrCreateFont(LPTSTR aOptions, LPTSTR aFontName, FontType *aFoun
 		{
 			// For simplifying other code sections, create an entry in the array for the default font
 			// (GUI constructor relies on at least one font existing in the array).
-			if (!sFont) // v1.0.44.14: Created upon first use to conserve ~14 KB memory in non-GUI scripts.
-				if (   !(sFont = (FontType *)malloc(sizeof(FontType) * MAX_GUI_FONTS))   )
-					g_script.ExitApp(EXIT_CRITICAL, ERR_OUTOFMEM); // Since this condition is so rare, just abort to avoid the need to add extra logic in several places to detect a failed/NULL array.
 			// Doesn't seem likely that DEFAULT_GUI_FONT face/size will change while a script is running,
 			// or even while the system is running for that matter.  I think it's always an 8 or 9 point
 			// font regardless of desktop's appearance/theme settings.

source/script.h
@@ -2729,7 +2729,7 @@ public:
 		, mStyle(WS_POPUP|WS_CLIPSIBLINGS|WS_CAPTION|WS_SYSMENU|WS_MINIMIZEBOX) // WS_CLIPCHILDREN (doesn't seem helpful currently)
 		, mExStyle(0) // This and the above should not be used once the window has been created since they might get out of date.
 		, mInRadioGroup(false), mUseTheme(true), mOwner(NULL), mDelimiter('|')
-		, mCurrentFontIndex(FindOrCreateFont()) // Must call this in constructor to ensure sFont array is never NULL while a GUI object exists.  Omit params to tell it to find or create DEFAULT_GUI_FONT.
+		, mCurrentFontIndex(FindOrCreateFont()) // Must call this in constructor to ensure sFont array is never empty while a GUI object exists.  Omit params to tell it to find or create DEFAULT_GUI_FONT.
 		, mCurrentListView(NULL), mCurrentTreeView(NULL)
 		, mTabControlCount(0), mCurrentTabControlIndex(MAX_TAB_CONTROLS), mCurrentTabIndex(0)
 		, mCurrentColor(CLR_DEFAULT)

commit d758dd87374f6bad9fcb7443aad92f19238d5a89

Fixed `Gui x:Default` if no Gui has been created [broken by e5f0dcba].

source/script_gui.cpp
@@ -218,6 +218,7 @@ ResultType Script::PerformGui(LPTSTR aBuf, LPTSTR aParam2, LPTSTR aParam3, LPTST
 			// a full GuiType structure is constructed.  We can't actually create the Gui yet,
 			// since that would prevent +Owner%N% from working and possibly break other scripts
 			// which rely on the old behaviour.
+			if (  (GuiType::sFont || (GuiType::sFont = (FontType *)malloc(sizeof(FontType) * MAX_GUI_FONTS)))  ) // See similar line below for comments regarding sFont.
 			if (pgui = new GuiType())
 			{
 				if (pgui->mName = tmalloc(name_length + 1))