AutoHotkey/AutoHotkey

commit 60005f450f02933b7e9c9046e4c33fd981a193f3

Improved support for overlapping hotkeys like <^a and ^a with #If.

source/hook.cpp
@@ -1118,9 +1118,12 @@ LRESULT LowLevelCommon(const HHOOK aHook, int aCode, WPARAM wParam, LPARAM lPara
 		// those modifiers themselves form another valid hotkey with this suffix.  In other words,
 		// ModifierVK/SC combos take precedence over normally-modified combos:
 		Hotkey *found_hk = NULL;
-		for (hotkey_id_temp = this_key.first_custom_combo; hotkey_id_temp != HOTKEY_ID_INVALID; )
+		for (hotkey_id_temp = this_key.first_hotkey; hotkey_id_temp != HOTKEY_ID_INVALID; )
 		{
 			Hotkey &this_hk = *Hotkey::shk[hotkey_id_temp]; // hotkey_id_temp does not include flags in this case.
+			if (!(this_hk.mModifierVK || this_hk.mModifierSC))
+				break; // Not a custom combo.
+			hotkey_id_temp = this_hk.mNextHotkey;
 			key_type &this_modifier_key = this_hk.mModifierVK ? kvk[this_hk.mModifierVK] : ksc[this_hk.mModifierSC];
 			// The following check supports the prefix+suffix pairs that have both an up hotkey and a down,
 			// such as:
@@ -1163,7 +1166,6 @@ LRESULT LowLevelCommon(const HHOOK aHook, int aCode, WPARAM wParam, LPARAM lPara
 					// generates down-events (e.g. certain Dell keyboards).
 				}
 			} // qualified prefix is down
-			hotkey_id_temp = this_hk.mNextCustomCombo;
 		} // for each prefix of this suffix
 		if (found_hk)
 		{
@@ -3759,9 +3761,9 @@ void ChangeHookState(Hotkey *aHK[], int aHK_count, HookType aWhichHook, HookType
 					ksc[hk.mModifierSC].sc_takes_precedence = true;
 				}
 			}
-			// Insert this hotkey at the front of the linked list of combos which use this suffix key.
-			hk.mNextCustomCombo = pThisKey->first_custom_combo;
-			pThisKey->first_custom_combo = hk.mID;
+			// Insert this hotkey at the front of the linked list of hotkeys which use this suffix key.
+			hk.mNextHotkey = pThisKey->first_hotkey;
+			pThisKey->first_hotkey = hk.mID;
 			continue;
 		}
 		#ifndef SEND_NOSUPPRESS_PREFIX_KEY_ON_RELEASE // Search for this symbol for details.
@@ -3806,6 +3808,16 @@ void ChangeHookState(Hotkey *aHK[], int aHK_count, HookType aWhichHook, HookType
 			hk_sorted_type &this_hk = hk_sorted[i]; // For performance and convenience.
 			this_hk_is_key_up = this_hk.id_with_flags & HOTKEY_KEY_UP;
 			this_hk_id = this_hk.id_with_flags & HOTKEY_ID_MASK;
+
+			// Insert this hotkey at the front of the list of hotkeys that use this suffix key.
+			// This enables fallback between overlapping hotkeys, such as LCtrl & a, <^+a, ^+a.
+			pThisKey = this_hk.vk ? kvk + this_hk.vk : ksc + this_hk.sc;
+			// Insert after any custom combos.
+			HotkeyIDType *first = &pThisKey->first_hotkey;
+			while (*first != HOTKEY_ID_INVALID && (aHK[*first]->mModifierVK || aHK[*first]->mModifierSC))
+				first = &aHK[*first]->mNextHotkey;
+			aHK[this_hk_id]->mNextHotkey = *first;
+			*first = this_hk_id;
 
 			i_modifiers_merged = this_hk.modifiers;
 			if (this_hk.modifiersLR)
@@ -4012,18 +4024,18 @@ bool HookAdjustMaxHotkeys(Hotkey **&aHK, int &aCurrentMax, int aNewMax)
 
 HotkeyIDType &CustomComboLast(HotkeyIDType *aFirst)
 {
-	for (; *aFirst != HOTKEY_ID_INVALID; aFirst = &Hotkey::shk[*aFirst]->mNextCustomCombo);
+	for (; *aFirst != HOTKEY_ID_INVALID; aFirst = &Hotkey::shk[*aFirst]->mNextHotkey);
 	return *aFirst;
 }
 
 void LinkKeysForCustomCombo(vk_type aNeutral, vk_type aLeft, vk_type aRight)
 {
-	HotkeyIDType first_neutral = kvk[aNeutral].first_custom_combo;
+	HotkeyIDType first_neutral = kvk[aNeutral].first_hotkey;
 	if (first_neutral == HOTKEY_ID_INVALID)
 		return;
 	// Append the neutral key's list to the lists of the left and right keys.
-	CustomComboLast(&kvk[aLeft].first_custom_combo) = first_neutral;
-	CustomComboLast(&kvk[aRight].first_custom_combo) = first_neutral;
+	CustomComboLast(&kvk[aLeft].first_hotkey) = first_neutral;
+	CustomComboLast(&kvk[aRight].first_hotkey) = first_neutral;
 }
 
 

source/hook.h
@@ -123,7 +123,7 @@ struct key_type
 	// Keep sub-32-bit members contiguous to save memory without having to sacrifice performance of
 	// 32-bit alignment:
 	HotkeyIDType hotkey_to_fire_upon_release; // A up-event hotkey queued by a prior down-event.
-	HotkeyIDType first_custom_combo; // The first custom combo hotkey using this key as a suffix.
+	HotkeyIDType first_hotkey; // The first hotkey using this key as a suffix.
 	modLR_type as_modifiersLR; // If this key is a modifier, this will have the corresponding bit(s) for that key.
 	#define PREFIX_ACTUAL 1 // Values for used_as_prefix below, for places that need to distinguish between type of prefix.
 	#define PREFIX_FORCED 2 // v1.0.44: Added so that a neutral hotkey like Control can be forced to fire on key-up even though it isn't actually a prefix key.
@@ -151,7 +151,7 @@ struct key_type
 //   RButton::return
 #define RESET_KEYTYPE_ATTRIB(item) \
 {\
-	item.first_custom_combo = HOTKEY_ID_INVALID;\
+	item.first_hotkey = HOTKEY_ID_INVALID;\
 	item.used_as_prefix = 0;\
 	item.used_as_suffix = false;\
 	item.used_as_key_up = false;\

source/hotkey.cpp
@@ -719,8 +719,10 @@ HotkeyVariant *Hotkey::CriterionFiringIsCertainHelper(HotkeyIDType &aHotkeyIDwit
 	}
 
 	// Since above didn't find any variant of the hotkey than can fire, check for other eligible hotkeys.
-	if (!(hk.mModifierVK || hk.mModifierSC || hk.mHookAction)) // Rule out those that aren't susceptible to the bug due to their lack of support for wildcards.
+	if (!hk.mHookAction) // Rule out those that aren't susceptible to the bug.
 	{
+		// Custom combos are no longer ruled out by the above since they allow extra modifiers and
+		// are capable of obscuring non-custom combos; e.g. LCtrl & a:: obscures <^a::, ^+a:: and so on.
 		// Fix for v1.0.46.13: Although the section higher above found no variant to fire for the
 		// caller-specified hotkey ID, it's possible that some other hotkey (one with a wildcard) is
 		// eligible to fire due to the eclipsing behavior of wildcard hotkeys.  For example:
@@ -736,18 +738,26 @@ HotkeyVariant *Hotkey::CriterionFiringIsCertainHelper(HotkeyIDType &aHotkeyIDwit
 		// makes the odds vanishingly small.  That's why the following simple, high-performance loop is used
 		// rather than more a more complex one that "locates the smallest (most specific) eclipsed wildcard
 		// hotkey", or "the uppermost variant among all eclipsed wildcards that is eligible to fire".
+		// UPDATE: This now uses a linked list of hotkeys which share the same suffix key, in the order of
+		// sort_most_general_before_least, which might solve the concern about precedence.
 		mod_type modifiers = ConvertModifiersLR(g_modifiersLR_logical_non_ignored); // Neutral modifiers.
-		for (int i = 0; i < sHotkeyCount; ++i) // This loop is undesirable; but its performance impact probably isn't measurable in the majority of cases.
+		for (HotkeyIDType candidate_id = hk.mNextHotkey; candidate_id != HOTKEY_ID_INVALID; )
 		{
-			Hotkey &hk2 = *shk[i]; // For performance and convenience.
-			if (   hk2.mVK == hk.mVK // VK and SC (one of which is typically zero) must both match for
-				&& hk2.mSC == hk.mSC // this bug to have wrongly eclipsed a qualified variant of some other hotkey.
-				&& hk2.mAllowExtraModifiers // To be eclipsable by the original hotkey, a candidate must have a wildcard.
-				&& hk2.mKeyUp == aKeyUp // Seems necessary that up/down nature is the same in both.
+			Hotkey &hk2 = *shk[candidate_id]; // For performance and convenience.
+			candidate_id = hk2.mNextHotkey;
+			// Non-wildcard hotkeys are eligible for the workaround in cases like ^+a vs <^+a vs ^<+a, where
+			// the neutral modifier acts as a sort of wildcard (it permits left, right or both).  This also
+			// increases support for varying names, such as Esc vs. Escape vs. vk1B (which already partially
+			// worked if wildcards were used).
+			// However, must ensure only the allowed modifiers are down when !mAllowExtraModifiers.
+			// mVK and mSC aren't checked since the linked list only includes hotkeys for this same suffix key.
+			// This also allows the workaround to be partially applied to LCtrl vs. Ctrl and similar (as suffixes).
+			if (  (hk2.mAllowExtraModifiers || !(~hk2.mModifiersConsolidatedLR & g_modifiersLR_logical_non_ignored))
+				&& hk2.mKeyUp == hk.mKeyUp // Seems necessary that up/down nature is the same in both.
 				&& !hk2.mModifierVK // Avoid accidental matching of normal hotkeys with custom-combo "&"
 				&& !hk2.mModifierSC // hotkeys that happen to have the same mVK/SC.
 				&& !hk2.mHookAction // Might be unnecessary to check this; but just in case.
-				&& hk2.mID != hotkey_id // Don't consider the original hotkey because it's was already found ineligible.
+				&& hk2.mID != hotkey_id // Don't consider the original hotkey because it was already found ineligible.
 				&& !(hk2.mModifiers & ~modifiers) // All neutral modifiers required by the candidate are pressed.
 				&& !(hk2.mModifiersLR & ~g_modifiersLR_logical_non_ignored) // All left-right specific modifiers required by the candidate are pressed.
 				//&& hk2.mType != HK_JOYSTICK // Seems unnecessary since joystick hotkeys don't call us and even if they did, probably should be included.

source/hotkey.h
@@ -155,7 +155,7 @@ public:
 	HookActionType mHookAction;
 	sc_type mSC; // Scan code.  All vk's have a scan code, but not vice versa.
 	sc_type mModifierSC; // If mModifierVK is zero, this scan code, if non-zero, will be used as the modifier.
-	HotkeyIDType mNextCustomCombo; // ID of the next custom combo with the same suffix as this one (initialized by the hook, but only for combos).
+	HotkeyIDType mNextHotkey; // ID of the next hotkey with the same suffix as this one (initialized by the hook).
 
 	// Keep single-byte attributes adjacent to each other to conserve memory within byte-aligned class/struct:
 	modLR_type mModifiersLR;  // Left-right centric versions of the above.

commit 105b45140f951853d58b86bd78dfaf307b17002c

Fixed AltTab hotkeys [broken by 60005f45].

source/hook.cpp
@@ -3809,15 +3809,18 @@ void ChangeHookState(Hotkey *aHK[], int aHK_count, HookType aWhichHook, HookType
 			this_hk_is_key_up = this_hk.id_with_flags & HOTKEY_KEY_UP;
 			this_hk_id = this_hk.id_with_flags & HOTKEY_ID_MASK;
 
-			// Insert this hotkey at the front of the list of hotkeys that use this suffix key.
-			// This enables fallback between overlapping hotkeys, such as LCtrl & a, <^+a, ^+a.
-			pThisKey = this_hk.vk ? kvk + this_hk.vk : ksc + this_hk.sc;
-			// Insert after any custom combos.
-			HotkeyIDType *first = &pThisKey->first_hotkey;
-			while (*first != HOTKEY_ID_INVALID && (aHK[*first]->mModifierVK || aHK[*first]->mModifierSC))
-				first = &aHK[*first]->mNextHotkey;
-			aHK[this_hk_id]->mNextHotkey = *first;
-			*first = this_hk_id;
+			if (this_hk_id <= HOTKEY_ID_MAX) // It's a valid ID and not an ALT_TAB action.
+			{
+				// Insert this hotkey at the front of the list of hotkeys that use this suffix key.
+				// This enables fallback between overlapping hotkeys, such as LCtrl & a, <^+a, ^+a.
+				pThisKey = this_hk.vk ? kvk + this_hk.vk : ksc + this_hk.sc;
+				// Insert after any custom combos.
+				HotkeyIDType *first = &pThisKey->first_hotkey;
+				while (*first != HOTKEY_ID_INVALID && (aHK[*first]->mModifierVK || aHK[*first]->mModifierSC))
+					first = &aHK[*first]->mNextHotkey;
+				aHK[this_hk_id]->mNextHotkey = *first;
+				*first = this_hk_id;
+			}
 
 			i_modifiers_merged = this_hk.modifiers;
 			if (this_hk.modifiersLR)