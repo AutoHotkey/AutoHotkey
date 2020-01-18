c299b851549f8bb0d97f670afe7669e4521a76b6
HelgeffegleH <HelgeffegleH@users.noreply.github.com>
2020-01-18 08:08:12 +0100

    Adding basic members and constructor to ScriptModule class.
    Vars and funcs, name.

--- source/ScriptModules.h
+++ source/ScriptModules.h
@@ -17,7 +17,37 @@ GNU General Public License for more details.
 #include "script.h"
 #include "ScriptModulesDefines.h"
 
-class ScriptModules
+class ScriptModule
 {
+private:
+	LPTSTR mName;										// Name of module.
+	ScriptModule* mOuter;								// The module in which this module resides. 
+														// Having a reference to the enclosing module facilitates load time restoration of the outer module when the inner module definition ends.
+														// Can also be used to refer to the outer module via scope resolution.
+
+	FuncList mFuncs;									// List of functions
+	Var** mVar, ** mLazyVar;							// Array of pointers-to-variable, allocated upon first use and later expanded as needed.
+	int mVarCount, mVarCountMax, mLazyVarCount;			// Count of items in the above array as well as the maximum capacity.
+
+public:
+
+	static const LPTSTR sUnamedModuleName;							// All anonymous namespaces will share this name
+																			// to facilitate implementation and debugging.
+
+
+	ScriptModule(LPTSTR aName, int aFuncsInitSize = 0, ScriptModule* aOuter = NULL) :
+		mVar(NULL), mLazyVar(NULL),
+		mVarCount(0), mVarCountMax(0), mLazyVarCount(0),
+		mOuter(aOuter)
+	{
+		if (aName != SMODULES_UNNAMED_NAME)
+			mName = SimpleHeap::Malloc(aName);	// copy the name for simplicity.
+		else
+			mName = SMODULES_UNNAMED_NAME;
+		if (!mName)
+			return; // out of memory
+		if (aFuncsInitSize && !mFuncs.Alloc(aFuncsInitSize))
+			return; // out of memory
+	}
 
 };
