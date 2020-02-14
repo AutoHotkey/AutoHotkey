153cbe977a4cb35117bcf824239c366638014c86
HotKeyIt <bios2000@web.de>
2020-02-14 01:55:12 +0100

    Fix memory leak: release Array after Invoke.

--- source/script_object.cpp
+++ source/script_object.cpp
@@ -792,7 +792,9 @@ ResultType Object::CallMeta(IObject *aFunc, LPTSTR aName, int aFlags, ResultToke
 	if (IS_INVOKE_SET)
 		param[param_count++] = aParam[aParamCount]; // value
 	// return %aFunc%(this, name, args [, value])
-	return aFunc->Invoke(aResultToken, IT_CALL, nullptr, ExprTokenType(aFunc), param, param_count);
+	ResultType aResult = aFunc->Invoke(aResultToken, IT_CALL, nullptr, ExprTokenType(aFunc), param, param_count);
+	vargs->Release();
+	return aResult;
 }
 
 
