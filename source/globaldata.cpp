7ad251f7043f3c99f299256046e5a6d002c9f2db
Lexikos <unravelin@gmail.com>
2020-06-15 07:38:07 +1000

    Changed default script codepage to UTF-8.

--- source/globaldata.cpp
+++ source/globaldata.cpp
@@ -33,7 +33,7 @@ DWORD g_MainThreadID = GetCurrentThreadId();
 DWORD g_HookThreadID; // Not initialized by design because 0 itself might be a valid thread ID.
 CRITICAL_SECTION g_CriticalRegExCache;
 
-UINT g_DefaultScriptCodepage = CP_ACP;
+UINT g_DefaultScriptCodepage = CP_UTF8;
 
 bool g_DestroyWindowCalled = false;
 HWND g_hWnd = NULL;
