AutoHotkey/AutoHotkey

commit a375f5cd91cb99803ec0daf256002067157c504a

Changed method definitions to create dynamic property with Call.
  - Methods cannot be assigned over by default.
  - Methods can coexist with property getters/setters.

source/script.cpp
@@ -6296,8 +6296,13 @@ ResultType Script::DefineClassProperty(LPTSTR aBuf, bool aStatic, bool &aBufHasB
 	if (!aStatic)
 		class_object = (Object *)class_object->GetOwnPropObj(_T("Prototype"));
 	*name_end = 0; // Terminate for aBuf use below.
-	if (class_object->GetOwnPropType(aBuf) > Object::PropType::Value)
+	switch (class_object->GetOwnPropType(aBuf))
+	{
+	case Object::PropType::Object:
+	case Object::PropType::DynamicValue: // get/set
+	case Object::PropType::DynamicMixed: // get/set and call
 		return ScriptError(ERR_DUPLICATE_DECLARATION, aBuf);
+	}
 	mClassProperty = class_object->DefineProperty(aBuf);
 	mClassPropertyStatic = aStatic;
 	if (!mClassProperty)
@@ -6454,7 +6459,7 @@ ResultType Script::DefineClassVars(LPTSTR aBuf, bool aStatic)
 		// or at the end of the __Init method belonging to this class.  Save the current values:
 		Line *script_first_line = mFirstLine, *script_last_line = mLastLine;
 		Line *block_end;
-		auto init_func = (UserFunc *)class_object->GetOwnPropObj(_T("__Init")); // Can only be a user-defined function or nullptr at this stage.
+		auto init_func = (UserFunc *)class_object->GetMethod(_T("__Init")); // Can only be a user-defined function or nullptr at this stage.
 
 		if (init_func)
 		{
@@ -6880,8 +6885,11 @@ UserFunc *Script::AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, Object *aCl
 		}
 		else
 		{
-			if (aClassObject->HasOwnProp(key))
+			switch (aClassObject->GetOwnPropType(key))
 			{
+			case Object::PropType::Object:
+			case Object::PropType::DynamicMethod: // call
+			case Object::PropType::DynamicMixed: // get/set and call
 				ScriptError(ERR_DUPLICATE_DECLARATION, new_name);
 				return nullptr;
 			}

source/script_object.h
@@ -328,8 +328,6 @@ private:
 	}
 
 protected:
-	IObject *GetMethod(name_t name);
-
 	ResultType CallAsMethod(ExprTokenType &aFunc, ResultToken &aResultToken, ExprTokenType &aThisToken, ExprTokenType *aParam[], int aParamCount);
 	ResultType CallMeta(LPTSTR aName, ResultToken &aResultToken, ExprTokenType &aThisToken, ExprTokenType *aParam[], int aParamCount);
 	ResultType CallMetaVarg(int aFlags, LPTSTR aName, ResultToken &aResultToken, ExprTokenType &aThisToken, ExprTokenType *aParam[], int aParamCount);
@@ -344,6 +342,7 @@ public:
 
 	bool HasProp(name_t aName);
 	bool HasMethod(name_t aName);
+	IObject *GetMethod(name_t name);
 
 	bool HasOwnProps() { return mFields.Length(); }
 	bool HasOwnProp(name_t aName)
@@ -356,7 +355,9 @@ public:
 		None = 0,
 		Value,
 		Object,
-		Dynamic
+		DynamicValue,
+		DynamicMethod,
+		DynamicMixed
 	};
 	PropType GetOwnPropType(name_t aName)
 	{
@@ -365,7 +366,10 @@ public:
 			return PropType::None;
 		switch (field->symbol)
 		{
-		case SYM_DYNAMIC: return PropType::Dynamic;
+		case SYM_DYNAMIC:
+			if (field->prop->Getter() || field->prop->Getter())
+				return field->prop->Method() ? PropType::DynamicMixed : PropType::DynamicValue;
+			return field->prop->Method() ? PropType::DynamicMethod : PropType::None;
 		case SYM_OBJECT: return PropType::Object;
 		default: return PropType::Value;
 		}

source/script_object.cpp
@@ -1329,7 +1329,12 @@ ResultType Object::GetMethod(ResultToken &aResultToken, name_t aName)
 
 bool Object::DefineMethod(name_t aName, IObject *aFunc)
 {
-	return SetOwnProp(aName, aFunc);
+	if (auto prop = DefineProperty(aName))
+	{
+		prop->SetMethod(aFunc);
+		return true;
+	}
+	return false;
 }
 
 Property *Object::DefineProperty(name_t aName)

commit 7c7437df1e7a6dc7cfec6886b804369ba1c6d9a3

Fixed instance var initializers being added to super.__Init.
Caused by a375f5cd.

source/script.cpp
@@ -6408,7 +6408,7 @@ ResultType Script::DefineClassVars(LPTSTR aBuf, bool aStatic)
 		// or at the end of the __Init method belonging to this class.  Save the current values:
 		Line *script_first_line = mFirstLine, *script_last_line = mLastLine;
 		Line *block_end;
-		auto init_func = (UserFunc *)class_object->GetMethod(_T("__Init")); // Can only be a user-defined function or nullptr at this stage.
+		auto init_func = (UserFunc *)class_object->GetOwnPropMethod(_T("__Init")); // Can only be a user-defined function or nullptr at this stage.
 
 		if (init_func)
 		{

source/script_object.h
@@ -390,6 +390,12 @@ public:
 		auto field = FindField(aName);
 		return field && field->symbol == SYM_OBJECT ? field->object : nullptr;
 	}
+	
+	IObject *GetOwnPropMethod(name_t name)
+	{
+		auto field = FindField(name);
+		return field && field->symbol == SYM_DYNAMIC ? field->prop->Method() : nullptr;
+	}
 
 	bool SetOwnProp(name_t aName, ExprTokenType &aValue)
 	{