AutoHotkey/AutoHotkey

commit 47ae50422f2705120b5b8f53c607b3afabf13793

Changed nested classes to static-initialize when first retrieved.

source/script.cpp
@@ -6171,10 +6171,9 @@ ResultType Script::DefineClass(LPTSTR aBuf)
 	if (conflict_found)
 		return ScriptError(ERR_DUPLICATE_DECLARATION, aBuf);
 
-	token.SetValue(mClassName, length);
-	
 	if (mUnresolvedClasses)
 	{
+		token.SetValue(mClassName, length);
 		ExprTokenType *param = &token;
 		ResultToken result_token;
 		result_token.symbol = SYM_STRING; // Init for detecting SYM_OBJECT below.
@@ -6196,7 +6195,22 @@ ResultType Script::DefineClass(LPTSTR aBuf)
 		class_object = Object::CreateClass(prototype = Object::CreatePrototype(mClassName));
 
 	if (mClassObjectCount)
-		outer_class->SetOwnProp(class_name, class_object); // Assign to outer_class[class_name].
+	{
+		// Define property outer_class.%class_name%.
+		auto prop = outer_class->DefineProperty(class_name);
+		
+		ExprTokenType values[] { class_object, class_name }, *param[] { values, values + 1 };
+
+		auto info = SimpleHeap::Alloc<NestedClassInfo>();
+		info->class_object = class_object;
+		info->constructed = false;
+		class_object->AddRef();
+
+		auto get = new BuiltInFunc { _T(""), Class_GetNestedClass, 1, 1, false, info };
+		prop->MinParams = 0;
+		prop->MaxParams = 0;
+		prop->SetGetter(get);
+	}
 	else
 	{
 		class_var->Assign(class_object); // Assign to global variable named %class_name%.
@@ -6210,8 +6224,6 @@ ResultType Script::DefineClass(LPTSTR aBuf)
 
 	if (!DefineClassInit(true))
 		return FAIL;
-	if (mClassObjectCount > 1) // Nested classes still require this for initialization.
-		mClasses->Append(ExprTokenType(class_object));
 	
 	// This line enables a class without any static methods to be freed at program exit,
 	// or sooner if it's a nested class and the script removes it from the outer class.

source/script_object.h
@@ -829,3 +829,11 @@ namespace ErrorPrototype
 
 ResultType GetEnumerator(IObject *&aEnumerator, ExprTokenType &aEnumerable, int aVarCount, bool aDisplayError);
 ResultType CallEnumerator(IObject *aEnumerator, ExprTokenType *aParam[], int aParamCount, bool aDisplayError);
+
+
+struct NestedClassInfo
+{
+	Object *class_object;
+	bool constructed;
+};
+BIF_DECL(Class_GetNestedClass);

source/script_object.cpp
@@ -3086,6 +3086,19 @@ Object *Object::ValueBase(ExprTokenType &aValue)
 
 
 
+BIF_DECL(Class_GetNestedClass)
+{
+	auto info = (NestedClassInfo *)aResultToken.func->mData;
+	auto cls = info->class_object;
+	cls->AddRef();
+	if (info->constructed)
+		_f_return(cls);
+	info->constructed = true;
+	cls->Construct(aResultToken, nullptr, 0);
+}
+
+
+
 #ifdef CONFIG_DEBUGGER
 
 void IObject::DebugWriteProperty(IDebugProperties *aDebugger, int aPage, int aPageSize, int aMaxDepth)

commit 95549b50f046da739e982b3cbea0ad26ae64f91b

Fixed resolution of nested classes for extends/catch.
Broken since 47ae5042.

source/script.cpp
@@ -6551,9 +6551,13 @@ Object *Script::FindClass(LPCTSTR aClassName, size_t aClassNameLength)
 		if (cp == key)
 			return NULL; // ScriptError(_T("Missing name."), cp);
 		*cp = '\0'; // Terminate at the delimiting dot.
-		if (!base_object->GetOwnProp(token, key) || token.symbol != SYM_OBJECT)
+		// Invoking at this stage isn't safe since aClassName could include the name of a
+		// user-defined property getter, or the class might implement __Get, and the script
+		// isn't necessarily ready to execute.  Get it this way instead:
+		auto getter = dynamic_cast<BuiltInFunc*>(base_object->GetOwnPropGetter(key));
+		if (!getter || getter->mBIF != Class_GetNestedClass)
 			return NULL;
-		base_object = (Object *)token.object; // See comment about Object() above.
+		base_object = ((NestedClassInfo*)getter->mData)->class_object;
 	}
 
 	return base_object;

source/script_object.h
@@ -397,6 +397,12 @@ public:
 		return field && field->symbol == SYM_DYNAMIC ? field->prop->Method() : nullptr;
 	}
 
+	IObject *GetOwnPropGetter(name_t name)
+	{
+		auto field = FindField(name);
+		return field && field->symbol == SYM_DYNAMIC ? field->prop->Getter() : nullptr;
+	}
+
 	bool SetOwnProp(name_t aName, ExprTokenType &aValue)
 	{
 		index_t insert_pos;