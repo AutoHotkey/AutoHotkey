AutoHotkey/AutoHotkey

commit 3be2c7a76710f5afb71a7d97e5797a71b585a564

Fixed crashes when a timer's __delete() deletes the next timer.

source/application.cpp
@@ -1824,25 +1824,40 @@ bool CheckScriptTimers()
 		timer.mLabel->ExecuteInNewThread(_T("Timer"));
 		--timer.mExistingThreads;
 
-		// Resolve the next timer only now, in case other timers were created or deleted while
-		// this timer was executing.  Must be done before the timer is potentially deleted below.
-		next_timer = timer.mNextTimer;
-		// If this is a run-once timer for a live reference-counted object (i.e. not a Label or
-		// Func, which can never be deleted), delete the timer.  Otherwise, there's a high risk
-		// that the script will leak objects, because if the object is only referenced by the
-		// timer list, there's no way to re-enable it.  By contrast, a Label or Func can be
-		// referenced by name, and a repeating timer can self-reference during execution.
-		// It is tempting to do this only when mRefCount == 1 (for backward-compatibility),
-		// but that would only work if the script releases its last reference to the object
-		// *before* the timer expires.
-		// mEnabled is checked in case the timer re-enabled itself.
-		if (timer.mRunOnlyOnce && !timer.mEnabled && timer.mLabel.IsLiveObject())
-			timer.mLabel = NULL;
-		// If the script attempted to delete this timer while it was executing, mLabel was set
-		// to NULL and it is now time to delete the timer.  mExistingThreads == 0 is implied
-		// at this point since timers are only allowed one thread.
-		if (timer.mLabel == NULL)
-			g_script.DeleteTimer(NULL);
+		for (auto *this_timer = &timer; this_timer; this_timer = next_timer)
+		{
+			// Resolve the next timer only now, in case other timers were created or deleted while
+			// this timer was executing.  Must be done before the timer is potentially deleted below.
+			next_timer = this_timer->mNextTimer;
+
+			// Check initial eligibility of this timer to be deleted.
+			if (this_timer->mEnabled || this_timer->mExistingThreads || this_timer->mDeleteLocked)
+				break;
+			if (next_timer)
+				next_timer->mDeleteLocked++; // Prevent next_timer from being deleted.
+
+			// If this is a run-once timer for a live reference-counted object (i.e. not a Label or
+			// Func, which can never be deleted), delete the timer.  Otherwise, there's a high risk
+			// that the script will leak objects, because if the object is only referenced by the
+			// timer list, there's no way to re-enable it.  By contrast, a Label or Func can be
+			// referenced by name, and a repeating timer can self-reference during execution.
+			// It is tempting to do this only when mRefCount == 1 (for backward-compatibility),
+			// but that would only work if the script releases its last reference to the object
+			// *before* the timer expires.
+			if (this_timer == &timer && timer.mRunOnlyOnce && timer.mLabel.IsLiveObject())
+				timer.mLabel = NULL; // This might cause __delete to execute.
+
+			// If the script attempted to delete this timer while it was executing, mLabel was set
+			// to NULL and it is now time to delete the timer.  mExistingThreads == 0 is implied
+			// at this point since timers are only allowed one thread.
+			if (this_timer->mLabel == NULL)
+				g_script.DeleteTimer(NULL);
+
+			if (next_timer)
+				next_timer->mDeleteLocked--; // Might still be non-zero due to thread interruption.
+			// Now also check next_timer, in case it was disabled while __delete was executing.
+		} // for() series of timers being deleted.
+
 	} // for() each timer.
 
 	if (at_least_one_timer_launched) // Since at least one subroutine was run above, restore various values for our caller.

source/script.cpp
@@ -3748,7 +3748,8 @@ void Script::DeleteTimer(IObject *aLabel)
 			// Disable it, even if it's not technically being deleted yet.
 			if (timer->mEnabled)
 				timer->Disable(); // Keeps track of mTimerEnabledCount and whether the main timer is needed.
-			if (timer->mExistingThreads) // This condition differs from g->CurrentTimer == timer, which only detects the "top-most" timer.
+			if (timer->mExistingThreads // This condition differs from g->CurrentTimer == timer, which only detects the "top-most" timer.
+				|| timer->mDeleteLocked)
 			{
 				if (!aLabel) // Caller requested we delete a previously marked timer which
 					continue; // has now finished, but this one hasn't, so keep looking.

source/script.h
@@ -2400,6 +2400,7 @@ public:
 	DWORD mTimeLastRun;  // TickCount
 	int mPriority;  // Thread priority relative to other threads, default 0.
 	UCHAR mExistingThreads;  // Whether this timer is already running its subroutine.
+	UCHAR mDeleteLocked;     // Lock count to prevent full deletion.  Separate to mExistingThreads so it doesn't prevent timer execution.
 	bool mEnabled;
 	bool mRunOnlyOnce;
 	ScriptTimer *mNextTimer;  // Next items in linked list
@@ -2409,6 +2410,7 @@ public:
 		: mLabel(aLabel), mPeriod(DEFAULT_TIMER_PERIOD), mPriority(0) // Default is always 0.
 		, mExistingThreads(0), mTimeLastRun(0)
 		, mEnabled(false), mRunOnlyOnce(false), mNextTimer(NULL)  // Note that mEnabled must default to false for the counts to be right.
+		, mDeleteLocked(0)
 	{}
 };
 

commit 66b7f0754da39a823ac434c5577536c46de5b5e6

Fixed execution of multiple run-once timers in the same tick.
Broken by 3be2c7a.

source/application.cpp
@@ -1828,11 +1828,16 @@ bool CheckScriptTimers()
 		{
 			// Resolve the next timer only now, in case other timers were created or deleted while
 			// this timer was executing.  Must be done before the timer is potentially deleted below.
-			next_timer = this_timer->mNextTimer;
 
 			// Check initial eligibility of this timer to be deleted.
 			if (this_timer->mEnabled || this_timer->mExistingThreads || this_timer->mDeleteLocked)
+			{
+				if (this_timer == &timer) // If this_timer itself has just executed.
+					next_timer = this_timer->mNextTimer;
+				//else leave next_timer == this_timer, in case it is ready to execute.
 				break;
+			}
+			next_timer = this_timer->mNextTimer;
 			if (next_timer)
 				next_timer->mDeleteLocked++; // Prevent next_timer from being deleted.
 