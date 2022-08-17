75911e13a949cdd783e75ab65b7c59d6054bf3e3
jeeswg <25886465+jeeswg@users.noreply.github.com>
2022-08-17 12:17:29 +0100

    Backported VerCompare() from alpha branch.

--- source/script.cpp
+++ source/script.cpp
@@ -8674,6 +8674,12 @@ Func *Script::FindFunc(LPCTSTR aFuncName, size_t aFuncNameLength, int *apInsertP
 		min_params = 0;
 		max_params = 3;
 	}
+	else if (!_tcsicmp(func_name, _T("VerCompare")))
+	{
+		bif = BIF_VerCompare;
+		min_params = 2;
+		max_params = 2;
+	}
 	else
 		return NULL; // Maint: There may be other lines above that also return NULL.
 
