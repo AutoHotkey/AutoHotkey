AutoHotkey/AutoHotkey

commit 7c02d1b71e1a288e1346d80e9a2f8f6bd3c3ad0b

Fix BlockInput to throw if its parameter is invalid

source/keyboard_mouse.cpp
@@ -4537,7 +4537,7 @@ void OurBlockInput(bool aEnable)
 	g_BlockInput = aEnable;
 }
 
-bif_impl void ScriptBlockInput(LPCTSTR aMode)
+bif_impl FResult ScriptBlockInput(LPCTSTR aMode)
 {
 	switch (auto toggle = Line::ConvertBlockInput(aMode))
 	{
@@ -4560,7 +4560,8 @@ bif_impl void ScriptBlockInput(LPCTSTR aMode)
 	case TOGGLE_MOUSEMOVEOFF:
 		g_BlockMouseMove = false; // But the mouse hook is left installed because it might be needed by other things. This approach is similar to that used by the Input command.
 		break;
-	// default (NEUTRAL or TOGGLE_INVALID): do nothing.
+	default: // NEUTRAL or TOGGLE_INVALID
+		return FR_E_ARG(0);
 	}
 }
 

source/lib/functions.h
@@ -1,5 +1,5 @@
 
-md_func_x(BlockInput, ScriptBlockInput, Void, (In, String, Mode))
+md_func_x(BlockInput, ScriptBlockInput, FResult, (In, String, Mode))
 
 md_func(CoordMode, (In, String, TargetType), (In_Opt, String, RelativeTo))
 md_func_x(Critical, Critical, Void, (In_Opt, String, OnOffNumber))

commit cc1964d8f66bb6db7461d9478e8ab2dd9a5e7a3f

Fix undefined result for BlockInput on success
Broken by 7c02d1b

source/keyboard_mouse.cpp
@@ -4563,6 +4563,7 @@ bif_impl FResult ScriptBlockInput(LPCTSTR aMode)
 	default: // NEUTRAL or TOGGLE_INVALID
 		return FR_E_ARG(0);
 	}
+	return OK;
 }
 
 #pragma endregion