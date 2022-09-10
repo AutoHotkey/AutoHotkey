AutoHotkey/AutoHotkey

commit 840ee354a11b012af0164e74b473f428725d2bf7

Convert TrayTip

source/script2.cpp
@@ -28,9 +28,11 @@ GNU General Public License for more details.
 // GUI-related: Tray //
 ///////////////////////
 
-ResultType TrayTipParseOptions(LPTSTR aOptions, NOTIFYICONDATA &nic)
+ResultType TrayTipParseOptions(LPCTSTR aOptions, NOTIFYICONDATA &nic)
 {
-	LPTSTR next_option, option_end;
+	if (!aOptions)
+		return OK;
+	LPCTSTR next_option, option_end;
 	TCHAR option[1+MAX_NUMBER_SIZE];
 	for (next_option = omit_leading_whitespace(aOptions); ; next_option = omit_leading_whitespace(option_end))
 	{
@@ -79,7 +81,7 @@ invalid_option:
 }
 
 
-ResultType Line::TrayTip(LPTSTR aText, LPTSTR aTitle, LPTSTR aOptions)
+bif_impl FResult TrayTip(LPCTSTR aText, LPCTSTR aTitle, LPCTSTR aOptions)
 {
 	NOTIFYICONDATA nic = {0};
 	nic.cbSize = sizeof(nic);
@@ -88,8 +90,10 @@ ResultType Line::TrayTip(LPTSTR aText, LPTSTR aTitle, LPTSTR aOptions)
 	nic.uFlags = NIF_INFO;
 	// nic.uTimeout is no longer used because it is valid only on Windows 2000 and Windows XP.
 	if (!TrayTipParseOptions(aOptions, nic))
-		return FAIL;
-	if (*aTitle && !*aText)
+		return FR_FAIL;
+	if (!aTitle) aTitle = _T("");
+	if (!aText) aText = _T("");
+	if (aTitle && !*aText)
 		// As passing an empty string hides the TrayTip (or does nothing on Windows 10),
 		// pass a space to ensure the TrayTip is shown.  Testing showed that Windows 10
 		// will size the notification to fit only the title, as if there was no text.

source/defines.h
@@ -619,7 +619,6 @@ enum enum_act {
 // ================================================================================
 // All others are not included in g_act, and are only used with BIF_PerformAction:
 // ================================================================================
-, ACT_TRAYTIP
 , ACT_CLICK, ACT_MOUSEMOVE, ACT_MOUSECLICK, ACT_MOUSECLICKDRAG
 , ACT_SLEEP
 , ACT_CRITICAL, ACT_THREAD

source/lib/functions.h
@@ -171,3 +171,6 @@ md_func(Suspend, (In_Opt, Int32, Mode))
 
 md_func_x(SysGet, SysGet, Int32, (In, Int32, Index))
 md_func(SysGetIPAddresses, (Ret, Object, RetVal))
+
+
+md_func(TrayTip, (In_Opt, String, Text), (In_Opt, String, Title), (In_Opt, String, Options))

source/script.cpp
@@ -238,7 +238,6 @@ FuncEntry g_BIF[] =
 	BIFA(Thread, 1, 3, ACT_THREAD),
 	BIF1(ToolTip, 0, 4),
 	BIF1(TraySetIcon, 0, 3),
-	BIFA(TrayTip, 0, 3, ACT_TRAYTIP),
 	BIFn(Trim, 1, 2, BIF_Trim),
 	BIF1(Type, 1, 1),
 	BIF1(VarSetStrCapacity, 1, 2, {1}),
@@ -11744,9 +11743,6 @@ ResultType Line::Perform()
 		// is in a variable reference (very rare in this case).
 		}
 		return OK;
-
-	case ACT_TRAYTIP:
-		return TrayTip(THREE_ARGS);
 
 
 //////////////////////////////////////////////////////////////////////////

source/script.h
@@ -724,8 +724,6 @@ private:
 	bool CatchThis(ExprTokenType &aThrown);
 	friend BIF_DECL(BIF_PerformAction);
 
-	ResultType TrayTip(LPTSTR aText, LPTSTR aTitle, LPTSTR aOptions);
-
 	static ResultType SetToggleState(vk_type aVK, ToggleValueType &ForceLock, LPTSTR aToggleText);
 
 public:

commit cb82b941fd099de48c7837689261ac7a4775aa9b

Fixed TrayTip() to hide the traytip [broken by 840ee354a].

source/script2.cpp
@@ -93,7 +93,7 @@ bif_impl FResult TrayTip(LPCTSTR aText, LPCTSTR aTitle, LPCTSTR aOptions)
 		return FR_FAIL;
 	if (!aTitle) aTitle = _T("");
 	if (!aText) aText = _T("");
-	if (aTitle && !*aText)
+	if (*aTitle && !*aText)
 		// As passing an empty string hides the TrayTip (or does nothing on Windows 10),
 		// pass a space to ensure the TrayTip is shown.  Testing showed that Windows 10
 		// will size the notification to fit only the title, as if there was no text.