AutoHotkey/AutoHotkey

commit 1be541778db5dfc3af837394f2cd33d7e723f5e1

Fixed FileCreateDir handling of paths containing ".." or "/".
Implementation copied from commit d4324b2.

Fixes #300.

source/script2.cpp
@@ -9800,10 +9800,9 @@ ResultType Line::FileSelectFile(LPTSTR aOptions, LPTSTR aWorkingDir, LPTSTR aGre
 
 
 
-// As of 2019-09-29, noinline reduces code size by over 20KB on VC++ 2019.
-// Prior to merging Util_CreateDir with this, it wasn't inlined.
-DECLSPEC_NOINLINE
-bool Line::FileCreateDir(LPTSTR aDirSpec, LPTSTR aCanModifyDirSpec)
+static bool FileCreateDirRecursive(LPTSTR aDirSpec);
+
+bool FileCreateDir(LPCTSTR aDirSpec)
 {
 	if (!aDirSpec || !*aDirSpec)
 	{
@@ -9811,6 +9810,25 @@ bool Line::FileCreateDir(LPTSTR aDirSpec, LPTSTR aCanModifyDirSpec)
 		return false;
 	}
 
+	// Make a modifiable copy to be used by recursive calls (supports long paths).
+	// Use GetFullPathName() instead of tmemcpy() or similar to normalize the path,
+	// which has at least two benefits:
+	//  1) Indirectly supports forward slash as a path separator.
+	//  2) Relative components such as "x\y\.." would otherwise cause the function
+	//     to report failure due to the order of checks and CreateDirectory calls.
+	TCHAR buf[T_MAX_PATH];
+	auto len = GetFullPathName(aDirSpec, _countof(buf), buf, nullptr);
+	if (!len || len >= _countof(buf))
+	{
+		SetLastError(ERROR_BUFFER_OVERFLOW);
+		return false;
+	}
+
+	return FileCreateDirRecursive(buf);
+}
+
+static bool FileCreateDirRecursive(LPTSTR aDirSpec)
+{
 	DWORD attr = GetFileAttributes(aDirSpec);
 	if (attr != 0xFFFFFFFF)  // aDirSpec already exists.
 	{
@@ -9822,37 +9840,18 @@ bool Line::FileCreateDir(LPTSTR aDirSpec, LPTSTR aCanModifyDirSpec)
 	// to create this directory:
 	LPTSTR last_backslash = _tcsrchr(aDirSpec, '\\');
 	if (last_backslash > aDirSpec // v1.0.48.04: Changed "last_backslash" to "last_backslash > aDirSpec" so that an aDirSpec with a leading \ (but no other backslashes), such as \dir, is supported.
-		&& last_backslash[-1] != ':') // v1.1.31.00: Don't attempt FileCreateDir("C:") since that's equivalent to either "C:\" or the working directory (which already exists), or FileCreateDir("\\?\C:") since it always fails.
+		&& last_backslash[-1] != ':' // v1.1.31.00: Don't attempt FileCreateDir("C:") since that's equivalent to either "C:\" or the working directory (which already exists), or FileCreateDir("\\?\C:") since it always fails.
+		&& last_backslash[1]) // Skip the recursive call if it's just a trailing backslash.
 	{
-		LPTSTR parent_dir;
-		if (aCanModifyDirSpec)
-		{
-			parent_dir = aDirSpec; // Caller provided a modifiable aDirSpec.
-			*last_backslash = '\0'; // Temporarily terminate for parent directory.
-		}
-		else
-		{
-			// v1.1.31.00: Allocate a modifiable buffer to be used by all calls (supports long paths).
-			parent_dir = (LPTSTR)_alloca((last_backslash - aDirSpec + 1) * sizeof(TCHAR));
-			tcslcpy(parent_dir, aDirSpec, last_backslash - aDirSpec + 1); // Omits the last backslash.
-		}
-		bool exists = FileCreateDir(parent_dir, parent_dir); // Recursively create all needed ancestor directories.
-		if (aCanModifyDirSpec)
-			*last_backslash = '\\'; // Undo temporary termination.
-
-		// v1.0.44: Fixed ErrorLevel being set to 1 when the specified directory ends in a backslash.  In such cases,
-		// two calls were made to CreateDirectory for the same folder: the first without the backslash and then with
-		// it.  Since the directory already existed on the second call, ErrorLevel was wrongly set to 1 even though
-		// everything succeeded.  So now, when recursion finishes creating all the ancestors of this directory
-		// our own layer here does not call CreateDirectory() when there's a trailing backslash because a previous
-		// layer already did:
-		if (!last_backslash[1] || !exists)
+		*last_backslash = '\0'; // Temporarily terminate for parent directory.
+		auto exists = FileCreateDirRecursive(aDirSpec); // Recursively create all needed ancestor directories.
+		*last_backslash = '\\'; // Undo temporary termination.
+		if (!exists)
 			return exists;
 	}
 
 	// The above has recursively created all parent directories of aDirSpec if needed.
-	// Now we can create aDirSpec.  Be sure to explicitly set g_ErrorLevel since it's value
-	// is now indeterminate due to action above:
+	// Now we can create aDirSpec.
 	return CreateDirectory(aDirSpec, NULL);
 }
 

source/script.h
@@ -704,7 +704,6 @@ private:
 	ResultType FileGetShortcut(LPTSTR aShortcutFile);
 	ResultType FileCreateShortcut(LPTSTR aTargetFile, LPTSTR aShortcutFile, LPTSTR aWorkingDir, LPTSTR aArgs
 		, LPTSTR aDescription, LPTSTR aIconFile, LPTSTR aHotkey, LPTSTR aIconNumber, LPTSTR aRunState);
-	static bool FileCreateDir(LPTSTR aDirSpec, LPTSTR aCanModifyDirSpec = NULL);
 	ResultType FileRead(LPTSTR aFilespec);
 	ResultType FileReadLine(LPTSTR aFilespec, LPTSTR aLineNumber);
 	ResultType FileAppend(LPTSTR aFilespec, LPTSTR aBuf, LoopReadFileStruct *aCurrentReadFile);
@@ -3443,6 +3442,7 @@ LPTSTR GetWorkingDir();
 int ConvertJoy(LPTSTR aBuf, int *aJoystickID = NULL, bool aAllowOnlyButtons = false);
 bool ScriptGetKeyState(vk_type aVK, KeyStateTypes aKeyStateType);
 double ScriptGetJoyState(JoyControls aJoy, int aJoystickID, ExprTokenType &aToken, bool aUseBoolForUpDown);
+bool FileCreateDir(LPCTSTR aDirSpec);
 LPTSTR GetExitReasonString(ExitReasons aExitReason);
 
 #endif

commit ba69353cddf74274191eba2baef334fd3d513d06

Fixed FileCreateDir X\Y\ (with trailing slash) failing to create X.
Broken by 1be54177 (v1.1.35.00)

source/script2.cpp
@@ -9846,19 +9846,11 @@ bool FileCreateDir(LPCTSTR aDirSpec)
 
 static bool FileCreateDirRecursive(LPTSTR aDirSpec)
 {
-	DWORD attr = GetFileAttributes(aDirSpec);
-	if (attr != 0xFFFFFFFF)  // aDirSpec already exists.
-	{
-		SetLastError(ERROR_ALREADY_EXISTS);
-		return (attr & FILE_ATTRIBUTE_DIRECTORY) != 0; // Indicate success if it already exists as a dir.
-	}
-
 	// If it has a backslash, make sure all its parent directories exist before we attempt
 	// to create this directory:
 	LPTSTR last_backslash = _tcsrchr(aDirSpec, '\\');
 	if (last_backslash > aDirSpec // v1.0.48.04: Changed "last_backslash" to "last_backslash > aDirSpec" so that an aDirSpec with a leading \ (but no other backslashes), such as \dir, is supported.
-		&& last_backslash[-1] != ':' // v1.1.31.00: Don't attempt FileCreateDir("C:") since that's equivalent to either "C:\" or the working directory (which already exists), or FileCreateDir("\\?\C:") since it always fails.
-		&& last_backslash[1]) // Skip the recursive call if it's just a trailing backslash.
+		&& last_backslash[-1] != ':') // v1.1.31.00: Don't attempt FileCreateDir("C:") since that's equivalent to either "C:\" or the working directory (which already exists), or FileCreateDir("\\?\C:") since it always fails.
 	{
 		*last_backslash = '\0'; // Temporarily terminate for parent directory.
 		auto exists = FileCreateDirRecursive(aDirSpec); // Recursively create all needed ancestor directories.
@@ -9869,7 +9861,7 @@ static bool FileCreateDirRecursive(LPTSTR aDirSpec)
 
 	// The above has recursively created all parent directories of aDirSpec if needed.
 	// Now we can create aDirSpec.
-	return CreateDirectory(aDirSpec, NULL);
+	return CreateDirectory(aDirSpec, NULL) || GetLastError() == ERROR_ALREADY_EXISTS;
 }
 
 