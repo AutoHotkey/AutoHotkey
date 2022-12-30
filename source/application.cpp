AutoHotkey/AutoHotkey

commit f6b5d7f5ba2cc96ffa4ef2eb88d5e58f93c60fe7

Fixed Gui event handler functions to not drop the Gui parameter when the Gui is its own event sink.

source/application.cpp
@@ -1010,9 +1010,10 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 			{
 #define EVT_ARG_ADD(_value) gui_event_args[gui_event_arg_count++].SetValue(_value)
 
-				if (event_is_control_generated || pgui->mEventSink != pgui)
-					// Set first argument
-					EVT_ARG_ADD(event_is_control_generated ? (IObject*)pcontrol : (IObject*)pgui);
+				// The following check isn't used because a Gui with an event sink can also have events handled
+				// by unrelated function objects.  Instead, MsgMonitorList::Call adjusts parameters as needed:
+				//if (event_is_control_generated || pgui->mEventSink != pgui)
+				EVT_ARG_ADD(event_is_control_generated ? (IObject*)pcontrol : (IObject*)pgui);
 
 				switch(gui_action)
 				{

source/script_object.cpp
@@ -2803,7 +2803,12 @@ ResultType MsgMonitorList::Call(ExprTokenType *aParamValue, int aParamCount, UIN
 		// Set last found window (as documented).
 		g->hWndLastUsed = aGui->mHwnd;
 		
-		result = CallMethod(func, func, method_name, aParamValue, aParamCount, &retval);
+		// If we're about to call a method of the Gui itself, don't pass the Gui as the first parameter
+		// since it will be in `this`.  Doing this here rather than when the parameters are built ensures
+		// that message monitor functions (not methods) still receive the expected Gui parameter.
+		int skip_arg = func == aGui && mon.is_method && aParamValue->symbol == SYM_OBJECT && aParamValue->object == aGui;
+
+		result = CallMethod(func, func, method_name, aParamValue + skip_arg, aParamCount - skip_arg, &retval);
 		if (result == FAIL) // Callback encountered an error.
 			break;
 		if (result == EARLY_RETURN) // Callback returned a non-empty value.

commit 5c248dbb9fcd53e3959a0ca0acc1be09c589911c

Fix ContextMenu event to drop Gui parameter if appropriate
Commit f6b5d7f5 caused the Gui to be erroneously passed to event
handler functions (not methods) if the Gui is its own event sink.

source/application.cpp
@@ -1167,10 +1167,8 @@ bool MsgSleep(int aSleepDuration, MessageMode aMode)
 				if (gui_action == GUI_EVENT_CONTEXTMENU && pcontrol)
 				{
 					// Call the control's context menu handler, if any, omitting the "Gui" parameter
-					// for consistency with other Ctrl events (and perhaps convenience).  But don't
-					// omit the first parameter if the Gui parameter was already omitted.
-					int arg_to_omit = pgui->mEventSink != pgui ? 1 : 0;
-					result = pcontrol->events.Call(gui_event_args + arg_to_omit, gui_event_arg_count - arg_to_omit, gui_event_code, gui_event_kind, pgui);
+					// for consistency with other Ctrl events (and perhaps convenience).
+					result = pcontrol->events.Call(gui_event_args + 1, gui_event_arg_count - 1, gui_event_code, gui_event_kind, pgui);
 					if (result == EARLY_RETURN // Suppress the GUI's handler for this event, if any.
 						|| !pgui->mHwnd) // Gui was destroyed.
 					{