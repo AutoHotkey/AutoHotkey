AutoHotkey/AutoHotkey

commit 9d9cd3bfe96bbeb7f8d030d4fa447bab55347bfd

Convert Hotkey and HotIf functions

source/script2.cpp
@@ -2434,62 +2434,77 @@ BIF_DECL(BIF_Number)
 ////////////////////
 
 
-BIF_DECL(BIF_Hotkey)
+bif_impl FResult BIF_Hotkey(StrArg aName, ExprTokenType *aAction, optl<StrArg> aOptions)
 {
-	_f_param_string_opt(aParam0, 0, nullptr, _f_callee_id == FID_HotIf);
-	_f_param_string_opt(aParam1, 1, nullptr, true);
-	_f_param_string_opt(aParam2, 2);
-	
 	ResultType result = OK;
 	IObject *functor = nullptr;
-
-	switch (_f_callee_id) 
+	HookActionType hook_action = 0;
+	if (aAction)
 	{
-	case FID_Hotkey:
-	{
-		HookActionType hook_action = 0;
-		if (!ParamIndexIsOmitted(1))
+		auto action_string = TokenToString(*aAction);
+		if (  !(functor = TokenToObject(*aAction)) && *action_string
+			&& !(hook_action = Hotkey::ConvertAltTab(action_string, true))  )
 		{
-			if (  !(functor = ParamIndexToObject(1)) && *aParam1
-				&& !(hook_action = Hotkey::ConvertAltTab(aParam1, true))  )
+			// Search for a match in the hotkey variants' "original callbacks".
+			// I.e., find the function implicitly defined by "x::action".
+			for (int i = 0; i < Hotkey::sHotkeyCount; ++i)
 			{
-				// Search for a match in the hotkey variants' "original callbacks".
-				// I.e., find the function implicitly defined by "x::action".
-				for (int i = 0; i < Hotkey::sHotkeyCount; ++i)
-				{
-					if (_tcscmp(Hotkey::shk[i]->mName, aParam1))
-						continue;
-					
-					for (HotkeyVariant* v = Hotkey::shk[i]->mFirstVariant; v; v = v->mNextVariant)
-						if (v->mHotCriterion == g->HotCriterion)
-						{
-							functor = v->mOriginalCallback.ToFunc();
-							goto break_twice;
-						}
-				}
-			break_twice:;
-				if (!functor)
-					_f_throw_param(1);
+				if (_tcscmp(Hotkey::shk[i]->mName, aName))
+					continue;
+				
+				for (HotkeyVariant* v = Hotkey::shk[i]->mFirstVariant; v; v = v->mNextVariant)
+					if (v->mHotCriterion == g->HotCriterion)
+					{
+						functor = v->mOriginalCallback.ToFunc();
+						goto break_twice;
+					}
 			}
+		break_twice:;
 			if (!functor)
-				hook_action = Hotkey::ConvertAltTab(aParam1, true);
+				return FR_E_ARG(1);
 		}
-		result = Hotkey::Dynamic(aParam0, aParam2, functor, hook_action, aResultToken);
-		break;
+		if (!functor)
+			hook_action = Hotkey::ConvertAltTab(action_string, true);
 	}
-	case FID_HotIf:
-		functor = ParamIndexToOptionalObject(0);
-		result = Hotkey::IfExpr(aParam0, functor, aResultToken);
-		break;
-	
-	default: // HotIfWinXXX
-		result = SetHotkeyCriterion(_f_callee_id, aParam0, aParam1); // Currently, it only fails upon out-of-memory.
-	
+	return Hotkey::Dynamic(aName, aOptions.value_or_empty(), functor, hook_action);
+}
+
+
+
+bif_impl FResult HotIf(ExprTokenType *aCriterion)
+{
+	TCHAR buf[MAX_NUMBER_SIZE];
+	if (!aCriterion)
+	{
+		g->HotCriterion = nullptr;
+		return OK;
 	}
-	
-	if (!result)
-		_f_return_FAIL;
-	_f_return_empty;
+	else if (auto obj = TokenToObject(*aCriterion))
+		return Hotkey::IfExpr(obj);
+	else
+		return Hotkey::IfExpr(TokenToString(*aCriterion, buf));
+}
+
+
+
+bif_impl FResult HotIfWinActive(optl<StrArg> aWinTitle, optl<StrArg> aWinText)
+{
+	return SetHotkeyCriterion(HOT_IF_ACTIVE, aWinTitle.value_or_empty(), aWinText.value_or_empty());
+}
+
+bif_impl FResult HotIfWinNotActive(optl<StrArg> aWinTitle, optl<StrArg> aWinText)
+{
+	return SetHotkeyCriterion(HOT_IF_NOT_ACTIVE, aWinTitle.value_or_empty(), aWinText.value_or_empty());
+}
+
+bif_impl FResult HotIfWinExist(optl<StrArg> aWinTitle, optl<StrArg> aWinText)
+{
+	return SetHotkeyCriterion(HOT_IF_EXIST, aWinTitle.value_or_empty(), aWinText.value_or_empty());
+}
+
+bif_impl FResult HotIfWinNotExist(optl<StrArg> aWinTitle, optl<StrArg> aWinText)
+{
+	return SetHotkeyCriterion(HOT_IF_NOT_EXIST, aWinTitle.value_or_empty(), aWinText.value_or_empty());
 }
 
 

source/hotkey.cpp
@@ -66,21 +66,21 @@ HWND HotCriterionAllowsFiring(HotkeyCriterion *aCriterion, LPTSTR aHotkeyName)
 
 
 
-ResultType SetHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText)
-// Returns FAIL if memory couldn't be allocated, or OK otherwise.
+FResult SetHotkeyCriterion(HotCriterionType aType, LPCTSTR aWinTitle, LPCTSTR aWinText)
+// Returns FR_FAIL if memory couldn't be allocated (and an error was raised), or OK otherwise.
 // This is a global function because it's used by both hotkeys and hotstrings.
 {
 	HotkeyCriterion *cp = nullptr;
 	if (  (*aWinTitle || *aWinText)
 		&& !(cp = FindHotkeyCriterion(aType, aWinTitle, aWinText))
 		&& !(cp = AddHotkeyCriterion(aType, aWinTitle, aWinText))  )
-		return FAIL;
+		return FR_FAIL;
 	g->HotCriterion = cp;
 	return OK;
 }
 
 
-HotkeyCriterion *FindHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText)
+HotkeyCriterion *FindHotkeyCriterion(HotCriterionType aType, LPCTSTR aWinTitle, LPCTSTR aWinText)
 {
 	// Storing combinations of WinTitle+WinText doesn't have as good a best-case memory savings as
 	// have a separate linked list for Title vs. Text.  But it does save code size, and in the vast
@@ -94,7 +94,7 @@ HotkeyCriterion *FindHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, L
 }
 
 
-HotkeyCriterion *AddHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText)
+HotkeyCriterion *AddHotkeyCriterion(HotCriterionType aType, LPCTSTR aWinTitle, LPCTSTR aWinText)
 {
 	HotkeyCriterion *cp;
 	cp = SimpleHeap::Alloc<HotkeyCriterion>();
@@ -147,7 +147,7 @@ HotkeyCriterion *AddHotkeyIfExpr()
 }
 
 
-HotkeyCriterion *FindHotkeyIfExpr(LPTSTR aExpr)
+HotkeyCriterion *FindHotkeyIfExpr(LPCTSTR aExpr)
 {
 	for (HotkeyCriterion* cp = g_FirstHotExpr; cp; cp = cp->NextExpr)
 		if (cp->OriginalExpr && !_tcscmp(aExpr, cp->OriginalExpr)) // Case-sensitive since the expression might be.
@@ -971,9 +971,10 @@ void Hotkey::PerformInNewThreadMadeByCaller(HotkeyVariant &aVariant)
 	}
 }
 
-ResultType Hotkey::IfExpr(LPTSTR aExpr, IObject *aExprObj, ResultToken &aResultToken)
+
+
+FResult Hotkey::IfExpr(IObject *aExprObj)
 // HotIf ; Set null criterion.
-// HotIf "Exact-expression-text"
 // HotIf FunctionObject
 {
 	if (aExprObj)
@@ -983,10 +984,11 @@ ResultType Hotkey::IfExpr(LPTSTR aExpr, IObject *aExprObj, ResultToken &aResultT
 		{
 			if (!cp) // End of the list and it wasn't found.
 			{
-				if (!ValidateFunctor(aExprObj, 1, aResultToken))
-					return FAIL;
+				auto fr = ValidateFunctor(aExprObj, 1);
+				if (fr != OK)
+					return fr;
 				if (  !(cp = AddHotkeyIfExpr())  )
-					return aResultToken.MemoryError();
+					return FR_E_OUTOFMEM;
 				aExprObj->AddRef();
 				cp->Type = HOT_IF_CALLBACK;
 				cp->Callback = aExprObj;
@@ -999,21 +1001,36 @@ ResultType Hotkey::IfExpr(LPTSTR aExpr, IObject *aExprObj, ResultToken &aResultT
 		}
 		g->HotCriterion = cp;
 	}
-	else if (!*aExpr)
+	else
 	{
-		g->HotCriterion = NULL;
+		g->HotCriterion = nullptr;
+	}
+	return OK;
+}
+
+
+
+FResult Hotkey::IfExpr(LPCTSTR aExpr)
+// HotIf ; Set null criterion.
+// HotIf "Exact-expression-text"
+{
+	if (!aExpr || !*aExpr)
+	{
+		g->HotCriterion = nullptr;
 	}
 	else
 	{
 		HotkeyCriterion *cp = FindHotkeyIfExpr(aExpr);
 		if (!cp) // Expression not found.
-			return aResultToken.ValueError(ERR_HOTKEY_IF_EXPR);
+			return FValueError(ERR_HOTKEY_IF_EXPR, aExpr);
 		g->HotCriterion = cp;
 	}
 	return OK;
 }
 
-ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions, IObject *aCallback, HookActionType aHookAction, ResultToken &aResultToken)
+
+
+FResult Hotkey::Dynamic(LPCTSTR aHotkeyName, LPCTSTR aOptions, IObject *aCallback, HookActionType aHookAction)
 // Creates, updates, enables, or disables a hotkey dynamically (while the script is running).
 // Returns OK or FAIL.
 {
@@ -1022,8 +1039,9 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions, IObject *aCallba
 	// (i.e. it's retaining its current callback).
 	if (aCallback)
 	{
-		if (!ValidateFunctor(aCallback, 1, aResultToken))
-			return FAIL;
+		auto fr = ValidateFunctor(aCallback, 1);
+		if (fr != OK)
+			return fr;
 	}
 
 	UCHAR no_suppress;
@@ -1039,12 +1057,12 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions, IObject *aCallba
 	case HOTKEY_ID_OFF:
 	case HOTKEY_ID_TOGGLE:
 		if (!hk)
-			return aResultToken.Error(ERR_NONEXISTENT_HOTKEY, aHotkeyName, ErrorPrototype::Target);
+			return FError(ERR_NONEXISTENT_HOTKEY, aHotkeyName, ErrorPrototype::Target);
 		if (!(variant || hk->mHookAction)) // mHookAction (alt-tab) hotkeys don't need a variant that matches the current criteria.
 			// To avoid ambiguity and also allow the script to use error handling to detect whether a variant
 			// already exists, it seems best to strictly require a matching variant rather than falling back
 			// onto some "default variant" such as the global variant (if any).
-			return aResultToken.Error(ERR_NONEXISTENT_VARIANT, aHotkeyName, ErrorPrototype::Target);
+			return FError(ERR_NONEXISTENT_VARIANT, aHotkeyName, ErrorPrototype::Target);
 		if (aHookAction == HOTKEY_ID_TOGGLE)
 			aHookAction = hk->mHookAction
 				? (hk->mParentEnabled ? HOTKEY_ID_OFF : HOTKEY_ID_ON) // Enable/disable parent hotkey (due to alt-tab being a global hotkey).
@@ -1067,7 +1085,7 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions, IObject *aCallba
 			else // Create hotkey: Hotkey Name, Callback [, Options]
 			{
 				if (!aCallback) // Caller is trying to set new aOptions for a nonexistent hotkey.
-					return aResultToken.Error(ERR_NONEXISTENT_HOTKEY, aHotkeyName, ErrorPrototype::Target);
+					return FError(ERR_NONEXISTENT_HOTKEY, aHotkeyName, ErrorPrototype::Target);
 				hk = AddHotkey(aCallback, 0, aHotkeyName, no_suppress);
 			}
 			if (!hk)
@@ -1122,7 +1140,7 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions, IObject *aCallba
 				else // No existing variant matching current criteria, so create a new variant.
 				{
 					if (   !(variant = hk->AddVariant(aCallback, no_suppress))   ) // Out of memory.
-						return aResultToken.MemoryError();
+						return FR_E_OUTOFMEM;
 					variant_was_just_created = true;
 					update_all_hotkeys = true;
 					// It seems undesirable for #UseHook to be applied to a hotkey just because it's options
@@ -1166,7 +1184,7 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions, IObject *aCallba
 	// Hotkey, Name,, Options  ; Where name exists as a hotkey, but the right variant doesn't yet exist.
 	// If it catches anything else, that could be a bug, so this error message will help spot it.
 	if (!(variant || hk->mHookAction)) // mHookAction (alt-tab) hotkeys don't need a variant that matches the current criteria.
-		return aResultToken.Error(ERR_NONEXISTENT_VARIANT, aHotkeyName, ErrorPrototype::Target);
+		return FError(ERR_NONEXISTENT_VARIANT, aHotkeyName, ErrorPrototype::Target);
 	// Below relies on the fact that either variant or hk->mHookAction (or both) is now non-zero.
 	// Specifically, when an existing hotkey was changed to become an alt-tab hotkey, above, there will sometimes
 	// be a NULL variant (depending on whether there happens to be a variant in the hotkey that matches the current criteria).
@@ -1175,7 +1193,7 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions, IObject *aCallba
 	// g_MaxThreadsBuffer, etc.
 	if (*aOptions)
 	{
-		for (LPTSTR cp = aOptions; *cp; ++cp)
+		for (auto cp = aOptions; *cp; ++cp)
 		{
 			switch(ctoupper(*cp))
 			{
@@ -1251,7 +1269,7 @@ ResultType Hotkey::Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions, IObject *aCallba
 
 
 
-Hotkey *Hotkey::AddHotkey(IObject *aCallback, HookActionType aHookAction, LPTSTR aName, UCHAR aNoSuppress)
+Hotkey *Hotkey::AddHotkey(IObject *aCallback, HookActionType aHookAction, LPCTSTR aName, UCHAR aNoSuppress)
 // aCallback can be NULL if the caller is creating a dynamic hotkey that has an aHookAction.
 // aName must not be NULL.
 // Returns the address of the new hotkey on success, or NULL otherwise.
@@ -1274,7 +1292,7 @@ Hotkey *Hotkey::AddHotkey(IObject *aCallback, HookActionType aHookAction, LPTSTR
 
 
 
-Hotkey::Hotkey(HotkeyIDType aID, IObject *aCallback, HookActionType aHookAction, LPTSTR aName
+Hotkey::Hotkey(HotkeyIDType aID, IObject *aCallback, HookActionType aHookAction, LPCTSTR aName
 	, UCHAR aNoSuppress)
 	: mID(HOTKEY_ID_INVALID)  // Default until overridden.
 	// Caller must ensure that either aName or aCallback isn't NULL.
@@ -1573,7 +1591,7 @@ HotkeyVariant *Hotkey::AddVariant(IObject *aCallback, UCHAR aNoSuppress)
 
 
 
-ResultType Hotkey::TextInterpret(LPTSTR aName, Hotkey *aThisHotkey, bool aSyntaxCheckOnly)
+ResultType Hotkey::TextInterpret(LPCTSTR aName, Hotkey *aThisHotkey, bool aSyntaxCheckOnly)
 // Returns OK or FAIL.  This function is static and aThisHotkey is passed in as a parameter
 // so that aThisHotkey can be NULL. NULL signals that aName should be checked as a valid
 // hotkey only rather than populating the members of the new hotkey aThisHotkey. This function
@@ -1613,7 +1631,7 @@ ResultType Hotkey::TextInterpret(LPTSTR aName, Hotkey *aThisHotkey, bool aSyntax
 
 
 
-LPTSTR Hotkey::TextToModifiers(LPTSTR aText, Hotkey *aThisHotkey, HotkeyProperties *aProperties)
+LPCTSTR Hotkey::TextToModifiers(LPCTSTR aText, Hotkey *aThisHotkey, HotkeyProperties *aProperties)
 // This function and those it calls should avoid showing any error dialogs when caller passes NULL for aThisHotkey.
 // Takes input param <text> to support receiving only a subset of object.text.
 // Returns the location in <text> of the first non-modifier key.
@@ -1632,7 +1650,7 @@ LPTSTR Hotkey::TextToModifiers(LPTSTR aText, Hotkey *aThisHotkey, HotkeyProperti
 
 	// Explicitly avoids initializing modifiers to 0 because the caller may have already included
 	// some set some modifiers in there.
-	LPTSTR marker;
+	LPCTSTR marker;
 	bool key_left, key_right;
 
 	// Simplifies and reduces code size below:
@@ -1763,11 +1781,10 @@ break_loop:
 	{
 		// When caller passes non-NULL aProperties, it didn't omit the prefix portion of a composite hotkey
 		// (e.g. the "a & " part of "a & b" is present).  So parse these and all other types of hotkeys when in this mode.
-		LPTSTR composite, temp;
-		if (composite = _tcsstr(marker, COMPOSITE_DELIMITER))
+		if (auto composite = _tcsstr(marker, COMPOSITE_DELIMITER))
 		{
 			tcslcpy(aProperties->prefix_text, marker, _countof(aProperties->prefix_text)); // Protect against overflow case script ultra-long (and thus invalid) key name.
-			if (temp = _tcsstr(aProperties->prefix_text, COMPOSITE_DELIMITER)) // Check again in case it tried to overflow.
+			if (auto temp = _tcsstr(aProperties->prefix_text, COMPOSITE_DELIMITER)) // Check again in case it tried to overflow.
 				omit_trailing_whitespace(aProperties->prefix_text, temp)[1] = '\0'; // Truncate prefix_text so that the suffix text is omitted.
 			composite = omit_leading_whitespace(composite + COMPOSITE_DELIMITER_LENGTH);
 			aProperties->prefix_has_tilde = aProperties->suffix_has_tilde;
@@ -1777,7 +1794,7 @@ break_loop:
 		}
 		else // A normal (non-composite) hotkey, so no_suppress was already set properly (higher above).
 			tcslcpy(aProperties->suffix_text, omit_leading_whitespace(marker), _countof(aProperties->suffix_text)); // Protect against overflow case script ultra-long (and thus invalid) key name.
-		if (temp = tcscasestr(aProperties->suffix_text, _T(" Up"))) // Should be reliable detection method because leading spaces have been omitted and it's unlikely a legitimate key name will ever contain a space followed by "Up".
+		if (auto temp = tcscasestr(aProperties->suffix_text, _T(" Up"))) // Should be reliable detection method because leading spaces have been omitted and it's unlikely a legitimate key name will ever contain a space followed by "Up".
 		{
 			omit_trailing_whitespace(aProperties->suffix_text, temp)[1] = '\0'; // Omit " Up" from suffix_text since caller wants that.
 			aProperties->is_key_up = true; // Override the default set earlier.
@@ -1788,7 +1805,7 @@ break_loop:
 
 
 
-ResultType Hotkey::TextToKey(LPTSTR aText, bool aIsModifier, Hotkey *aThisHotkey, bool aSyntaxCheckOnly)
+ResultType Hotkey::TextToKey(LPCTSTR aText, bool aIsModifier, Hotkey *aThisHotkey, bool aSyntaxCheckOnly)
 // This function and those it calls should avoid showing any error dialogs when caller passes NULL for
 // aThisHotkey (however, there is at least one exception explained in comments below where it occurs).
 // Caller must ensure that aText is a modifiable string.
@@ -1804,8 +1821,10 @@ ResultType Hotkey::TextToKey(LPTSTR aText, bool aIsModifier, Hotkey *aThisHotkey
 	bool is_mouse = false;
 	int joystick_id;
 
+	TCHAR buf[32]; // Large enough that truncation wouldn't yield a valid key name.
+
 	// Previous steps should make it unnecessary to call omit_leading_whitespace(aText).
-	LPTSTR keyname_end = find_identifier_end(aText);
+	auto keyname_end = find_identifier_end(aText);
 	if (keyname_end == aText && *aText) // Any single character except '\0' can be a key name.
 		++keyname_end;
 
@@ -1816,7 +1835,10 @@ ResultType Hotkey::TextToKey(LPTSTR aText, bool aIsModifier, Hotkey *aThisHotkey
 		// This is a key-up hotkey, such as "Ctrl Up::".
 		if (aThisHotkey)
 			aThisHotkey->mKeyUp = true;
-		*keyname_end = '\0'; // Terminate at the first space so that the word "up" is removed from further consideration.
+		tcslcpy(buf, aText, _countof(buf)); // Make a null-terminated copy to simplify various checks below.
+		if (keyname_end - aText < _countof(buf))
+			buf[keyname_end - aText] = '\0'; // Terminate at the first space so that the word "up" is removed from further consideration.
+		aText = buf;
 	}
 	else
 	{
@@ -2003,7 +2025,7 @@ void Hotkey::InstallMouseHook()
 
 
 
-Hotkey *Hotkey::FindHotkeyByTrueNature(LPTSTR aName, UCHAR &aNoSuppress, bool &aHookIsMandatory)
+Hotkey *Hotkey::FindHotkeyByTrueNature(LPCTSTR aName, UCHAR &aNoSuppress, bool &aHookIsMandatory)
 // Returns the address of the hotkey if found, NULL otherwise.
 // In v1.0.42, it tries harder to find a match so that the order of modifier symbols doesn't affect the true nature of a hotkey.
 // For example, ^!c should be the same as !^c, primarily because RegisterHotkey() and the hook would consider them the same.

source/hotkey.h
@@ -65,12 +65,12 @@ enum HotkeyTypeEnum {HK_NORMAL, HK_KEYBD_HOOK, HK_MOUSE_HOOK, HK_BOTH_HOOKS, HK_
 
 HWND HotCriterionAllowsFiring(HotkeyCriterion *aCriterion, LPTSTR aHotkeyName); // Used by hotkeys and hotstrings.
 bool HotInputLevelAllowsFiring(SendLevelType inputLevel, ULONG_PTR aEventExtraInfo, LPTSTR aKeyHistoryChar);
-ResultType SetHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText);
+FResult SetHotkeyCriterion(HotCriterionType aType, LPCTSTR aWinTitle, LPCTSTR aWinText);
 HotkeyCriterion *AddHotkeyCriterion(HotkeyCriterion *aCriterion);
-HotkeyCriterion *AddHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText);
+HotkeyCriterion *AddHotkeyCriterion(HotCriterionType aType, LPCTSTR aWinTitle, LPCTSTR aWinText);
 HotkeyCriterion *AddHotkeyIfExpr();
-HotkeyCriterion *FindHotkeyCriterion(HotCriterionType aType, LPTSTR aWinTitle, LPTSTR aWinText);
-HotkeyCriterion *FindHotkeyIfExpr(LPTSTR aExpr);
+HotkeyCriterion *FindHotkeyCriterion(HotCriterionType aType, LPCTSTR aWinTitle, LPCTSTR aWinText);
+HotkeyCriterion *FindHotkeyIfExpr(LPCTSTR aExpr);
 
 inline int InputLevelFromInfo(ULONG_PTR aExtraInfo)
 {
@@ -161,7 +161,7 @@ private:
 
 	// For now, constructor & destructor are private so that only static methods can create new
 	// objects.  This allow proper tracking of which OS hotkey IDs have been used.
-	Hotkey(HotkeyIDType aID, IObject *aCallback, HookActionType aHookAction, LPTSTR aName, UCHAR aNoSuppress);
+	Hotkey(HotkeyIDType aID, IObject *aCallback, HookActionType aHookAction, LPCTSTR aName, UCHAR aNoSuppress);
 	~Hotkey() {if (mIsRegistered) Unregister();}
 
 public:
@@ -211,11 +211,11 @@ public:
 
 	static void AllDestructAndExit(int exit_code);
 
-	static ResultType IfExpr(LPTSTR aExpr, IObject *aExprObj, ResultToken &aResultToken);
-	static ResultType Dynamic(LPTSTR aHotkeyName, LPTSTR aOptions
-		, IObject *aCallback, HookActionType aHookAction, ResultToken &aResultToken);
+	static FResult IfExpr(IObject *aExprObj);
+	static FResult IfExpr(LPCTSTR aExpr);
+	static FResult Dynamic(LPCTSTR aHotkeyName, LPCTSTR aOptions, IObject *aCallback, HookActionType aHookAction);
 
-	static Hotkey *AddHotkey(IObject *aCallback, HookActionType aHookAction, LPTSTR aName, UCHAR aNoSuppress);
+	static Hotkey *AddHotkey(IObject *aCallback, HookActionType aHookAction, LPCTSTR aName, UCHAR aNoSuppress);
 	HotkeyVariant *FindVariant();
 	HotkeyVariant *AddVariant(IObject *aCallback, UCHAR aNoSuppress);
 	static bool PrefixHasNoEnabledSuffixes(int aVKorSC, bool aIsSC, bool &aSuppress);
@@ -228,7 +228,7 @@ public:
 	static void ManifestAllHotkeysHotstringsHooks();
 	static void RequireHook(HookType aWhichHook, bool aRequire = true) { aRequire ? sWhichHookAlways |= aWhichHook : sWhichHookAlways &= ~aWhichHook; }
 	static void MaybeUninstallHook();
-	static ResultType TextInterpret(LPTSTR aName, Hotkey *aThisHotkey, bool aSyntaxCheckOnly = false);
+	static ResultType TextInterpret(LPCTSTR aName, Hotkey *aThisHotkey, bool aSyntaxCheckOnly = false);
 
 	static constexpr int KEY_NAME_BUF_SIZE = 24; // Large enough to hold the largest key name in g_key_to_vk, which is probably "Browser_Favorites" (17).
 
@@ -244,8 +244,8 @@ public:
 		bool is_key_up;
 		bool hook_is_mandatory;
 	};
-	static LPTSTR TextToModifiers(LPTSTR aText, Hotkey *aThisHotkey, HotkeyProperties *aProperties = NULL);
-	static ResultType TextToKey(LPTSTR aText, bool aIsModifier, Hotkey *aThisHotkey, bool aSyntaxCheckOnly);
+	static LPCTSTR TextToModifiers(LPCTSTR aText, Hotkey *aThisHotkey, HotkeyProperties *aProperties = NULL);
+	static ResultType TextToKey(LPCTSTR aText, bool aIsModifier, Hotkey *aThisHotkey, bool aSyntaxCheckOnly);
 
 	static void InstallKeybdHook();
 	static void InstallMouseHook();
@@ -300,7 +300,7 @@ public:
 				vp->mRunAgainAfterFinished = false;
 	}
 
-	static HookActionType ConvertAltTab(LPTSTR aBuf, bool aAllowOnOff)
+	static HookActionType ConvertAltTab(LPCTSTR aBuf, bool aAllowOnOff)
 	{
 		if (!aBuf || !*aBuf) return 0;
 		if (!_tcsicmp(aBuf, _T("AltTab"))) return HOTKEY_ID_ALT_TAB;
@@ -317,7 +317,7 @@ public:
 		return 0;
 	}
 
-	static Hotkey *FindHotkeyByTrueNature(LPTSTR aName, UCHAR &aNoSuppress, bool &aHookIsMandatory);
+	static Hotkey *FindHotkeyByTrueNature(LPCTSTR aName, UCHAR &aNoSuppress, bool &aHookIsMandatory);
 	static Hotkey *FindHotkeyContainingModLR(modLR_type aModifiersLR);  //, HotkeyIDType hotkey_id_to_omit);
 	//static Hotkey *FindHotkeyWithThisModifier(vk_type aVK, sc_type aSC);
 	//static Hotkey *FindHotkeyBySC(sc2_type aSC2, mod_type aModifiers, modLR_type aModifiersLR);

source/lib/functions.h
@@ -132,6 +132,12 @@ md_func(GroupDeactivate, (In, String, GroupName), (In_Opt, String, Mode))
 md_func_v(GuiCtrlFromHwnd, (In, UInt32, Hwnd), (Ret, Object, Gui))
 md_func_v(GuiFromHwnd, (In, UInt32, Hwnd), (In_Opt, Bool32, Recurse), (Ret, Object, Gui))
 
+md_func(HotIf, (In_Opt, Variant, Criterion))
+md_func(HotIfWinActive, (In_Opt, String, WinTitle), (In_Opt, String, WinText))
+md_func(HotIfWinExist, (In_Opt, String, WinTitle), (In_Opt, String, WinText))
+md_func(HotIfWinNotActive, (In_Opt, String, WinTitle), (In_Opt, String, WinText))
+md_func(HotIfWinNotExist, (In_Opt, String, WinTitle), (In_Opt, String, WinText))
+md_func_x(Hotkey, BIF_Hotkey, FResult, (In, String, KeyName), (In_Opt, Variant, Action), (In_Opt, String, Options))
 md_func_x(Hotstring, BIF_Hotstring, FResult, (In, String, String), (In_Opt, Variant, Replacement), (In_Opt, String, OnOffToggle), (Ret, Variant, RetVal))
 
 md_func(IL_Add, (In, UIntPtr, ImageList), (In, String, Filename), (In_Opt, Int32, IconNumber), (In_Opt, Bool32, ResizeNonIcon), (Ret, Int32, Index))

source/script.cpp
@@ -64,12 +64,6 @@ FuncEntry g_BIF[] =
 	BIF1(HasBase, 2, 2),
 	BIFn(HasMethod, 1, 3, BIF_GetMethod),
 	BIF1(HasProp, 2, 2),
-	BIFn(HotIf, 0, 1, BIF_Hotkey),
-	BIFn(HotIfWinActive, 0, 2, BIF_Hotkey),
-	BIFn(HotIfWinExist, 0, 2, BIF_Hotkey),
-	BIFn(HotIfWinNotActive, 0, 2, BIF_Hotkey),
-	BIFn(HotIfWinNotExist, 0, 2, BIF_Hotkey),
-	BIFn(Hotkey, 1, 3, BIF_Hotkey),
 	BIF1(InStr, 2, 5),
 	BIFi(IsAlnum, 1, 2, BIF_IsTypeish, VAR_TYPE_ALNUM),
 	BIFi(IsAlpha, 1, 2, BIF_IsTypeish, VAR_TYPE_ALPHA),
@@ -1949,7 +1943,7 @@ process_completed_line:
 				// v1.0.40: Check if this is a remap rather than hotkey:
 				if (!hotkey_uses_otb   
 					&& *hotkey_flag // This hotkey's action is on the same line as its trigger definition.
-					&& (remap_dest_vk = hotkey_flag[1] ? TextToVK(cp = Hotkey::TextToModifiers(hotkey_flag, NULL)) : 0xFF)   ) // And the action appears to be a remap destination rather than a command.
+					&& (remap_dest_vk = hotkey_flag[1] ? TextToVK(cp = const_cast<LPTSTR>(Hotkey::TextToModifiers(hotkey_flag, NULL))) : 0xFF)   ) // And the action appears to be a remap destination rather than a command.
 					// For above:
 					// Fix for v1.0.44.07: Set remap_dest_vk to 0xFF if hotkey_flag's length is only 1 because:
 					// 1) It allows a destination key that doesn't exist in the keyboard layout (such as 6::ð in
@@ -1969,6 +1963,7 @@ process_completed_line:
 					bool remap_source_is_combo, remap_source_is_mouse, remap_dest_is_mouse, remap_keybd_to_mouse;
 
 					// These will be ignored in other stages if it turns out not to be a remap later below:
+					LPCTSTR cp1;
 					remap_source_vk = TextToVK(cp1 = Hotkey::TextToModifiers(buf, NULL)); // An earlier stage verified that it's a valid hotkey, though VK could be zero.
 					remap_source_is_combo = _tcsstr(cp1, COMPOSITE_DELIMITER);
 					remap_source_is_mouse = IsMouseVK(remap_source_vk);
@@ -3575,9 +3570,9 @@ inline ResultType Script::IsDirective(LPTSTR aBuf)
 
 		CreateHotFunc();
 		
-		ResultToken result_token;
-		if (!Hotkey::IfExpr(NULL, mLastHotFunc, result_token))		// Set the new criterion.
-			return FAIL;
+		auto fr = Hotkey::IfExpr(mLastHotFunc); // Set the new criterion.
+		if (fr != OK) // Only OK and FR_E_OUTOFMEM should be possible given how it's called.
+			return fr == FR_E_OUTOFMEM ? ScriptError(ERR_OUTOFMEM) : FAIL;
 
 		g->HotCriterion->OriginalExpr = SimpleHeap::Alloc(parameter);
 		

commit 998cf702a51d3bd16526df4ee5dda67548ca8507

Fix BIF_Hotkey's Action assignment via Strings.
Restores the currently documented behavior, specifically:

"If Action is a hotkey name, its original function is used; specifically, the original function of the hotkey variant corresponding to the current HotIf criteria. This is usually used to restore a hotkey's original function after having changed it, but can be used to assign the function of a different hotkey, provided that both hotkeys use the same HotIf criteria."

Broken by 9d9cd3b. (See also Helgef PR 7931ec7.)

source/script2.cpp
@@ -2438,7 +2438,7 @@ bif_impl FResult BIF_Hotkey(StrArg aName, ExprTokenType *aAction, optl<StrArg> a
 			// I.e., find the function implicitly defined by "x::action".
 			for (int i = 0; i < Hotkey::sHotkeyCount; ++i)
 			{
-				if (_tcscmp(Hotkey::shk[i]->mName, aName))
+				if (_tcscmp(Hotkey::shk[i]->mName, action_string))
 					continue;
 				
 				for (HotkeyVariant* v = Hotkey::shk[i]->mFirstVariant; v; v = v->mNextVariant)