AutoHotkey/AutoHotkey

commit df9119f28c66802da2ea3017ae6afdc0084d6daf

Fixed scope of function definition expressions in fat arrow functions.

source/script.cpp
@@ -2160,17 +2160,19 @@ process_completed_line:
 					// was encountered by ParseAndAddLine().  expr->code includes up to (and including)
 					// the function name.  Append this current line to form the final expression (unless
 					// there's another function definition, in which case, rinse and repeat).
-					// If the function had no explicit name, AddFunc has given it one we can insert here.
-					size_t extra = !IS_IDENTIFIER_CHAR(expr->code[expr->length - 1]) ? _tcslen(expr->func->mName) : 0;
+					// A special marker is inserted so that ParseOperands() can reference the function.
+					size_t extra = FDE_SUBSTITUTE_STRING_LENGTH;
 					if (!buf.EnsureCapacity(--buf_length + expr->length + extra))
 						return MemoryError();
 					tmemmove(buf + expr->length + extra, buf + 1, buf_length + 1);
 					tmemmove(buf, expr->code, expr->length);
-					tmemmove(buf + expr->length, expr->func->mName, extra); // Insert automatic name, if any.
+					tmemmove(buf + expr->length, FDE_SUBSTITUTE_STRING, extra);
 					buf_length += expr->length + extra;
 					mCombinedLineNumber = expr->line_no;
 					mCurrLine = NULL; // See comments below.
 					mExprContainingThisFunc = expr->outer;
+					if (!mExprFuncs.Insert(expr->func, mExprFuncs.mCount))
+						return FAIL;
 					if (!GetLineContExpr(fp, buf, next_buf, phys_line_number, has_continuation_section))
 						return FAIL;
 					if (!expr->action && IsFunctionDefinition(buf, next_buf))
@@ -2184,7 +2186,7 @@ process_completed_line:
 							|| expr->add_block_end_after && !AddLine(ACT_BLOCK_END)  )
 							return FAIL;
 					}
-					if (mExprContainingThisFunc != expr->outer) // fn(a:=fn(){}){}
+					if (mExprContainingThisFunc != expr->outer) // (f1(){},f2(){}) or f2(a:=f1(){}){}
 					{
 						mExprContainingThisFunc->pending_hotkey = expr->pending_hotkey;
 						mExprContainingThisFunc->rejoin_first_line = expr->rejoin_first_line;
@@ -4156,10 +4158,10 @@ ResultType Script::ParseAndAddLine(LPTSTR aLineText, ActionTypeType aActionType)
 		if (!code)
 			return MemoryError();
 		tmemcpy(code, aLineText, len);
-		code[open_paren - aLineText] = '\0';
+		code[len] = '\0';
 		auto expr = new PartialExpression(code, len, aActionType, *this); // Save the prefix code and some current script state.
 		// Parse the start of the function definition, including the open brace.
-		if (!DefineFunc(id, false, true))
+		if (!DefineFunc(id, false, FuncDefExpression))
 		{
 			delete expr;
 			return FAIL;
@@ -5280,6 +5282,31 @@ ResultType Script::ParseOperands(LPTSTR aArgText, DerefList &aDeref, int *aPos, 
 							return FAIL;
 						continue;
 					}
+					else if (!_tcsnicmp(op_begin, FDE_SUBSTITUTE_STRING, FDE_SUBSTITUTE_STRING_LENGTH))
+					{
+						int index = mExprContainingThisFunc ? mExprContainingThisFunc->first_index : 0;
+						if (index >= mExprFuncs.mCount)
+							return ScriptError(ERR_EXPR_SYNTAX, FDE_SUBSTITUTE_STRING);
+						auto func = mExprFuncs.mItem[index];
+						memcpy(mExprFuncs.mItem + index, mExprFuncs.mItem + index + 1, (mExprFuncs.mCount - index) * sizeof(UserFunc*));
+						mExprFuncs.mCount--;
+						func->mOuterFunc = g->CurrentFunc;
+						auto var = AddFuncVar(func);
+						if (!var || !AddFuncToList(func))
+							return FAIL;
+
+						DerefType *d = aDeref.count ? aDeref.Last() : nullptr;
+						if (d && d->type == DT_VAR && d->marker + d->length == op_begin)
+							d->length += FDE_SUBSTITUTE_STRING_LENGTH;
+						else if (!aDeref.Push())
+							return FAIL;
+						else
+							d = aDeref.Last(), d->marker = op_begin, d->length = FDE_SUBSTITUTE_STRING_LENGTH;
+						d->type = DT_FUNCREF;
+						d->var = var;
+						op_begin = d->marker + d->length;
+						continue;
+					}
 				}
 			}
 			if (!ParseOperands(aArgText, aDeref, &j, close_char))
@@ -5524,7 +5551,7 @@ ResultType Script::ParseFatArrow(DerefList &aDeref, LPTSTR aPrmStart, LPTSTR aPr
 	{
 		orig_end = aPrmEnd[1];
 		aPrmEnd[1] = '\0';
-		if (!DefineFunc(aPrmStart, false, true))
+		if (!DefineFunc(aPrmStart, false, FuncDefFatArrow))
 			return FAIL;
 		aPrmEnd[1] = orig_end;
 	}
@@ -5533,7 +5560,7 @@ ResultType Script::ParseFatArrow(DerefList &aDeref, LPTSTR aPrmStart, LPTSTR aPr
 		// Format the parameter list as needed for DefineFunc().
 		TCHAR prm[MAX_VAR_NAME_LENGTH + 4];
 		sntprintf(prm, _countof(prm), _T("(%.*s)"), aPrmEnd - aPrmStart, aPrmStart);
-		if (!DefineFunc(prm, false, true))
+		if (!DefineFunc(prm, false, FuncDefFatArrow))
 			return FAIL;
 	}
 
@@ -5598,7 +5625,7 @@ UserFunc* Script::CreateHotFunc()
 
 
 
-ResultType Script::DefineFunc(LPTSTR aBuf, bool aStatic, bool aIsInExpression)
+ResultType Script::DefineFunc(LPTSTR aBuf, bool aStatic, FuncDefType aIsInExpression)
 // Returns OK or FAIL.
 {
 	LPTSTR param_end, param_start = _tcschr(aBuf, '('); // Caller has ensured that this will return non-NULL.
@@ -5624,14 +5651,14 @@ ResultType Script::DefineFunc(LPTSTR aBuf, bool aStatic, bool aIsInExpression)
 		*param_start = '('; // Undo temporary termination.
 
 		// Below passes class_object for AddFunc() to store the func "by reference" in it:
-		if (  !(g->CurrentFunc = AddFunc(full_name, -1, class_object))  )
+		if (  !(g->CurrentFunc = AddFunc(full_name, -1, aIsInExpression, class_object))  )
 			return FAIL;
 	}
 	else
 	{
 		// The value of g->CurrentFunc must be set here rather than by our caller since AddVar(), which we call,
 		// relies upon it having been done.
-		if (   !(g->CurrentFunc = AddFunc(aBuf, param_start - aBuf))   )
+		if (   !(g->CurrentFunc = AddFunc(aBuf, param_start - aBuf, aIsInExpression))   )
 			return FAIL; // It already displayed the error.
 	}
 
@@ -6722,7 +6749,7 @@ Func *Script::GetBuiltInFunc(LPTSTR aFuncName)
 
 
 
-UserFunc *Script::AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, Object *aClassObject)
+UserFunc *Script::AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, FuncDefType aIsInExpression, Object *aClassObject)
 // Returns the address of the new function or NULL on failure.
 // The caller must already have verified that this isn't a duplicate function.
 {
@@ -6766,6 +6793,12 @@ UserFunc *Script::AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, Object *aCl
 
 	auto the_new_func = new UserFunc(new_name);
 
+	if (aIsInExpression == FuncDefExpression)
+	{
+		the_new_func->mOuterFunc = g->CurrentFunc; // For parsing purposes; may be corrected by ParseOperands.
+		return the_new_func; // Defer the rest until the outer scope can be confirmed.
+	}
+
 	if (aClassObject)
 	{
 		LPTSTR key = _tcsrchr(new_name, '.'); // DefineFunc() always passes "ClassName.MethodName".
@@ -6808,59 +6841,74 @@ UserFunc *Script::AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, Object *aCl
 	}
 	else if (*new_name != '<') // i.e. not <Hotkey>
 	{
-		ResultType result = OK;
-		VarList *varlist;
-		int insert_pos;
-		int scope = (g->CurrentFunc ? VAR_DECLARE_LOCAL : VAR_DECLARE_GLOBAL);
 		if (is_static)
-		{
 			the_new_func->mIsStatic = true;
-			scope |= VAR_LOCAL_STATIC;
-		}
-		if (!aFuncNameLength) // Anonymous.
-		{
-			// Give it a name for debugging purposes and to allow function definition expressions to
-			// be anonymous (since they currently work by preprocessing/replacing the definition with
-			// just its name).  Starting with a digit ensures it will never conflict.
-			TCHAR buf[16];
-			static UINT sCount = 0;
-			aFuncNameLength = _stprintf(buf, _T("%ufn"), ++sCount);
-			the_new_func->mName = aFuncName = SimpleHeap::Alloc(buf, aFuncNameLength); // The previous value of the_new_func->mName was the constant _T("").
-		}
-		Var *var = aFuncNameLength ? FindVar(aFuncName, aFuncNameLength, FINDVAR_NO_BIF | scope, &varlist, &insert_pos, &result) : NULL;
-		if (var)
-		{
-			// Anything found at this early stage must have been created by a prior declaration
-			// or predefined.  Treat it as an error even if var is uninitialized, otherwise:
-			//  1) "static x" or "global x" prior to the function would affect its scope/duration
-			//     in a way that is problematic for closures and probably unintentional.
-			//  2) It would be possible to declare the function as local/global prior to defining
-			//     it, but not afterward (as the conflict would be detected by ParseAndAddLine).
-			ConflictingDeclarationError(_T("function"), var);
+		if (!AddFuncVar(the_new_func))
 			return nullptr;
-		}
-		var = AddVar(aFuncName, aFuncNameLength, varlist, insert_pos, scope | ADDVAR_NO_VALIDATE); // Already validated, or invalid in the case of anonymous functions.
-		if (!var)
-			return nullptr;
-		var->Assign(the_new_func);
-		var->MakeReadOnly();
 	}
 
-	the_new_func->mOuterFunc = g->CurrentFunc;
+	return AddFuncToList(the_new_func);
+}
+
+
+
+Var *Script::AddFuncVar(UserFunc *aFunc)
+{
+	ResultType result = OK;
+	VarList *varlist;
+	int insert_pos;
+	int scope = (g->CurrentFunc ? VAR_DECLARE_LOCAL : VAR_DECLARE_GLOBAL);
+	if (aFunc->mIsStatic)
+		scope |= VAR_LOCAL_STATIC;
+	size_t name_length = _tcslen(aFunc->mName);
+	Var *var = name_length ? FindVar(aFunc->mName, name_length, FINDVAR_NO_BIF | scope, &varlist, &insert_pos, &result) : NULL;
+	if (var && var->IsDeclared())
+	{
+		// Anything found at this early stage must have been created by a prior declaration
+		// or predefined.  Treat it as an error even if var is uninitialized, otherwise:
+		//  1) "static x" or "global x" prior to the function would affect its scope/duration
+		//     in a way that is problematic for closures and probably unintentional.
+		//  2) It would be possible to declare the function as local/global prior to defining
+		//     it, but not afterward (as the conflict would be detected by ParseAndAddLine).
+		ConflictingDeclarationError(_T("function"), var);
+		return nullptr;
+	}
+	if (!var)
+	{
+		if (!name_length)
+		{
+			insert_pos = 0;
+			varlist = g->CurrentFunc ? &g->CurrentFunc->mVars : GlobalVars();
+		}
+		var = AddVar(aFunc->mName, name_length, varlist, insert_pos, scope | ADDVAR_NO_VALIDATE); // Already validated, or invalid in the case of anonymous functions.
+		if (!var)
+			return nullptr;
+	}
+	ASSERT(var->IsUninitialized());
+	var->Assign(aFunc);
+	var->MakeReadOnly();
+	return var;
+}
+
+
+	
+UserFunc *Script::AddFuncToList(UserFunc *aFunc)
+{
+	aFunc->mOuterFunc = g->CurrentFunc;
 	
 	// The general rule is that a nested function's namespace includes all of the names that
 	// were in the outer function's namespace by default.  In other words, if the outer function
 	// is assume-global, the nested function should also default to assume-global.
-	if (the_new_func->mOuterFunc && the_new_func->mOuterFunc->IsAssumeGlobal())
-		the_new_func->mDefaultVarType = VAR_GLOBAL; // Not VAR_DECLARE_GLOBAL, which would prevent referencing of outer vars.
+	if (aFunc->mOuterFunc && aFunc->mOuterFunc->IsAssumeGlobal() && aFunc->mDefaultVarType == VAR_DECLARE_LOCAL)
+		aFunc->mDefaultVarType = VAR_GLOBAL; // Not VAR_DECLARE_GLOBAL, which would prevent referencing of outer vars.
 
-	if (!mFuncs.Insert(the_new_func, mFuncs.mCount))
+	if (!mFuncs.Insert(aFunc, mFuncs.mCount))
 	{
 		ScriptError(ERR_OUTOFMEM);
 		return nullptr;
 	}
 
-	return the_new_func;
+	return aFunc;
 }
 
 
@@ -7130,7 +7178,7 @@ Var *Script::FindUpVar(LPCTSTR aVarName, size_t aVarNameLength, UserFunc &aInner
 	// At this point aInner doesn't have a variable by this name, so create one:
 	int insert_pos;
 	aInner.mVars.Find(aVarName, &insert_pos);
-	Var *inner_var = AddVar(aVarName, aVarNameLength, &aInner.mVars, insert_pos, VAR_LOCAL | VAR_DECLARED); // VAR_DECLARED suppresses checking for a global for #Warn LocalSameAsGlobal or when #Warn UseUnset is triggered.
+	Var *inner_var = AddVar(aVarName, aVarNameLength, &aInner.mVars, insert_pos, VAR_LOCAL | VAR_DECLARED | ADDVAR_NO_VALIDATE); // VAR_DECLARED suppresses checking for a global for #Warn LocalSameAsGlobal or when #Warn UseUnset is triggered.
 	if (!inner_var)
 	{
 		if (aDisplayError)

source/script.h
@@ -419,6 +419,13 @@ struct ArgStruct
 	int max_stack, max_alloc;
 };
 
+enum FuncDefType : UCHAR
+{
+	FuncDefNormal = FALSE,
+	FuncDefFatArrow,
+	FuncDefExpression
+};
+
 __int64 pow_ll(__int64 base, __int64 exp); // integer power function
 
 #define _f__oneline(act)		do { act } while (0)		// Make the macro safe to use like a function, under if(), etc.
@@ -1559,7 +1566,7 @@ public:
 	int mClosureCount = 0;
 
 	// Keep small members adjacent to each other to save space and improve perf. due to byte alignment:
-	bool mIsFuncExpression; // Whether this function was defined *within* an expression and is therefore allowed under a control flow statement.
+	FuncDefType mIsFuncExpression; // Whether this function was defined *within* an expression and is therefore allowed under a control flow statement.
 	bool mIsStatic = false; // Whether the "static" keyword was used with a function (not method); this prevents a nested function from becoming a closure.
 #define VAR_DECLARE_GLOBAL (VAR_DECLARED | VAR_GLOBAL)
 #define VAR_DECLARE_LOCAL  (VAR_DECLARED | VAR_LOCAL)
@@ -2806,15 +2813,19 @@ private:
 		size_t length;
 		Line *rejoin_first_line = nullptr, *rejoin_last_line = nullptr;
 		LPCTSTR pending_hotkey = nullptr;
+		int first_index;
 		LineNumberType line_no;
 		ActionTypeType action;
 		bool add_block_end_after = false;
 		PartialExpression(LPTSTR aCode, size_t aLen, ActionTypeType aAct, Script &aScript)
 			: code(aCode), length(aLen), action(aAct)
 			, outer(aScript.mExprContainingThisFunc)
+			, first_index(aScript.mExprFuncs.mCount)
 			, line_no(aScript.mCombinedLineNumber) {}
 		~PartialExpression() { free(code); }
 	};
+#define FDE_SUBSTITUTE_STRING L"(\u2026){}"
+#define FDE_SUBSTITUTE_STRING_LENGTH (_countof(FDE_SUBSTITUTE_STRING)-1)
 
 	Line *mFirstLine, *mLastLine;     // The first and last lines in the linked list.
 	Label *mFirstLabel, *mLastLabel;  // The first and last labels in the linked list.
@@ -2822,6 +2833,7 @@ private:
 	int mLabelCount;
 #endif
 	FuncList mFuncs;
+	FuncList mExprFuncs; // Pending function definition expressions, from within an expression yet to be parsed.
 	
 	VarList mVars; // Sorted list of global variables.
 	WinGroup *mFirstGroup, *mLastGroup;  // The first and last variables in the linked list.
@@ -2999,7 +3011,7 @@ public:
 	void DeleteTimer(IObject *aCallback);
 	LPTSTR DefaultDialogTitle();
 	UserFunc* CreateHotFunc();
-	ResultType DefineFunc(LPTSTR aBuf, bool aStatic = false, bool aIsInExpression = false);
+	ResultType DefineFunc(LPTSTR aBuf, bool aStatic = false, FuncDefType aIsInExpression = FuncDefNormal);
 #ifndef AUTOHOTKEYSC
 	struct FuncLibrary
 	{
@@ -3013,7 +3025,9 @@ public:
 	Func *FindGlobalFunc(LPCTSTR aFuncName, size_t aFuncNameLength = 0);
 	static Func *GetBuiltInFunc(LPTSTR aFuncName);
 	static Func *GetBuiltInMdFunc(LPTSTR aFuncName);
-	UserFunc *AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, Object *aClassObject = NULL);
+	UserFunc *AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, FuncDefType aIsInExpression = FuncDefNormal, Object *aClassObject = nullptr);
+	Var *AddFuncVar(UserFunc *aFunc);
+	UserFunc *AddFuncToList(UserFunc *aFunc);
 
 	ResultType DefineClass(LPTSTR aBuf);
 	UserFunc *DefineClassInit(bool aStatic);

commit 49d4744b9f9f447ba11e5373cbc5edbd0793497a

Fixed closures inside function definition expressions.
Broken by df9119f2 (due to inner function preceding outer in mFuncs).

source/script.cpp
@@ -2162,8 +2162,6 @@ process_completed_line:
 					mCombinedLineNumber = expr->line_no;
 					mCurrLine = NULL; // See comments below.
 					mExprContainingThisFunc = expr->outer;
-					if (!mExprFuncs.Insert(expr->func, mExprFuncs.mCount))
-						return FAIL;
 					if (!GetLineContExpr(fp, buf, next_buf, phys_line_number, has_continuation_section))
 						return FAIL;
 					if (!expr->action && IsFunctionDefinition(buf, next_buf))
@@ -5278,15 +5276,16 @@ ResultType Script::ParseOperands(LPTSTR aArgText, DerefList &aDeref, int *aPos, 
 					}
 					else if (!_tcsnicmp(op_begin, FDE_SUBSTITUTE_STRING, FDE_SUBSTITUTE_STRING_LENGTH))
 					{
-						int index = mExprContainingThisFunc ? mExprContainingThisFunc->first_index : 0;
-						if (index >= mExprFuncs.mCount)
-							return ScriptError(ERR_EXPR_SYNTAX, FDE_SUBSTITUTE_STRING);
-						auto func = mExprFuncs.mItem[index];
-						memcpy(mExprFuncs.mItem + index, mExprFuncs.mItem + index + 1, (mExprFuncs.mCount - index) * sizeof(UserFunc*));
-						mExprFuncs.mCount--;
+						// Find the next unresolved FDE at the current nesting level.
+						int &index = mExprContainingThisFunc ? mExprContainingThisFunc->funcs_index : mExprFuncIndex;
+						for (; index < mFuncs.mCount && mFuncs.mItem[index]->mIsFuncExpression != FuncDefExpression; ++index);
+						if (index >= mFuncs.mCount)
+							return ScriptError(ERR_EXPR_SYNTAX, FDE_SUBSTITUTE_STRING); // The source code likely included FDE_SUBSTITUTE_STRING, but it might have been a prior one.
+						auto func = mFuncs.mItem[index++];
+						func->mIsFuncExpression = FuncDefExpressionResolved; // Allow outer functions to skip over this one when looking for their own FDE.
 						func->mOuterFunc = g->CurrentFunc;
 						auto var = AddFuncVar(func);
-						if (!var || !AddFuncToList(func))
+						if (!var)
 							return FAIL;
 
 						DerefType *d = aDeref.count ? aDeref.Last() : nullptr;
@@ -5674,8 +5673,6 @@ ResultType Script::DefineFunc(LPTSTR aBuf, bool aStatic, FuncDefType aIsInExpres
 	func.mLineNumber = mCombinedLineNumber;
 	func.mFileIndex = mCurrFileIndex;
 #endif
-
-	func.mIsFuncExpression = aIsInExpression;
 
 	if (is_method)
 	{
@@ -6787,11 +6784,7 @@ UserFunc *Script::AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, FuncDefType
 
 	auto the_new_func = new UserFunc(new_name);
 
-	if (aIsInExpression == FuncDefExpression)
-	{
-		the_new_func->mOuterFunc = g->CurrentFunc; // For parsing purposes; may be corrected by ParseOperands.
-		return the_new_func; // Defer the rest until the outer scope can be confirmed.
-	}
+	the_new_func->mIsFuncExpression = aIsInExpression;
 
 	if (aClassObject)
 	{
@@ -6833,7 +6826,8 @@ UserFunc *Script::AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, FuncDefType
 		// Also add it to the script's list of functions, to support #Warn LocalSameAsGlobal
 		// and automatic cleanup of objects in static vars on program exit.
 	}
-	else if (*new_name != '<') // i.e. not <Hotkey>
+	else if (*new_name != '<' // i.e. not <Hotkey>
+		&& aIsInExpression != FuncDefExpression) // Defer the following for these.
 	{
 		if (is_static)
 			the_new_func->mIsStatic = true;
@@ -6890,13 +6884,23 @@ UserFunc *Script::AddFuncToList(UserFunc *aFunc)
 {
 	aFunc->mOuterFunc = g->CurrentFunc;
 	
-	// The general rule is that a nested function's namespace includes all of the names that
-	// were in the outer function's namespace by default.  In other words, if the outer function
-	// is assume-global, the nested function should also default to assume-global.
-	if (aFunc->mOuterFunc && aFunc->mOuterFunc->IsAssumeGlobal() && aFunc->mDefaultVarType == VAR_DECLARE_LOCAL)
-		aFunc->mDefaultVarType = VAR_GLOBAL; // Not VAR_DECLARE_GLOBAL, which would prevent referencing of outer vars.
+	// Function definition expressions are parsed before the expression which "contains" them,
+	// which puts the FDE before its outer function in mFuncs, in cases like `outer() => FDE(){`.
+	// To correct that, insert new non-FDE functions before the first unresolved FDE.
+	int &index = mExprContainingThisFunc ? mExprContainingThisFunc->funcs_index : mExprFuncIndex;
+	int new_index = mFuncs.mCount;
+	if (index == INT_MAX && aFunc->mIsFuncExpression == FuncDefExpression)
+		index = new_index;
+	if (aFunc->mIsFuncExpression != FuncDefExpression && index <= new_index) // <= vs < keeps it pointing beyond the end when there aren't any FDEs.
+	{
+		// index might not point to an FDE if functions were defined within a previous FDE,
+		// or if a previous FDE was resolved prior to this definition.  This might not be
+		// necessary, but keeps mFuncs in a sensible order:
+		for (; index < mFuncs.mCount && mFuncs.mItem[index]->mIsFuncExpression != FuncDefExpression; ++index);
+		new_index = index++; // Insert here and increment index so it still points to the same item.
+	}
 
-	if (!mFuncs.Insert(aFunc, mFuncs.mCount))
+	if (!mFuncs.Insert(aFunc, new_index))
 	{
 		ScriptError(ERR_OUTOFMEM);
 		return nullptr;
@@ -7429,6 +7433,16 @@ ResultType Script::PreparseExpressions(FuncList &aFuncs)
 	for (int i = 0; i < aFuncs.mCount; ++i)
 	{
 		UserFunc &func = *aFuncs.mItem[i];
+		
+		// Variables that were assignable in the outer function should also be assignable
+		// in the inner function by default; i.e. assume-global mode should be inherited.
+		// This must be done before assignments are parsed, and can't be done when the
+		// function is first added because a function definition expression's initial value
+		// for mOuterFunc can be incorrect (if it's inside a parameter default initializer
+		// or fat arrow function, although the latter can't use a global declaration).
+		if (func.mOuterFunc && func.mOuterFunc->IsAssumeGlobal() && func.mDefaultVarType == VAR_DECLARE_LOCAL)
+			func.mDefaultVarType = VAR_GLOBAL; // Not VAR_DECLARE_GLOBAL, which would prevent referencing of outer vars.
+
 		g->CurrentFunc = &func;
 		if (!PreparseExpressions(func.mJumpToLine)) // Preparse this function's body.
 			return FAIL;

source/script.h
@@ -421,7 +421,8 @@ enum FuncDefType : UCHAR
 {
 	FuncDefNormal = FALSE,
 	FuncDefFatArrow,
-	FuncDefExpression
+	FuncDefExpression,
+	FuncDefExpressionResolved
 };
 
 __int64 pow_ll(__int64 base, __int64 exp); // integer power function
@@ -2811,14 +2812,14 @@ private:
 		size_t length;
 		Line *rejoin_first_line = nullptr, *rejoin_last_line = nullptr;
 		LPCTSTR pending_hotkey = nullptr;
-		int first_index;
+		int funcs_index;
 		LineNumberType line_no;
 		ActionTypeType action;
 		bool add_block_end_after = false;
 		PartialExpression(LPTSTR aCode, size_t aLen, ActionTypeType aAct, Script &aScript)
 			: code(aCode), length(aLen), action(aAct)
 			, outer(aScript.mExprContainingThisFunc)
-			, first_index(aScript.mExprFuncs.mCount)
+			, funcs_index(aScript.mFuncs.mCount + 1) // +1 to exclude func itself.
 			, line_no(aScript.mCombinedLineNumber) {}
 		~PartialExpression() { free(code); }
 	};
@@ -2831,7 +2832,6 @@ private:
 	int mLabelCount;
 #endif
 	FuncList mFuncs;
-	FuncList mExprFuncs; // Pending function definition expressions, from within an expression yet to be parsed.
 	
 	VarList mVars; // Sorted list of global variables.
 	WinGroup *mFirstGroup, *mLastGroup;  // The first and last variables in the linked list.
@@ -2840,6 +2840,7 @@ private:
 	Line *mLastParamInitializer;
 	LPCTSTR mPendingHotkey = nullptr; // The name of a hotkey or hotstring awaiting its block/function.
 	PartialExpression *mExprContainingThisFunc = nullptr;
+	int mExprFuncIndex = INT_MAX;
 	SymbolType mDefaultReturn = SYM_STRING;
 	bool mNextLineIsFunctionBody; // Whether the very next line to be added will be the first one of the body.
 	bool mIgnoreNextBlockBegin;