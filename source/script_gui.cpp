AutoHotkey/AutoHotkey

commit 9716f2a7b880a28c90794c57e55446ca88f59783

Added CueText for ComboBox controls

source/script_gui.cpp
@@ -626,6 +626,11 @@ ObjectMemberMd GuiControlType::sMembersSB[] =
 	md_member_x(GuiControlType, SetText, SB_SetText, CALL, (In, String, NewText), (In_Opt, UInt32, PartNumber), (In_Opt, UInt32, Style))
 };
 
+ObjectMemberMd GuiControlType::sMembersCB[] =
+{
+	md_member_x(GuiControlType, CueText, CB_CueText, CALL, (In, String, CueText))
+};
+
 #undef FUN1
 #undef FUNn
 
@@ -658,8 +663,8 @@ void GuiControlType::DefineControlClasses()
 		switch (i)
 		{
 		case GUI_CONTROL_TAB: more_items = sMembersTab; how_many = _countof(sMembersTab); // Fall through:
+		case GUI_CONTROL_COMBOBOX: more_items = sMembersCB; how_many = _countof(sMembersCB);
 		case GUI_CONTROL_DROPDOWNLIST:
-		case GUI_CONTROL_COMBOBOX:
 		case GUI_CONTROL_LISTBOX: base_proto = sPrototypeList; base_class = list_class; break;
 		case GUI_CONTROL_DATETIME: more_items = sMembersDate; how_many = _countof(sMembersDate); break;
 		case GUI_CONTROL_LISTVIEW: more_items = sMembersLV; how_many = _countof(sMembersLV); break;
@@ -891,6 +896,12 @@ FResult GuiControlType::set_Visible(BOOL aValue)
 	CTRL_THROW_IF_DESTROYED;
 	gui->ControlSetVisible(*this, aValue);
 	return OK;
+}
+
+
+FResult GuiControlType::CB_CueText(StrArg aCueText)
+{
+	return SendMessage(hwnd, CB_SETCUEBANNER, 0, (LPARAM)aCueText) ? OK : FR_E_FAILED;
 }
 
 

source/script.h
@@ -2282,6 +2282,7 @@ struct GuiControlType : public Object
 	static ObjectMemberMd sMembersLV[];
 	static ObjectMemberMd sMembersTV[];
 	static ObjectMemberMd sMembersSB[];
+	static ObjectMemberMd sMembersCB[];
 
 	static Object *sPrototype, *sPrototypeList;
 	static Object *sPrototypes[GUI_CONTROL_TYPE_COUNT];
@@ -2339,7 +2340,9 @@ struct GuiControlType : public Object
 	FResult SB_SetIcon(StrArg aFilename, optl<int> aIconNumber, optl<UINT> aPartNumber, UINT_PTR &aRetVal);
 	FResult SB_SetParts(VariantParams &aParam, UINT& aRetVal);
 	FResult SB_SetText(StrArg aNewText, optl<UINT> aPartNumber, optl<UINT> aStyle);
-	
+
+	FResult CB_CueText(StrArg aCueText);
+
 	FResult Tab_UseTab(ExprTokenType *aTab, optl<BOOL> aExact);
 	
 	FResult TV_AddModify(bool aAdd, UINT_PTR aItemID, UINT_PTR aParentItemID, optl<StrArg> aOptions, optl<StrArg> aName, UINT_PTR &aRetVal);

commit 1f9441c0b1e71b0e5683490b873e44035a5f4772

Fix Tab.Prototype.UseTab
Broken by 9716f2a7 (#315)

source/script_gui.cpp
@@ -667,15 +667,20 @@ void GuiControlType::DefineControlClasses()
 		int how_many = 0;
 		switch (i)
 		{
-		case GUI_CONTROL_TAB: more_items = sMembersTab; how_many = _countof(sMembersTab); // Fall through:
-		case GUI_CONTROL_COMBOBOX: more_items = sMembersCB; how_many = _countof(sMembersCB);
-		case GUI_CONTROL_DROPDOWNLIST:
-		case GUI_CONTROL_LISTBOX: base_proto = sPrototypeList; base_class = list_class; break;
+		case GUI_CONTROL_TAB: more_items = sMembersTab; how_many = _countof(sMembersTab); break;
+		case GUI_CONTROL_COMBOBOX: more_items = sMembersCB; how_many = _countof(sMembersCB); break;
 		case GUI_CONTROL_DATETIME: more_items = sMembersDate; how_many = _countof(sMembersDate); break;
 		case GUI_CONTROL_EDIT: more_items = sMembersEdit; how_many = _countof(sMembersEdit); break;
 		case GUI_CONTROL_LISTVIEW: more_items = sMembersLV; how_many = _countof(sMembersLV); break;
 		case GUI_CONTROL_TREEVIEW: more_items = sMembersTV; how_many = _countof(sMembersTV); break;
 		case GUI_CONTROL_STATUSBAR: more_items = sMembersSB; how_many = _countof(sMembersSB); break;
+		}
+		switch (i)
+		{
+		case GUI_CONTROL_TAB:
+		case GUI_CONTROL_COMBOBOX:
+		case GUI_CONTROL_DROPDOWNLIST:
+		case GUI_CONTROL_LISTBOX: base_proto = sPrototypeList; base_class = list_class; break;
 		}
 		TCHAR buf[32];
 		_sntprintf(buf, 32, _T("Gui.%s"), sTypeNames[i]);